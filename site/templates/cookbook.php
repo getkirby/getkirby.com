fe16613630f7f0110291f786adab27744c0bf509
Bastian Allgeier <mail@bastianallgeier.com>
2019-01-19 15:39:35 +0100

    Add cookbook search

--- site/templates/cookbook.php
+++ site/templates/cookbook.php
@@ -2,21 +2,22 @@
 
   <main class="cookbook-page | main" id="maincontent">
 
-    <div class="wrap">
+    <div class="wrap" id="cookbook-recipes">
 
       <header class="hero">
         <h1>Cookbook</h1>
+        <input class="cookbook-search search" placeholder="Search recipes …" />
       </header>
 
-      <ul class="cookbook-articles cardgrid">
+      <ul class="cookbook-articles cardgrid list">
         <?php foreach($recipes as $item): ?>
         <li class="cardgrid-item" id="<?= $item->slug() ?>">
           <a href="<?= $item->url() ?>" class="cardgrid-link">
-            <small class="h6">
+            <small class="h6 cookbook-category">
               <?= $item->parent()->title() ?>
             </small>
-            <h2 class="h5"><?= $item->title()->widont() ?></h2>
-            <p class="description">
+            <h2 class="h5 cookbook-title"><?= $item->title()->widont() ?></h2>
+            <p class="description cookbook-description">
               <?= $item->description() ?>
             </p>
           </a>
---
+++ src/js/templates/cookbook.js
@@ -0,0 +1,11 @@
+import List from 'list.js';
+
+var options = {
+  valueNames: [
+    "cookbook-category",
+    "cookbook-title",
+    "cookbook-description"
+  ]
+};
+
+var recipes = new List('cookbook-recipes', options);
