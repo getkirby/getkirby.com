e9553bc99933a839bef7f5eb9b021446ab026905
Nico Hoffmann <on-github@nhoffmann.com>
2019-01-19 17:42:49 +0100

    Revert "Draft implementation of return type links"
    This reverts commit db44b4793091655add5ecc992d1d640b4143fffc.
    
    # Conflicts:
    #	site/snippets/method/returns.php

--- site/snippets/method/returns.php
+++ site/snippets/method/returns.php
@@ -1,10 +1,4 @@
 <?php if ($type !== null): ?>
 <h2 id="returns"><a href="#returns">Return type</a></h2>
-<code>
-  <?php if ($reference = page('docs/reference')->children()->filterBy('template', 'class')->filterBy('class', $type)->first()) : ?>
-    <?= Html::a($reference->url(), $type) ?>
-  <?php else : ?>
-    <?= $type ?>
-  <?php endif ?>
-</code>
+<code><?= $type ?></code>
 <?php endif ?>
