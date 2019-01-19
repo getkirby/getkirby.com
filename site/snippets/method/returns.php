db44b4793091655add5ecc992d1d640b4143fffc
Nico Hoffmann <on-github@nhoffmann.com>
2019-01-19 17:31:51 +0100

    Draft implementation of return type links

--- site/snippets/method/returns.php
+++ site/snippets/method/returns.php
@@ -1,4 +1,10 @@
 <?php if ($type !== null): ?>
 <h2 id="returns"><a href="#returns">Return Type</a></h2>
-<code><?= $type ?></code>
+<code>
+  <?php if ($reference = page('docs/reference')->children()->filterBy('template', 'class')->filterBy('class', $type)->first()) : ?>
+    <?= Html::a($reference->url(), $type) ?>
+  <?php else : ?>
+    <?= $type ?>
+  <?php endif ?>
+</code>
 <?php endif ?>
