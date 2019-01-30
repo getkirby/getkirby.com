13b74b44e5e63ae2b1138d46d6f5d70abdec7bad
Bastian Allgeier <mail@bastianallgeier.com>
2019-01-30 15:35:34 +0100

    Fix team links and remove email addresses

--- site/templates/community.php
+++ site/templates/community.php
@@ -70,13 +70,6 @@
                   </a>
                 </li>
                 <?php endif ?>
-                <?php if($mate->email()->isNotEmpty()): ?>
-                <li>
-                  <a href="mailto:<?= $mate->email() ?>">
-                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 16 16" xml:space="preserve" width="16" height="16"><g class="nc-icon-wrapper" fill="#0d0f12"><path fill="#0d0f12" d="M15,1H1C0.448,1,0,1.447,0,2v12c0,0.553,0.448,1,1,1h14c0.552,0,1-0.447,1-1V2C16,1.447,15.552,1,15,1z M14,13H2V6.723l5.504,3.145c0.308,0.176,0.685,0.176,0.992,0L14,6.723V13z M14,4.42L8,7.849L2,4.42V3h12V4.42z"></path></g></svg>
-                  </a>
-                </li>
-                <?php endif ?>
                 <?php if($mate->twitter()->isNotEmpty()): ?>
                 <li>
                   <a href="https://twitter.com/<?= $mate->twitter() ?>">
