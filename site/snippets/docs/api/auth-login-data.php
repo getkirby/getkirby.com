418d888d524e13d9d7f54fb1f15ac9eb6607e8f7
Nico Hoffmann <on-github@nhoffmann.com>
2019-02-02 17:16:28 +0100

    Fix #203

--- site/snippets/docs/api/auth-login-data.php
+++ site/snippets/docs/api/auth-login-data.php
@@ -2,3 +2,4 @@
 | - | - | - | - |
 | `email`    | `string` | `required` | The email address of the user |
 | `password` | `string` | `required` | The password of the user |
+| `long` | `bool` | <code></code> | Create a long-lasting session |
