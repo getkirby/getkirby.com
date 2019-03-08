c5a77e5558160797d0bd80e03eabc494a2851e26
Nico Hoffmann <on-github@nhoffmann.com>
2019-03-08 15:52:31 +0100

    Fix switch between simple and advanced view

--- site/plugins/site/pagesMethods.php
+++ site/plugins/site/pagesMethods.php
@@ -3,22 +3,23 @@
 return [
     
     'forCheatsheet' => function () {
-        $advanced = param('advanced') === 'true';
-        $entries  = $this->listed();
-  
-        if ($advanced === false) {
-            $entries = $entries->filter(function ($p) {
-                if (method_exists($p, 'isInternal') && method_exists($p, 'isDeprecated')) {
-                    return !$p->isInternal() && !$p->isDeprecated();
-                }
-                return  true;
-            });
+        if (param('advanced') === 'true') {
+            return $this->listed();
         }
         
-        return $entries;
+        return $this->simple();
     },
     
     'hasAdvanced' => function () {
-        return $this->listed()->count() !== $this->forCheatsheet()->count();
+        return $this->listed()->count() !== $this->simple()->count();
+    },
+    
+    'simple' => function () {
+        return $this->listed()->filter(function ($p) {
+            if (method_exists($p, 'isInternal') && method_exists($p, 'isDeprecated')) {
+                return !$p->isInternal() && !$p->isDeprecated();
+            }
+            return  true;
+        });
     }
 ];
