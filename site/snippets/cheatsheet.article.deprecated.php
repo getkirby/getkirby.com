20d2aeac61991519458706fb99c349c382b0315e
Nico Hoffmann <on-github@nhoffmann.com>
2019-03-12 09:19:34 +0100

    Fix deprecated snippet

--- site/snippets/cheatsheet.article.deprecated.php
+++ site/snippets/cheatsheet.article.deprecated.php
@@ -2,7 +2,7 @@
   <aside class="warning">
     <?php icon('warning') ?>
     <strong>
-      Deprecated in <?= version($deprecated[0], '<code>%s</code>') ?>
+      Deprecated in <code><?= version($deprecated[0], '%s') ?></code>
     </strong>
     <?php if (isset($deprecated[1]) === true) : ?>
     <?= parseObjectReference($deprecated[1], $page->className()) ?>
