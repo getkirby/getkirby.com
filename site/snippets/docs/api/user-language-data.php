50f5a0eee8fc51c0d40335ad6806aeaab0f47921
Nico Hoffmann <on-github@nhoffmann.com>
2019-03-12 14:14:41 +0100

    Update docs for users API

--- site/snippets/docs/api/user-language-data.php
+++ site/snippets/docs/api/user-language-data.php
@@ -1,3 +1,3 @@
 | Parameter | Type | Required | Description |
 | - | - | - | - |
-| `language` | `string` | `required` | The code for the new language for the user. Example: "de" |
+| `language` | `string` | `required` | The code for the new language for the user. Example: `de` |
