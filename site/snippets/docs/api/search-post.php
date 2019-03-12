2ab74948326629599cef9816da89673644d3f796
Nico Hoffmann <on-github@nhoffmann.com>
2019-03-12 20:20:06 +0100

    Add new API search endpoints

---
+++ site/snippets/docs/api/search-post.php
@@ -0,0 +1,9 @@
+| Parameter | Type | Required | Description |
+| - | - | - | - |
+| `search` | `string|array` | <code></code> | Either search string or array with `query` and `options` keys |
+| `filterBy` | `array` | <code></code> | Array of filters |
+| `sortBy` | `string|array` | <code></code> | Sort the returned results |
+| `not` | `array` | <code></code> | Array of results to be excluded |
+| `paginate` | `array` | <code></code> | Paginate the returned results |
+| `limit` | `int` | <code></code> | Limits the number of returned results|
+| `offset` | `int` | <code></code> | Sets a manual offset |
