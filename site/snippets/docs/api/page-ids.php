4fba0d847369e63fbcc210d3bc6226e8c841117f
Nico Hoffmann <on-github@nhoffmann.com>
2019-03-12 13:42:19 +0100

    Update docs for page API

--- site/snippets/docs/api/page-ids.php
+++ site/snippets/docs/api/page-ids.php
@@ -1,9 +1,6 @@
 <info>
 All slashes in page IDs must be replaced with a plus
 
-**No**
-<code>/api/pages/**projects/example-project**/files</code>
-
-**Yes**
-<code>/api/pages/**projects+example-project**/files</code>
+**Not** <code>/api/pages/**projects/example-project**/files</code>,
+**but** <code>/api/pages/**projects+example-project**/files</code>
 </info>
