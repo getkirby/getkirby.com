70400c4b2b5b1af943f9bf43646138b086f0a833
Nico Hoffmann <on-github@nhoffmann.com>
2019-03-12 13:29:37 +0100

    Update docs for languages API

--- site/snippets/docs/api/language-update-data.php
+++ site/snippets/docs/api/language-update-data.php
@@ -2,5 +2,5 @@
 | - | - | - | - |
 | `name` | `string` | <code></code> | The display name of the language |
 | `locale` | `string` | <code></code> | PHP locale string |
-| `direction` | `string` | <code></code> | "ltr" or "rtl". "ltr" will be used as default. |
-| `url` | `string` | <code></code> | Custom Url path setting for the language. If not set, the language code will be used as Url path. |
+| `direction` | `string` | <code></code> | `ltr` or `rtl`. `ltr` will be used as default. |
+| `url` | `string` | <code></code> | Custom URL path setting for the language. If not set, the language code will be used as URL path. |
