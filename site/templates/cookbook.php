b2ddd6eeee1c25864d2b0d0028af4cebb6925e28
Nico Hoffmann <on-github@nhoffmann.com>
2019-04-14 12:48:42 +0200

    Refactor cookbook

--- site/templates/cookbook.php
+++ site/templates/cookbook.php
@@ -1,32 +1,42 @@
 <?php snippet('header') ?>
 
-  <main class="cookbook-page | main" id="maincontent">
+<main class="main">
 
-    <div class="wrap" id="cookbook-recipes">
+  <div id="cookbook" class="wrap">
 
-      <header class="hero">
-        <h1>Cookbook</h1>
-        <input class="cookbook-search search" placeholder="Search recipes …" />
-      </header>
+    <header class="hero filter-hero">
+      <h1><a href="<?= $page->url() ?>">Cookbook</a></h1>
+      <input class="filter-search search" placeholder="Search recipes …" />
+    </header>
 
-      <ul class="cookbook-articles cardgrid list">
-        <?php foreach($recipes as $item): ?>
-        <li class="cardgrid-item" id="<?= $item->slug() ?>">
-          <a href="<?= $item->url() ?>" class="cardgrid-link">
-            <small class="h6 cookbook-category">
-              <?= $item->parent()->title() ?>
-            </small>
-            <h2 class="h5 cookbook-title"><?= $item->title()->widont() ?></h2>
-            <p class="description cookbook-description">
-              <?= $item->description() ?>
-            </p>
-          </a>
-        </li>
-        <?php endforeach ?>
-      </ul>
+    <div class="filter-layout">
+      <aside class="filter-sidebar">
+        <div class="filter-filters">
+          <h2 class="h6 -mb:medium">Categories</h2>
+          <ul class="filter-categories">
+            <?php foreach ($categories as $cat): ?>
+            <li<?php e($category === $cat->slug(), ' aria-current') ?>>
+              <a href="?category=<?= $cat->slug() ?>"><span class="filter-category-icon"><?= icon($cat->icon()) ?></span> <?= $cat->title() ?></a>
+            </li>
+            <?php endforeach ?>
+          </ul>
+        </div>
+      </aside>
+      <div class="filter-overview">
+        <h2 class="h6 -mb:medium">
+          <?php if ($category): ?>
+          <?php e($category, '<small>Category</small>: ' . $category->title()) ?>
+          <?php else: ?>
+          All recipes
+          <?php endif ?>
+        </h2>
+        <?php snippet('recipes', ['recipes' => $recipes]) ?>
 
+      </div>
     </div>
 
-  </main>
+  </div>
+
+</main>
 
 <?php snippet('footer') ?>
