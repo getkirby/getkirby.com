9976f6c70887ab4a9afb5749042d4f9a7f13a8b3
Nico Hoffmann <on-github@nhoffmann.com>
2019-04-14 19:03:10 +0200

    Multiple categories for recipes

--- site/controllers/recipe.php
+++ site/controllers/recipe.php
@@ -2,8 +2,17 @@
 
 return function ($page) {
 
+  $categories = array_map(function ($category) use ($page) {
+    return [
+      'slug'  => $category->slug(),
+      'title' => $category->title(),
+      'items' => $category->parent()->recipes($category->slug())->not($page)->shuffle()->limit(5)
+    ];
+  }, $page->categories());
+
   return [
-    'headlines' => $page->text()->headlines('h2')
+    'headlines'  => $page->text()->headlines('h2'),
+    'categories' => $categories
   ];
 
 };
