243f76314a53305c697d791398d04afe0028f5b5
Fabian Michael <hallo@fabianmichael.de>
2019-04-25 16:44:41 +0200

    Add noscript-fallback for lazyloaded images and fix resizing of images, when Cloudinary is deactivated.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 // Load config package first, to set CDN path. The
 // corresponding code has to be in its own package, otherwise
 // it would not execute before any other imports are resolved.
-import './components/config';
+import "./components/config";
 
 /* -----  Polyfills  -------------------------------------------------------- */
 
