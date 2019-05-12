7154940e99e2d3e7899d44aa3ea2e3671908bb14
Nico Hoffmann <on-github@nhoffmann.com>
2019-05-12 10:47:24 +0200

    Document $props for new User()

---
+++ site/snippets/docs/methods/user/props.php
@@ -0,0 +1,13 @@
+## The `$props` parameter
+
+Property       | Type     | Required      | Description |
+--             | --       | --            | --          |
+`blueprint`    | `array`  | <code></code> | Blueprint definition |
+`content`      | `array`  | <code></code> | Field values |
+`email`    | `string`  | <code></code> |  |
+`id`    | `string`  | <code></code> |  |
+`language`    | `string`  | <code></code> |  |
+`name`    | `string`  | <code></code> |  |
+`password`    | `string`  | <code></code> |  |
+`role`    | `string`  | <code></code> |  |
+`translations` | `array`  | <code></code> | Language codes with subarrays of field values   |
