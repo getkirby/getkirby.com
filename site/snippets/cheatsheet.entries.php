2fc8c74176525eeda95cc9937050e2f41b6d0aaf
Lukas Bestle <mail@lukasbestle.com>
2019-09-24 21:46:18 +0200

    Fix word-wrap in reference entries list in Google Chrome. Closes #404

--- site/snippets/cheatsheet.entries.php
+++ site/snippets/cheatsheet.entries.php
@@ -19,10 +19,19 @@
             </svg>
           </figure>
           <?php endif ?>
-          <span>
-            <strong><?= $entry->title() ?> <span><?= $entry->info() ?></span></strong>
+          <div>
+            <?php
+            // Add zero-width space characters between the parts of the title.
+            // This is necessary, because Google Chrome (tested with v76) does
+            // not provide a any good way to wrap the single parts of long
+            // titles (e.g. API endpoints) at places, where it makes sense.
+            // Inserting "breakpoints" before any slash allows to control
+            // the wrapping of long titles.
+            $titleSanitized = str_replace('/', '&#8203;/' /* 1 */, $entry->title());
+            ?>
+            <strong><span><?= $titleSanitized ?></span><?php if ($entry->info()->isNotEmpty()): ?> <span><?= $entry->info() ?></span><?php endif ?></strong>
             <small><?= $entry->excerpt()->kt() ?></small>
-          </span>
+          </div>
         </a>
       </li>
       <?php endforeach ?>
