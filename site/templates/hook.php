880c6837a17cc5e50f0c5bf7cd011a02d4e20a5b
Sonja Broda <info@texniq.de>
2019-10-13 15:27:59 +0200

    Modify hook template

--- site/templates/hook.php
+++ site/templates/hook.php
@@ -3,6 +3,7 @@
   <div class="text">
     <h2 id="example"><a href="#example">Example</a></h2>
     <?= $page->example()->kt() ?>
+    <?= $page->details()->kt() ?>
   </div>
 
 <?php snippet('cheatsheet.article.footer') ?>
