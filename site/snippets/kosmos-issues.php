3fee1e0b381ed33ea62d8b272940b8e603c3479d
Bastian Allgeier <mail@bastianallgeier.com>
2020-01-14 16:46:56 +0100

    Show first image for each Kosmos issue #810

--- site/snippets/kosmos-issues.php
+++ site/snippets/kosmos-issues.php
@@ -2,6 +2,13 @@
   <?php foreach($issues as $issue): ?>
     <li class="kosmos-issue">
       <a href="<?= $issue->url() ?>">
+
+        <figure>
+          <?php if ($image = $issue->image()): ?>
+          <?= $image->crop(300, 200) ?>
+          <?php endif ?>
+        </figure>
+
         <h4 class="h3 | -mb:0 -color:yellow-on-dark"><?= $issue->slug() ?></h4>
         <p class="h6 | -mb:small"><?= $issue->date('F jS, Y') ?></p>
         <?php
