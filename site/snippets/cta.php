af6c434911ac06950a6fff74c5ade5a1483df240
Bastian Allgeier <mail@bastianallgeier.com>
2020-02-12 14:43:53 +0100

    New try page

--- site/snippets/cta.php
+++ site/snippets/cta.php
@@ -1,6 +1,13 @@
 <?php
 
-$class = $class ?? '';
-$icon  = $icon ? icon($icon, true) : '';
+$link   = $link ?? '#';
+$class  = $class ?? '';
+$icon   = $icon ? icon($icon, true) : '';
+$button = $button ?? false;
 
-echo Html::a($link, [$icon, "<span class=\"cta-text\">{$text}</span>"], ['class' => trim("cta {$class}")]);
+if ($button) {
+  echo Html::button([$icon, "<span class=\"cta-text\">{$text}</span>"], ['class' => trim("cta {$class}")]);
+} else {
+  echo Html::a($link, [$icon, "<span class=\"cta-text\">{$text}</span>"], ['class' => trim("cta {$class}")]);
+}
+
--- site/templates/try.php
+++ site/templates/try.php
@@ -1,58 +1,89 @@
 <?php snippet('header') ?>
 
-  <main class="try-page | main" id="maincontent">
-    <article class="wrap">
+<main class="try-page | main" id="maincontent">
+  <article class="wrap">
 
-      <?php if ($statusType && $statusMessage): ?>
-      <div aria-labelledby="try-status-label" class="status -type:<?= $statusType ?>">
-        <?= icon(r($statusType === 'status', 'check', 'warning'), true) ?>
-        <p class="screen-reader-text" id="try-status-label"><strong><?= r($statusType === 'status', 'Status', 'Error') ?>:</strong></p>
-        <p><?= widont($statusMessage) ?></p>
+    <?php if ($statusType && $statusMessage): ?>
+    <div aria-labelledby="try-status-label" class="status -type:<?= $statusType ?>">
+      <?= icon(r($statusType === 'status', 'check', 'warning'), true) ?>
+      <p class="screen-reader-text" id="try-status-label"><strong><?= r($statusType === 'status', 'Status', 'Error') ?>:</strong></p>
+      <p><?= widont($statusMessage) ?></p>
+    </div>
+    <?php endif ?>
+
+    <header class="hero">
+      <h1>Try Kirby for free</h1>
+      <div class="intro">
+        <?= $page->intro()->kt() ?>
       </div>
-      <?php endif ?>
+    </header>
 
-      <header class="hero -align:center">
-        <h1>Try Kirby for free</h1>
-        <div class="intro">
-          <?= $page->intro()->kt() ?>
-        </div>
-      </header>
+    <div class="grid" style="--gutter: 3rem">
+      <div class="column box" style="--columns: 8">
+        <form class="demo" action="https://trykirby.com" method="POST">
+          <h2 class="h3">Personal Demo</h2>
+          <figure>
+            <button class="button-reset"><?= $page->image('interface.png')->resize(800) ?></button>
+          </figure>
 
-      <div class="links -mb:huge -align:center">
-        <?php foreach ($page->links()->toStructure() as $link) : ?>
-          <?php if ($link->action()->isNotEmpty()): ?>
-          <form action="<?= $link->action() ?>" method="POST" class="demo">
-            <button type="submit" class="cta">
-              <?= icon($link->icon(), true) ?><!--
-              --><span class="cta-text"><?= $link->text() ?></span>
-            </button>
-
-            <?= $link->description() ?>
-          </form>
-          <?php else: ?>
+          <div class="grid -mb:medium">
+            <div class="column text" style="--columns: 6">
+              <p>You are one click away from your personal demo. Give Kirby a spin and explore the Panel and our six example projects.</p>
+            </div>
+            <div class="column text" style="--columns: 6">
+              <p>Questions? Please let us know: <a href="mailto:support@getkirby.com">support@getkirby.com</a></p>
+            </div>
+          </div>
           <p>
-            <?php snippet('cta', [
-              'link' => $link->link(),
-              'text' => $link->text(),
-              'icon' => $link->icon(),
+            <?= snippet('cta', [
+              'text'   => 'Start the demo',
+              'icon'   => 'panel',
+              'button' => true
             ]) ?>
-            <?= $link->description() ?>
           </p>
-          <?php endif ?>
-        <?php endforeach ?>
+        </form>
       </div>
+      <div class="column local" style="--columns: 4">
+        <h2 class="h3">Kirby on your computer</h2>
+        <div class="text -mb:medium">
+          <p>
+            You can install Kirby on your computer or a test server and evaluate it as long as you need.<br>
+            <a href="<?= url('docs/guide/quickstart') ?>">Get up and running …</a>
+          </p>
+        </div>
 
-      <figure class="starterkit">
-        <a href="https://download.getkirby.com" class="intrinsic" style="padding-bottom: 164.5%">
-          <?= $page->image('starterkit.jpg')->resize(800)->html(['alt' => 'The home page of Kirby\'s starterkit']) ?>
-        </a>
-      </figure>
+        <div class="kit">
+          <p class="text -mb:medium">
+            <strong>Starterkit</strong><br>
+            Fully annotated example site – for everyone who wants to explore Kirby's capabilities.
+          </p>
+          <p>
+            <?= snippet('cta', [
+              'text' => 'Download',
+              'icon' => 'download',
+              'link' => 'https://github.com/getkirby/starterkit/archive/master.zip'
+            ]) ?>
+          </p>
+        </div>
 
-      <div class="text description">
-        <?= $page->text()->kt() ?>
+        <div class="kit -mb:large">
+          <p class="text -mb:medium">
+            <strong>Plainkit</strong><br>
+            Start from scratch. No templates, no content, no styles – just you, Kirby and your imagination.<br>
+          </p>
+          <p>
+            <?= snippet('cta', [
+              'text' => 'Download',
+              'icon' => 'download',
+              'link' => 'https://github.com/getkirby/plainkit/archive/master.zip'
+            ]) ?>
+          </p>
+        </div>
       </div>
+    </div>
 
-    </article>
-  </main>
+
+  </article>
+</main>
 
 <?php snippet('footer') ?>
