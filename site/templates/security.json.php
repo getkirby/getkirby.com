3a3e730ef45d54b41dbbe7c154763d2f94535b21
Lukas Bestle <mail@lukasbestle.com>
2020-06-03 20:58:54 +0200

    Add Kirby 1 status

--- site/templates/security.json.php
+++ site/templates/security.json.php
@@ -5,6 +5,7 @@ $data = [
     'supported' => [
         $supported->value() => 'actively supported',
         '2.5.13'            => 'security support until 31.12.2020'
+        '1.*'                     => 'Not supported'
     ],
     'incidents' => array_values($incidents->toArray())
 ];
