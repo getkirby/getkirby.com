848e0b81d50e9370d844fbcfa70c94239c34bd14
Nico Hoffmann <on-github@nhoffmann.com>
2020-08-20 20:40:02 +0200

    Revert falsely removing cookbook model

--- site/plugins/site/pageModels.php
+++ site/plugins/site/pageModels.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'cookbook'  => 'Kirby\\Site\\Models\\CookbookPage',
     'changelog' => 'Kirby\\Site\\Models\\ChangeLogPage',
     'guide'     => 'Kirby\\Site\\Models\\GuidePage',
     'recipe'    => 'Kirby\\Site\\Models\\RecipePage',
