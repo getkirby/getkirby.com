4ff05c89f24e3ed72923cb450a82de6e2924fa05
Bastian Allgeier <mail@bastianallgeier.com>
2020-12-09 13:36:08 +0100

    Docs for toBlocks field method

---
+++ site/snippets/docs/blocks/blocks.php
@@ -0,0 +1,64 @@
+## Rendering all blocks
+
+If you don't want to care about the HTML for each individual block, you can echo the entire blocks collection to render all blocks.
+
+```php
+<?= '<?= $page->myBlocksField()->toBlocks() ?>' ?>
+
+```
+
+## Looping through blocks
+
+Looping through blocks to control their HTML can be very powerful. You can assign custom CSS classes, IDs for links and more.
+
+### Simple
+
+You don't need to render the HTML for each individual block in the loop though. You can wrap the block with your custom HTML and then echo `$block` to render the block snippet.
+
+```php
+<?= <<<'CODE'
+<?php foreach ($page->myBlocksField()->toBlocks() as $block): ?>
+<div id="<?= $block->id() ?>" class="block block-type-<?= $block->type() ?>">
+  <?= $block ?>
+</div>
+<?php endforeach ?>
+CODE;
+?>
+
+```
+
+### Manually loading snippets
+
+Sometimes you might wish to customize the way block snippets are loaded. Maybe you want to inject more snippet variables.
+
+```php
+<?= <<<'CODE'
+<?php foreach ($page->myBlocksField()->toBlocks() as $block): ?>
+<div id="<?= $block->id() ?>" class="block block-type-<?= $block->type() ?>">
+  <?php snippet('blocks/' . $block->type(), [
+    'block' => $block,
+    'theme' => 'dark'
+  ]) ?>
+</div>
+<?php endforeach ?>
+CODE;
+?>
+
+```
+
+… or load snippets from a different location …
+
+```php
+<?= <<<'CODE'
+<?php foreach ($page->myBlocksField()->toBlocks() as $block): ?>
+<div id="<?= $block->id() ?>" class="block block-type-<?= $block->type() ?>">
+  <?php snippet('blocks/custom/' . $block->type(), [
+    'block' => $block,
+    'theme' => 'dark'
+  ]) ?>
+</div>
+<?php endforeach ?>
+CODE;
+?>
+
+```
