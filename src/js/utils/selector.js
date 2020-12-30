3d2ebdf9d347919fc53724d036bc41e88b332697
Nico Hoffmann <on-github@nhoffmann.com>
2020-12-30 13:37:15 +0100

    Build-less JS: new JS modules entry file

--- src/js/utils/selector.js
+++
@@ -1,9 +0,0 @@
-const $ = (selector) => {
-  return document.querySelector(selector);
-};
-
-const $$ = (selector) => {
-  return [].slice.call(document.querySelectorAll(selector));
-};
-
-export { $, $$ };
