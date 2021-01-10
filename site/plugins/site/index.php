b743a55728ad472af09feb684be9bf5ece840d38
Nico Hoffmann <on-github@nhoffmann.com>
2021-01-10 17:10:44 +0100

    New site plugin structure

--- site/plugins/site/index.php
+++ site/plugins/site/index.php
@@ -1,10 +1,18 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/helpers.php';
 
-Kirby::plugin('kirby/site', [
-    'fieldMethods' => include __DIR__ . '/fieldMethods.php',
-    'pageModels'   => include __DIR__ . '/pageModels.php',
-    'pagesMethods' => include __DIR__ . '/pagesMethods.php',
+require __DIR__ . '/helpers/cdn.php';
+require __DIR__ . '/helpers/html.php';
+require __DIR__ . '/helpers/reference.php';
+
+Kirby::plugin('getkirby/site', [
+    'cacheTypes'   => include __DIR__ . '/extensions/cacheTypes.php',
+    'components'   => include __DIR__ . '/extensions/components.php',
+    'fieldMethods' => include __DIR__ . '/extensions/fieldMethods.php',
+    'hooks'        => include __DIR__ . '/extensions/hooks.php',
+    'pageMethods'  => include __DIR__ . '/extensions/pageMethods.php',
+    'pagesMethods' => include __DIR__ . '/extensions/pagesMethods.php',
+    'routes'       => include __DIR__ . '/extensions/routes.php',
+    'tags'         => include __DIR__ . '/extensions/tags.php',
 ]);
