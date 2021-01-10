47eb3c4fee0e27761e0ed5cce21bdecc078c0fea
Nico Hoffmann <on-github@nhoffmann.com>
2021-01-10 17:15:23 +0100

    Move webhook route into `site` plugin

--- site/config/routes.php
+++ site/config/routes.php
@@ -2,19 +2,6 @@
 
 return [
     [
-        'pattern' => 'hooks/clean',
-        'method'  => 'GET|POST',
-        'action'  => function () {
-            $key = trim(F::read(__DIR__ . '/keys/hooks.txt'));
-
-            if (empty($key) === false && get('key') === $key) {
-                kirby()->cache('pages')->flush();
-            }
-
-            go();
-        }
-    ],
-    [
         'pattern' => 'releases/(:num)\-(:num)',
         'action'  => function ($generation, $major) {
             return go('releases/' . $generation . '.' . $major);
--- site/plugins/site/extensions/routes.php
+++ site/plugins/site/extensions/routes.php
@@ -24,4 +24,18 @@ return [
         },
     ],
 
+    /* Webhooks */
+    [
+        'pattern' => 'hooks/clean',
+        'method'  => 'GET|POST',
+        'action'  => function () {
+            $key = trim(F::read(__DIR__ . '/keys/hooks.txt'));
+
+            if (empty($key) === false && get('key') === $key) {
+                kirby()->cache('pages')->flush();
+            }
+
+            go();
+        }
+    ]
 ];
