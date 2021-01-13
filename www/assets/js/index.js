c6a3c20067d3d0224da5cc400a1d9b9eca06eb91
Nico Hoffmann <on-github@nhoffmann.com>
2021-01-13 19:52:35 +0100

    Fix search JS

--- www/assets/js/index.js
+++ www/assets/js/index.js
@@ -1,26 +1,23 @@
 import "./components/affiliates.js";
 import "./components/lazyloading.js";
-import "./components/search.js";
 
 // Code highlighting
 import Code from "./components/code.js";
 new Code();
 
-// Menu
-const menu = document.querySelector(".menu");
+// Components
+const component = async (selector, source) => {
+  const element = document.querySelector(selector);
 
-if (menu) {
-  import("./components/menu.js").then(({ default: Menu }) => new Menu(menu));
-}
+  if (element) {
+    const { default: Component } = await import(source);
+    new Component(element);
+  }
+};
 
-// Guide sidebar
-const sidebar = document.querySelector(".js-sidebar");
-
-if (sidebar) {
-  import("./components/sidebar.js").then(
-    ({ default: Sidebar }) => new Sidebar(sidebar)
-  );
-}
+component(".menu", "./components/menu.js");
+component(".js-menu-search", "./components/search.js");
+component(".js-sidebar", "./components/sidebar.js");
 
 // Tooltips
 import Tooltips from "./components/tooltips.js";
