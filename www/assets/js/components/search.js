4c6dfc6fd12ec85f1049768de15bd9fea1a6e1f9
Nico Hoffmann <on-github@nhoffmann.com>
2021-01-13 19:52:35 +0100

    Fix search JS

--- www/assets/js/components/search.js
+++ www/assets/js/components/search.js
@@ -9,146 +9,129 @@ const SEARCH_ERROR_LABEL = "SEARCH_ERROR_UXfpeDFlmye9rXkXP5wd";
 const CHEVRON_RIGHT_SVG =
   '<svg viewBox="0 0 5 11" width="5" height="11" aria-hidden="true"><path d="M2.96153846,5.41538462 L0,9.13846154 L1.35384615,10.1538462 L4.73846154,5.92307692 C4.82307692,5.75384615 4.90769231,5.58461538 4.90769231,5.41538462 C4.90769231,5.24615385 4.82307692,5.07692308 4.73846154,4.90769231 L1.35384615,0.676923077 L0,1.69230769 L2.96153846,5.41538462 Z"/></svg>';
 
-document.addEventListener("DOMContentLoaded", async function () {
-  const form = document.querySelector(".js-menu-search");
-
-  // abort initialization if there’s no search form
-  if (!form) {
-    return;
+export default class {
+  constructor(element) {
+    this.init(element);
   }
 
-  await import("../libraries/algoliasearch.js");
-  await import("../libraries/awesomplete.js");
+  async init(element) {
+    this.$el = element;
 
-  const input = document.querySelector(".js-menu-search-input");
-  const client = algoliasearch(ALGOLIA_APP, ALGOLIA_KEY);
-  const index = client.initIndex(ALGOLIA_INDEX);
+    await import("../libraries/algoliasearch.js");
+    await import("../libraries/awesomplete.js");
 
-  let lastValue = "";
-  let list = [];
+    this.input = this.$el.querySelector(".js-menu-search-input");
+    this.client = algoliasearch(ALGOLIA_APP, ALGOLIA_KEY);
+    this.index = this.client.initIndex(ALGOLIA_INDEX);
+    this.recent = "";
+    this.list = [];
+    this.awesome = new Awesomplete(this.input, {
+      list: [],
+      filter: () => true,
+      sort: false,
+      item: this.item,
+    });
 
-  const awesome = new Awesomplete(input, {
-    list: [],
-    filter: () => true,
-    sort: false,
-    item: (text /*, input */) => {
-      const item = document.createElement("li");
+    this.input.addEventListener("focus", this.onFocus.bind(this));
+    this.input.addEventListener("blur", this.onBlur.bind(this));
+    this.input.addEventListener("keypress", this.onKeypress.bind(this));
+    this.input.addEventListener("keyup", this.onKeyup.bind(this), 250);
+    this.input.addEventListener("awesomplete-select", this.onSelect.bind(this));
+  }
 
-      if (text.label === VIEW_ALL_RESULTS_LABEL) {
-        // view all link
-        item.classList.add("menu-search-view-all");
-        item.innerHTML = `<strong>View all results</strong>${CHEVRON_RIGHT_SVG}`;
-      } else if (text.label === SEARCH_ERROR_LABEL) {
-        // error message link
-        item.classList.add("menu-search-error");
-        item.innerHTML = `<strong>Sorry, an error occured. Please try advanced search instead.</strong>${CHEVRON_RIGHT_SVG}`;
-      } else {
-        // regular result
-        item.innerHTML = `<strong>${text.label}</strong> <small>${text.value}</small>`;
-      }
+  item(text) {
+    const item = document.createElement("li");
 
-      return item;
-    },
-  });
+    if (text.label === VIEW_ALL_RESULTS_LABEL) {
+      // view all link
+      item.classList.add("menu-search-view-all");
+      item.innerHTML = `<strong>View all results</strong>${CHEVRON_RIGHT_SVG}`;
+    } else if (text.label === SEARCH_ERROR_LABEL) {
+      // error message link
+      item.classList.add("menu-search-error");
+      item.innerHTML = `<strong>Sorry, an error occured. Please try advanced search instead.</strong>${CHEVRON_RIGHT_SVG}`;
+    } else {
+      // regular result
+      item.innerHTML = `<strong>${text.label}</strong> <small>${text.value}</small>`;
+    }
 
-  input.addEventListener("focus", () => {
+    return item;
+  }
+
+  onBlur() {
+    document.documentElement.classList.remove("is-menu-search-open");
+  }
+
+  onFocus() {
     // helper class used by the reference templates’ CSS
     document.documentElement.classList.add("is-menu-search-open");
-  });
+  }
 
-  input.addEventListener("blur", () => {
-    document.documentElement.classList.remove("is-menu-search-open");
-  });
-
-  input.addEventListener("keypress", (e) => {
+  onKeypress(e) {
     if ((e.key && e.key === "Enter") || e.keyCode === 13) {
-      form.submit();
+      this.$el.submit();
     }
-  });
+  }
 
-  input.addEventListener(
-    "keyup",
-    () => {
-      const value = input.value.trim();
+  async onKeyup() {
+    const value = this.input.value.trim();
 
-      if (value === lastValue) {
-        return true;
-      } else if (value === "") {
-        lastValue = "";
-        list = [];
-        awesome.list = list;
-        awesome.evaluate();
-        return true;
-      }
+    if (value === this.recent) {
+      return true;
+    }
 
-      lastValue = value;
+    this.recent = value;
+    this.list = [];
 
-      if (value.length <= 2) {
-        // don't search for very short words
-        return true;
-      }
+    if (value === "") {
+      this.awesome.list = this.list;
+      this.awesome.evaluate();
+      return true;
+    }
 
-      setTimeout(function () {
-        const params = {
-          hitsPerPage: 5,
-        };
+    // don't search for very short words
+    if (value.length <= 2) {
+      return true;
+    }
 
-        const filters = input.getAttribute("data-filters");
+    const params = {
+      hitsPerPage: 5,
+    };
 
-        if (filters) {
-          params.filters = filters;
-        }
+    const filters = this.input.getAttribute("data-filters");
 
-        /* eslint-disable handle-callback-err */
-        index.search(value, params, (err, content) => {
-          list = [];
+    if (filters) {
+      params.filters = filters;
+    }
 
-          if (err) {
-            /* eslint-disable no-console */
-            if ("console" in window) {
-              console.error("Quicksearch error", err);
-            }
-            /* eslint-enable no-console */
+    const { hits, nbHits, hitsPerPage } = await this.index.search(
+      value,
+      params
+    );
 
-            list.push({
-              label: SEARCH_ERROR_LABEL,
-              value: 0,
-            });
-
-            awesome.list = list;
-            awesome.evaluate();
-            awesome.open();
-
-            return;
-          }
-
-          for (var i = 0, l = content.hits.length; i < l; i++) {
-            const item = content.hits[i];
-            list.push({
-              label: item.title,
-              value: item.objectID,
-            });
-          }
-
-          if (content.nbHits > 0 && content.nbHits > content.hitsPerPage) {
-            // Show View all item if there are any hits and
-            // there are more hits than those displayed in the popup.
-            list.push({
-              label: VIEW_ALL_RESULTS_LABEL,
-              value: content.nbHits,
-            });
-          }
-
-          awesome.list = list;
-          awesome.evaluate();
-          awesome.open();
-        });
+    for (var i = 0, l = hits.length; i < l; i++) {
+      const item = hits[i];
+      this.list.push({
+        label: item.title,
+        value: item.objectID,
       });
-    },
-    250
-  );
+    }
 
-  input.addEventListener("awesomplete-select", (e /*, item */) => {
+    // Show View all item if there are any hits and
+    // there are more hits than those displayed in the popup.
+    if (nbHits > 0 && nbHits > hitsPerPage) {
+      this.list.push({
+        label: VIEW_ALL_RESULTS_LABEL,
+        value: nbHits,
+      });
+    }
+
+    this.awesome.list = this.list;
+    this.awesome.evaluate();
+    this.awesome.open();
+  }
+
+  onSelect(e) {
     e.preventDefault();
 
     // When the "view all results" or "error" entry was clicked/selected,
@@ -157,11 +140,11 @@ document.addEventListener("DOMContentLoaded", async function () {
       e.text.label === VIEW_ALL_RESULTS_LABEL ||
       e.text.label === SEARCH_ERROR_LABEL
     ) {
-      form.submit();
+      this.$el.submit();
 
       // Regular search result selected.
     } else {
       window.location.href = `/${e.text.value}`;
     }
-  });
-});
+  }
+}
