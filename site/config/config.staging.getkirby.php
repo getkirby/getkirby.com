4b8f5ebc3dfcd32b3977e6b31fd5f5a2be75cbac
Nico Hoffmann <on-github@nhoffmann.com>
2021-01-25 20:38:16 +0100

    Add config for staging server

---
+++ site/config/config.staging.getkirby.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'debug' => true,
+    'cache' => [
+        'pages' => [
+            'active' => true,
+            'type'   => 'file'
+        ],
+        'plugins' => [
+            'active' => true,
+            'type'   => 'file'
+        ]
+    ],
+    'keycdn' => [
+        'domain' => 'https://staginggetkirby-1653.kxcdn.com/',
+    ],
+    'referenceLookup' => true
+];
