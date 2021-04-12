7565e470ec0b7f4c3d471c4c11bf173a7d10910e
Nico Hoffmann <on-github@nhoffmann.com>
2021-04-12 12:59:54 +0200

    `reference-block`: file paths as comments

--- site/templates/reference-block.php
+++ site/templates/reference-block.php
@@ -10,17 +10,19 @@
 
   <h3 id="default-files__snippet"><a href="#default-files__snippet">Snippet</a></h3>
   <?= kt('
-```php "/site/snippets/blocks/' . $page->slug() . '.php"
+```php
 ' . file_get_contents(kirby()->root('kirby') . '/config/blocks/' . $page->slug() . '/' . $page->slug() . '.php') .'
 ```    
   ') ?>
+  <p>To overwrite this default snippet, place your custom file in <code>/site/snippets/blocks/<?= $page->slug() ?>.php</code>.</p>
 
   <h3 id="default-files__blueprint"><a href="#default-files__blueprint">Blueprint</a></h3>
   <?= kt('
-```yaml "/site/blueprints/blocks/' . $page->slug() . '.yml"
+```yaml
 ' . file_get_contents(kirby()->root('kirby') . '/config/blocks/' . $page->slug() . '/' . $page->slug() . '.yml') .'
 ```    
   ') ?>
+    <p>To overwrite this default blueprint, place your custom file in <code>/site/blueprints/blocks/<?= $page->slug() ?>.yml</code>.</p>
 
   <h3 id="default-files__vue-component"><a href="#default-files__vue-component">Vue component</a></h3>
   <p><a href="https://github.com/getkirby/kirby/blob/master/panel/src/components/Blocks/Types/<?= ucfirst($page->title()) ?>.vue">kirby/blob/master/panel/src/components/Blocks/Types/<?= ucfirst($page->title()) ?>.vue</a></p>
