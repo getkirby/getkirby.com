f7d5e7943e4cd1467612658de52dbc364928ec89
Nico Hoffmann <on-github@nhoffmann.com>
2021-04-15 11:06:36 +0200

    Simple template switcher

--- site/snippets/templates/features-for-developers/templating-figure.php
+++ site/snippets/templates/features-for-developers/templating-figure.php
@@ -1,6 +1,24 @@
-<div id="php" class="mb-6">
+<style>
+.template-select {
+  background: none;
+}
+</style>
+
+
+<select class="template-select mb-3">
+  <option value="php" selected>PHP template</option>
+  <option value="twig">Twig template</option>
+</select>
+
+<div id="php" data-template>
   <?= $page->phpTemplate()->kt() ?>
 </div>
-<div id="twig">
+<div id="twig" data-template class="hidden">
   <?= $page->twigTemplate()->kt() ?>
 </div>
+
+<script>
+document.querySelector(".template-select").addEventListener("change", e => {
+  [...document.querySelectorAll("[data-template]")].forEach(x => x.classList.toggle("hidden"));
+})
+</script>
