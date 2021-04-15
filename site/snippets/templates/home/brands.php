f1b96b6dd0ac764cd82983542094b336c36e7665
Bastian Allgeier <mail@bastianallgeier.com>
2021-04-15 14:26:09 +0200

    Update brand logos

--- site/snippets/templates/home/brands.php
+++ site/snippets/templates/home/brands.php
@@ -2,7 +2,7 @@
   <h2 class="h2 mb-12">Trusted by brands world&#8209;wide</h2>
   <ul class="brands items-center">
     <?php foreach(page('home/clients')->children()->listed()->shuffle() as $client): ?>
-    <li><?= $client->image()->read() ?></li>
+    <li class="<?= $client->slug() ?>"><?= $client->image()->read() ?></li>
     <?php endforeach ?>
   </ul>
 </section>
