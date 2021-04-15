8e78d3cedda0d938a35622d6b77c03a1f65861d0
Bastian Allgeier <mail@bastianallgeier.com>
2021-04-15 10:51:15 +0200

    Responsive images for the developer page

--- site/snippets/templates/features-for-developers/plugins-figure.php
+++ site/snippets/templates/features-for-developers/plugins-figure.php
@@ -1,3 +1,22 @@
+<?php if ($image = image('matomo.jpg')): ?>
 <figure>
-  <?= image('matomo.jpg') ?>
+  <a class="block bg-light mb-3" href="/plugins" style="--aspect-ratio: <?= $image->width() . '/' . $image->height() ?>">
+    <?= img($image, [
+      'src' => [
+        'width' => 1000,
+      ],
+      'srcset' => [
+        '1x' => [
+          'width' => 1000
+        ],
+        '2x' => [
+          'width' => 2000
+        ],
+      ]
+    ]) ?>
+  </a>
+  <figcaption class="font-mono text-xs color-gray-700">
+    <a href="/plugins/sylvainjule/matomo"><strong class="font-normal color-black link">Matomo Plugin</strong> by Sylvain Julé</a>
+  </figcaption>
 </figure>
+<?php endif ?>
