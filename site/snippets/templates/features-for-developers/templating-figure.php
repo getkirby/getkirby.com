9fab63f4c0eb6d523c155c14d334d5b4d270970a
Bastian Allgeier <mail@bastianallgeier.com>
2021-04-15 11:43:09 +0200

    Fix template switcher on back button and add caption

--- site/snippets/templates/features-for-developers/templating-figure.php
+++ site/snippets/templates/features-for-developers/templating-figure.php
@@ -4,7 +4,6 @@
 }
 </style>
 
-
 <select class="template-select mb-3">
   <option value="php" selected>PHP template</option>
   <option value="twig">Twig template</option>
@@ -14,11 +13,28 @@
   <?= $page->phpTemplate()->kt() ?>
 </div>
 <div id="twig" data-template class="hidden">
-  <?= $page->twigTemplate()->kt() ?>
+  <div class="mb-3">
+    <?= $page->twigTemplate()->kt() ?>
+  </div>
+  <div class="font-mono text-xs color-gray-700">
+    <a href="/plugins/mgfagency/twig"><strong class="font-normal color-black link">Kirby Twig plugin</strong> by Christian Zehetner</a>
+  </div>
 </div>
 
 <script>
-document.querySelector(".template-select").addEventListener("change", e => {
-  [...document.querySelectorAll("[data-template]")].forEach(x => x.classList.toggle("hidden"));
-})
+const select    = document.querySelector(".template-select");
+const templates = [...document.querySelectorAll("[data-template]")];
+
+const changeTemplate = () => {
+  templates.forEach(x => x.classList.add("hidden"));
+  document.querySelector("#" + select.value).classList.remove("hidden");
+}
+
+window.addEventListener("load", () => {
+  changeTemplate();
+});
+
+select.addEventListener("change", () => {
+  changeTemplate();
+});
 </script>
