96dfef5960cc5dab14a065363084ee56166c0f35
Bastian Allgeier <mail@bastianallgeier.com>
2021-04-15 09:50:07 +0200

    Responsive issue preview

--- site/snippets/templates/kosmos/issue.php
+++ site/snippets/templates/kosmos/issue.php
@@ -2,9 +2,16 @@
   <a class="bg-black block leading-tight" href="<?= $issue->url() ?>">
     <figure>
       <p class="bg-black" style="--aspect-ratio: 16/9">
-        <?php if ($image = $issue->image()): ?>
-        <img src="<?= $image->resize(500)->url() ?>" loading="lazy">
-        <?php endif ?>
+        <?= img($issue->image(), [
+          'alt' => 'A preview of ' . $issue->title(),
+          'src' => [
+            'width' => 450
+          ],
+          'srcset' => [
+            '1x' => 450,
+            '2x' => 900,
+          ]
+        ]) ?>
       </p>
       <figcaption class="p-3 color-white">
         <h3 class="font-bold mb-1">Episode <?= $issue->slug() ?></h3>
