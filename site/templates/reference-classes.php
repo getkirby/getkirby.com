83feff0e9201e129d72756d6ce51fa152007a311
Nico Hoffmann <on-github@nhoffmann.com>
2021-04-18 14:03:56 +0200

    `reference-packages` → `reference-classes`

--- site/templates/reference-classes.php
+++ site/templates/reference-classes.php
@@ -1,6 +1,6 @@
 <?php layout('reference') ?>
 
-<?php foreach ($page->children()->listed() as $package): ?>
+<?php foreach ($page->children()->unlisted() as $package): ?>
 <div class="mb-24">
   <h2 class="h2 mb-3" id="<?= $package->slug() ?>"><?= $package->title() ?></h2>
   <?php snippet('templates/reference/section', $package->children()->listed()) ?>
