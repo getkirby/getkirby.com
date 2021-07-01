dd670ffce98246ce67a2b05d3d82057a2242984f
Sonja Broda <info@texniq.de>
2021-07-01 20:00:21 +0200

    Some fixes and rephrasing

--- site/snippets/templates/release-36/plugins.php
+++ site/snippets/templates/release-36/plugins.php
@@ -2,25 +2,25 @@
   'id'    => 'plugins',
   'title' => 'Panel areas',
   'intro' => 'The plugin API you’ve been dreaming of',
-  'text'  => 'Our new Fiber architecture splits the Panel in areas. Site area, users area, settings area, etc. You can now create your own areas and build entire applications on top of the Panel. <a href="/releases/3.6/features/fiber">Learn more &rsaquo;</a>',
+  'text'  => 'Our new Fiber architecture splits the Panel in areas: site area, users area, settings area, etc. You can create your own areas and build entire applications on top of the Panel. <a href="/releases/3.6/features/fiber">Learn more &rsaquo;</a>',
   'figure' => 'templates/release-36/plugins-figure',
   'reverse' => true,
   'features' => [
     [
       'title' => 'Custom routes',
-      'text' => 'Building custom views with your own routes is now as simple as routing for your Kirby site. Define props for your view components and no longer worry about API endpoints, state management or any other complex stuff. Just start building.'
+      'text' => 'Building custom views with your own routes is now as simple as routing for your Kirby site. Define props for your view components and don\'t worry about API endpoints, state management or any other complex stuff. Just start building.'
     ],
     [
       'title' => 'Your own dialogs in seconds',
-      'text' => 'Dialogs can now also be defined in PHP and opened with a simple JS call `$dialog("my-dialog")`. Create form dialogs, text dialogs or confirmation dialogs to delete entries. It’s just so much fun.'
+      'text' => 'Dialogs can now also be defined in PHP and opened with a simple JS call `$dialog("my-dialog")`. Create form dialogs, text dialogs or confirmation dialogs to delete entries. It’s so much fun.'
     ],
     [
       'title' => 'Access control made simple',
-      'text' => 'You want your own permissions for your shiny new Panel area? No problem. It’s automatically handled for you. Deny access for individual roles and the Panel will kick users out if they are not allowed in.'
+      'text' => 'You want your own permissions for your shiny new Panel area? No problem. It’s automatically handled for you. Deny access for individual roles and the Panel will lock users out that are not allowed in.'
     ],
     [
       'title' => 'No more API endpoint hassle',
-      'text' => 'With Fiber, you can return exactly the kind of data you need for your Panel components. No multiple API calls or custom API routes. Write some PHP and be done.'
+      'text' => 'With Fiber, you return exactly the kind of data you need for your Panel components. No more multiple API calls or custom API routes. Write some PHP and be done.'
     ],
   ]
 ]);
