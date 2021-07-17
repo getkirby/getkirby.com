36dd5891f0f966ddd9968c46b95b2d85e2a5d61e
Nico Hoffmann <on-github@nhoffmann.com>
2021-07-17 12:59:47 +0200

    Temporary icon for public ui components

--- site/snippets/templates/reference/entry/icon.php
+++ site/snippets/templates/reference/entry/icon.php
@@ -1,7 +1,19 @@
-<?php if ($entry->intendedTemplate()->name() === 'reference-icon'): ?>
+<?php
+$icon = null;
+
+if ($entry->intendedTemplate()->name() === 'reference-icon') {
+  $icon = $entry->slug();
+}
+
+if ($entry->intendedTemplate()->name() === 'reference-ui') {
+  $icon = $entry->isPublic() ? 'check' : 'none';
+}
+?>
+
+<?php if ($icon): ?>
 <figure class="p-3 mr-3 bg-light rounded">
   <svg style="width: 1rem; height: 1rem;">
-    <use xlink:href="#icon-<?= $entry->slug() ?>" />
+    <use xlink:href="#icon-<?= $icon ?>" />
   </svg>
 </figure>
 <?php endif ?>
