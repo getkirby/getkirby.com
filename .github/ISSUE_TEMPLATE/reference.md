e95912a9defa26b6af87c627aadf2c8959e2e8d4
Nico Hoffmann <on-github@nhoffmann.com>
2021-08-07 17:28:06 +0200

    GitHub edit link for non-existing content files
    Fixes #1425

---
+++ .github/ISSUE_TEMPLATE/reference.md
@@ -0,0 +1,16 @@
+---
+name: Reference improvements
+about: Let us know about necessary changes
+
+---
+
+<!-- So far, this Reference page is generated fully automatically. Please let us know about the problem you have and we will make sure to fix it. -->
+
+**Issue with Reference**  
+<!--  Let us know where and what is wrong -->
+
+**Expected**  
+<!-- A clear and concise description of what you expected to happen. -->
+
+**Screenshots**  
+<!-- If applicable, add screenshots to help explain your problem. -->
--- site/snippets/layouts/github-edit.php
+++ site/snippets/layouts/github-edit.php
@@ -4,7 +4,6 @@
   Did you find an error? Help us improve our docs and edit this page on Github. Make sure to check out
   <a href="/styleguide">our styleguide &rarr;</a>
 </p>
-
-<a href="<?= option('github.url') ?>/getkirby.com/edit/main/content/<?= $page->diruri() ?>/<?= $page->intendedTemplate() ?>.txt" class="btn btn--outlined mb-3">
+<a href="<?= option('github.url') ?>/getkirby.com/<?= F::exists($page->contentFile()) ? 'edit/main/content/' . $page->diruri() . '/' . $page->intendedTemplate() . '.txt' : 'issues/new?template=reference.md' ?>" class="btn btn--outlined mb-3">
   <?= icon('github') ?> Edit this page
 </a>
