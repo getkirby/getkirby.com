db141c1244cd1d37e061cc947f37b1e4f3dac4d3
Bastian Allgeier <mail@bastianallgeier.com>
2021-10-05 09:57:30 +0200

    Sale

--- site/config/banners.php
+++ site/config/banners.php
@@ -2,8 +2,8 @@
 
 return [
     'sale' => [
-        'text'      => '🛍 &nbsp; <strong>Save 20%</strong> until July 9th',
-        'startDate' => '2021-07-02',
-        'endDate'   => '2021-07-09',
+        'text'      => '🛍 &nbsp; <strong>Save 20%</strong> until October 12th',
+        'startDate' => '2021-10-05',
+        'endDate'   => '2021-10-12',
     ]
 ];
