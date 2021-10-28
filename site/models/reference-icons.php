ba1eae4ff863db866fcff095e94e886f9c4a8072
Nico Hoffmann <me@nhoffmann.com>
2021-10-28 19:18:08 +0200

    Reference: new icons

--- site/models/reference-icons.php
+++ site/models/reference-icons.php
@@ -5,13 +5,14 @@ use Kirby\Reference\SectionPage;
 
 class ReferenceIconsPage extends SectionPage
 {
-    
+
     public function children(): Pages
     {
         if ($this->children !== null) {
             return $this->children;
         }
 
+        $pages    = parent::children();
         $children = [];
         $root     = $this->kirby()->root('panel') . '/dist/img/icons.svg';
         $file     = F::read($root);
@@ -19,19 +20,26 @@ class ReferenceIconsPage extends SectionPage
 
         foreach ($svg->defs->children() as $symbol) {
             $slug = str_replace('icon-', '', $symbol->attributes()->id);
+
+            if ($page = $pages->find($slug)) {
+                $content = $page->content()->toArray();
+            } else {
+                $content = [];
+            }
+
             $children[] = [
                 'slug'     => $slug,
                 'template' => 'reference-icon',
                 'model'    => 'reference-icon',
                 'num'      => 0,
-                'content'  => [
+                'content'  => array_merge([
                     'intro' => '&lt;k-icon type="' . $slug . '"&gt;'
-                ]
+                ], $content)
             ];
         }
 
         return $this->children = Pages::factory($children, $this)
             ->sortBy('slug');
     }
-    
+
 }
