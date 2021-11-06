285c2ad0f4678234ff29a2b19dc753dc27391dae
Nico Hoffmann <me@nhoffmann.com>
2021-11-06 13:56:55 +0100

    `<since>` highlighting new in current version

--- site/snippets/kirbytext/since.php
+++ site/snippets/kirbytext/since.php
@@ -1,5 +1,9 @@
-<summary>
-  Since <?= version($version) ?>
+<?php
+$current = version_compare($version, $kirby->version(), '>=');
+?>
+
+<summary <?php e($current, 'class="new"') ?>>
+  <?= $current ? 'New in' : 'Since' ?> <?= version($version) ?>
 </summary>
 <div>
   <?= kirbytext($text) ?>
