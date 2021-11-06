a1f8f6a33d20f24252f9e0ab2231b555cbbec607
Nico Hoffmann <me@nhoffmann.com>
2021-11-06 14:12:25 +0100

    Logo: gray underline for archived versions

--- site/snippets/layouts/logo.php
+++ site/snippets/layouts/logo.php
@@ -3,7 +3,7 @@
 </a>
 
 <?php if ($page->parents()->has(page('docs')) === true): ?>
-<p>
+<p class="version <?php e(option('archived') === true, 'archived') ?>">
   <a class="font-bold" href="https://github.com/getkirby/kirby/releases/<?= $kirby->version() ?>">
     Kirby <span class="link tabular-nums"><?= $kirby->version() ?></span>
   </a>
