a6024a1ca7c04632ef44ca6e374028dd3b395035
Bastian Allgeier <mail@bastianallgeier.com>
2021-11-14 20:50:56 +0100

    Sections

--- site/snippets/templates/release-36/features-planned.php
+++
@@ -1,7 +0,0 @@
-<div class="pt-6">
-  <?php foreach ($page->children()->unlisted() as $feature): ?>
-  <a class="block" href="<?= $feature->url() ?>">
-    <input type="checkbox" class="mr-3"><?= $feature->title() ?>
-  </a>
-  <?php endforeach ?>
-</div>
--- site/snippets/templates/release-36/features.php
+++ site/snippets/templates/release-36/features.php
@@ -1,6 +1,6 @@
-<div class="columns pt-6" style="--columns: 4; --gap: .75rem">
+<div class="columns pt-6" style="--columns: 4; --gap: var(--spacing-2)">
   <?php foreach ($page->children()->listed() as $feature): ?>
-  <a class="block" href="<?= $feature->url() ?>">
+  <a class="flex items-center bg-white p-3 shadow text-sm" href="<?= $feature->url() ?>">
     <input type="checkbox" class="mr-3" checked><?= $feature->title() ?>
   </a>
   <?php endforeach ?>
