9d1fb704d9ad31933295b976b513aca2dc0837b4
Bastian Allgeier <mail@bastianallgeier.com>
2021-11-15 22:15:16 +0100

    Banner

--- site/snippets/layouts/header.php
+++ site/snippets/layouts/header.php
@@ -1,3 +1,12 @@
+<div class="mb-6">
+  <div class="container">
+    <a href="/releases/3.6" class="font-bold py-3 block flex justify-between" style="border-bottom: 2px solid var(--color-black)">
+      <span><span class="mr-3">🚀</span> 3.6.0 is here!</span>
+      <span>See what’s new &rsaquo;</span>
+    </a>
+  </div>
+</div>
+
 <header class="header mb-24">
   <?php snippet('layouts/skipper') ?>
   <div class="container">
@@ -6,8 +15,8 @@
       <?php snippet('layouts/menu') ?>
       <?php snippet('layouts/search', ['area' => $search ?? 'all']) ?>
 
-      <?php if ($page->id() !== 'buy'): ?>
-      <?php snippet('layouts/banner', ['banner' => banner()]) ?>
+      <?php if ($page->id() !== 'buy') : ?>
+        <?php snippet('layouts/banner', ['banner' => banner()]) ?>
       <?php endif ?>
     </div>
   </div>
--- site/snippets/layouts/logo.php
+++ site/snippets/layouts/logo.php
@@ -2,7 +2,7 @@
   <?= icon('icon') ?>
 </a>
 
-<?php if ($page->parents()->has(page('docs')) === true): ?>
+<?php if ($page->is('docs/reference') || $page->parents()->has('docs/reference') === true): ?>
 <p class="version <?php e(option('archived') === true, 'archived') ?>">
   <a class="font-bold" href="https://github.com/getkirby/kirby/releases/<?= $kirby->version() ?>">
     Kirby <span class="link tabular-nums"><?= $kirby->version() ?></span>
