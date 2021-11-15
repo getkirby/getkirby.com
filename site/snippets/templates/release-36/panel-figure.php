7036dd1d204d331052cc800d9f95cd2301d83773
Bastian Allgeier <mail@bastianallgeier.com>
2021-11-15 15:44:59 +0100

    Release page updates

---
+++ site/snippets/templates/release-36/panel-figure.php
@@ -0,0 +1,6 @@
+<figure style="--aspect-ratio: 878/810">
+  <video class="rounded shadow-xl" width="100%" autoplay muted playsinline loop>
+    <source src="<?= $page->file('copy-and-paste.mp4')->url() ?>" type="video/mp4">
+    <source src="<?= $page->file('copy-and-paste.webm')->url() ?>" type="video/webm">
+  </video>
+</figure>
