a5af58c010c2054c7860b9224bc45417e33763aa
Bastian Allgeier <mail@bastianallgeier.com>
2021-11-18 14:29:56 +0100

    Don’t index any pages of archived docs

--- site/snippets/layouts/head.php
+++ site/snippets/layouts/head.php
@@ -6,6 +6,7 @@ extract([
 
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="robots" content="noindex">
 
 <link rel="icon" type="image/png" href="<?= url('/assets/images/favicon.png') ?>">
 <link rel="mask-icon" href="<?= url('/assets/images/safari-mask-icon.svg') ?>" color="#000">
@@ -20,18 +21,18 @@ extract([
 <link rel="modulepreload" href="<?= url('/assets/js/components/menu.js') ?>">
 <link rel="modulepreload" href="<?= url('/assets/js/components/search.js') ?>">
 
-<?php if (option('cdn', false) !== false): ?>
-<link rel="dns-prefetch" href="<?= option('cdn.domain') ?>">
-<link rel="preconnect" href="<?= option('cdn.domain') ?>">
+<?php if (option('cdn', false) !== false) : ?>
+  <link rel="dns-prefetch" href="<?= option('cdn.domain') ?>">
+  <link rel="preconnect" href="<?= option('cdn.domain') ?>">
 <?php endif ?>
 
-<?php if ($page->template()->name() === 'buy'): ?>
-<link rel="preconnect" href="https://checkout.paddle.com">
-<link rel="preconnect" href="https://cdn.paddle.com">
-<link rel="preload" href="https://checkout.paddle.com/api/2.0/prices?product_ids=499826&callback=paddle_price" as="script">
-<link rel="preload" href="https://cdn.paddle.com/paddle/paddle.js" as="script">
-<?php else: ?>
-<link rel="dns-prefetch" href="https://cdn.paddle.com">
+<?php if ($page->template()->name() === 'buy') : ?>
+  <link rel="preconnect" href="https://checkout.paddle.com">
+  <link rel="preconnect" href="https://cdn.paddle.com">
+  <link rel="preload" href="https://checkout.paddle.com/api/2.0/prices?product_ids=499826&callback=paddle_price" as="script">
+  <link rel="preload" href="https://cdn.paddle.com/paddle/paddle.js" as="script">
+<?php else : ?>
+  <link rel="dns-prefetch" href="https://cdn.paddle.com">
 <?php endif ?>
 
 <link href="<?= url('releases.rss') ?>" rel="alternate" type="application/rss+xml" title="Kirby Releases" />
@@ -45,25 +46,27 @@ extract([
 <?= css('assets/css/index.css') ?>
 
 <script type="module">
-window.debounce = (callback, delay) => {
-  let timeout;
-  return () => {
+  window.debounce = (callback, delay) => {
+    let timeout;
+    return () => {
       clearTimeout(timeout);
       timeout = setTimeout(callback, delay);
+    }
   }
-}
 
-import "<?= url('/assets/js/polyfills/dialog.js') ?>";
+  import "<?= url('/assets/js/polyfills/dialog.js') ?>";
 
-import { Affiliates } from "<?= url('/assets/js/components/paddle.js') ?>";
-import Code from "<?= url('/assets/js/components/code.js') ?>";
-import Lightbox from "<?= url('/assets/js/components/lightbox.js') ?>";
-import Menu from "<?= url('/assets/js/components/menu.js') ?>";
-import Search from "<?= url('/assets/js/components/search.js') ?>";
+  import {
+    Affiliates
+  } from "<?= url('/assets/js/components/paddle.js') ?>";
+  import Code from "<?= url('/assets/js/components/code.js') ?>";
+  import Lightbox from "<?= url('/assets/js/components/lightbox.js') ?>";
+  import Menu from "<?= url('/assets/js/components/menu.js') ?>";
+  import Search from "<?= url('/assets/js/components/search.js') ?>";
 
-new Affiliates();
-new Code();
-new Lightbox();
-new Menu();
-new Search();
+  new Affiliates();
+  new Code();
+  new Lightbox();
+  new Menu();
+  new Search();
 </script>
