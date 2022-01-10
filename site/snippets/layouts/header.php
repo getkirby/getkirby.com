892e0d96c1b814248814a707df0e573be11f5a53
Bastian Allgeier <mail@bastianallgeier.com>
2022-01-10 14:08:57 +0100

    Fix link

--- site/snippets/layouts/header.php
+++ site/snippets/layouts/header.php
@@ -1,6 +1,6 @@
 <div class="mb-6">
   <div class="container">
-    <a href="/releases/3.6" class="font-bold py-3 block flex justify-between" style="border-bottom: 2px solid var(--color-black)">
+    <a href="/10" class="font-bold py-3 block flex justify-between" style="border-bottom: 2px solid var(--color-black)">
       <span><span class="mr-3">🎉</span> 10 years since 1.0!</span>
 
       <?php if ($page->is('10')) : ?>
