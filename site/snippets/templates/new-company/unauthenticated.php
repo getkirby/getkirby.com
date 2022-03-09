2c5ce0e4eef50ecf0a2a020f74b4d3c2a541b6f0
Bastian Allgeier <mail@bastianallgeier.com>
2022-03-09 11:27:09 +0100

    Clearer layout for unauthenticated users

--- site/snippets/templates/new-company/unauthenticated.php
+++ site/snippets/templates/new-company/unauthenticated.php
@@ -1,41 +1,30 @@
 <div id="form" class="columns" style="--columns: 2; --gap: var(--spacing-1)">
-  <form method="POST" action="<?= option('hub.url') . '/login' ?>" class="highlight bg-light flex flex-column justify-between">
+  <form method="GET" action="<?= option('hub.url') . '/login' ?>" class="highlight bg-white shadow-xl flex flex-column justify-between">
     <header>
       <h2 class="h2 mb-6">Customers</h2>
-      <div class="prose text-base mb-12">
+      <div class="prose mb-12">
         <p>
-          If you are an existing customer, you can find a magic approval button in the email we sent you.
-          If you didn't get the email, you can log in with your email address and an order ID to give your approval.
+          <mark>If you are an existing customer, you can find a magic approval button in the email we sent you.</mark>
+        </p>
+        <p>
+          If you didn't get the email, you can log in to our brand new customer hub for approval.
         </p>
       </div>
     </header>
     <footer>
-      <div class="columns mb-12" style="--columns: 2">
-        <p>
-          <label for="email" class="color-black font-bold block mb-3">Email address</label>
-          <input id="email" type="email" name="email" required class="input">
-        </p>
-        <p>
-          <label for="order" class="color-black font-bold block mb-3">Order ID</label>
-          <input id="order" type="password" name="order" required class="input">
-        </p>
-      </div>
       <input type="hidden" name="redirect" value="true">
-      <button type="submit" class="btn btn--filled w-100%"><?= icon('key') ?> Login</button>
+      <button type="submit" class="btn btn--filled"><?= icon('key') ?> Login</button>
     </footer>
   </form>
-  <form method="POST" action="#form" class="highlight bg-light flex flex-column justify-between">
+  <form method="POST" action="https://newsletter.getkirby.com/app/sub/" class="highlight bg-light flex flex-column justify-between">
     <header>
-      <h2 class="h2 mb-6">Newsletter subscribers</h2>
-      <div class="prose text-base mb-12">
-        <p>
-          As a <a class="link" href="<?= url('kosmos') ?>">newsletter subscriber</a> you have also received an email with a magic approval button to confirm your subscription.
-          If you didn't get the email, you can (re)subscribe here.
-        </p>
+      <h2 class="h2 mb-6">Newsletter</h2>
+      <div class="prose mb-12">
+        <p>Sonja will keep providing Kirby community highlights and the latest web development news in our monthly <a class="link whitespace-nowrap" href="<?= url('kosmos') ?>">Kosmos newsletter</a>. Add yourself to the list if you are not subscribed yet.</p>
       </div>
     </header>
     <footer>
-      <div class="columns mb-12" style="--columns: 2">
+      <div class="columns mb-3" style="--columns-md: 1; --columns: 2">
         <p>
           <label for="email" class="color-black font-bold block mb-3">Email address</label>
           <input id="email" type="email" name="email" required class="input">
@@ -45,7 +34,11 @@
           <input id="name" type="name" name="name" class="input">
         </p>
       </div>
-      <button type="submit" class="btn btn--filled w-100%"><?= icon('flash') ?> Subscribe</button>
+
+      <p class="text-sm mb-12">We won't ever spam you! You can unsubscribe at any&nbsp;time.</p>
+
+      <input type="hidden" name="gruppe" value="<?= option('newstroll.list') ?>">
+      <button type="submit" class="btn btn--filled"><?= icon('flash') ?> Subscribe</button>
     </footer>
   </form>
 </div>
