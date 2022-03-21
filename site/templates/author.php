7bb47d6f760fb6cfac1a74f2c8a5ee408b49afd8
Nico Hoffmann <me@nhoffmann.com>
2022-03-21 15:09:20 -0300

    Author template: more reduced design

--- site/templates/author.php
+++ site/templates/author.php
@@ -1,27 +1,28 @@
 <?php layout('cookbook') ?>
 
-<?php slot('h1') ?>
-Author
+<?php slot('hero') ?>
+<header class="mb-6 flex">
+  <h1 class="h1 mr-3"><?= $page->title() ?></h1>
+  <div style="width: 3.5rem;">
+    <?= $page->image()->crop(400) ?>
+  </div>
+</header>
 <?php endslot() ?>
 
 <?php slot() ?>
-<figure class="flex max-w-xl mb-12 bg-white py-6 shadow-2xl">
-  <div class="px-6" style="width: 200px;">
-<p style="--aspect-ratio: 1/1">
-    <?= $page->image()->crop(400) ?>
-</p>
-</div>
-  <figcaption class="text-sm leading-tight">
-    <p class="h2 mb-1 font-bold"><?= $page->title() ?></p>
-    <p class="mb-6 color-gray-700"><?= $page->bio() ?></p>
 
-    <?php if ($page->website()->isNotEmpty()): ?>
-    <a href="<?= $page->website() ?>">
-      <p class="font-mono link"><?= $page->website()->shortUrl() ?></p>
-    </a>
-    <?php endif ?>
-  </figcaption>
-</figure>
+<div class="mb-24">
+  <span class="pill">
+    <span class="mr-3"><?= icon('user') ?></span><?= $page->bio() ?>
+  </span>
+
+  <a href="<?= $page->website() ?>" class="pill">
+    <span class="mr-3"><?= icon('globe') ?></span>
+    <span class="link"><?= $page->website()->shortUrl() ?></span>
+  </a>
+</div>
+
+<h2 class="pill mb-6"><span class="mr-3"><?= icon('book') ?></span>All recipes</h2>
 
 <?php snippet('templates/cookbook/recipes', [
   'recipes' => $recipes
