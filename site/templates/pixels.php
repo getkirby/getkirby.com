ad6177afed7cb59ae9fad48daa73072c5ed3f41f
Bastian Allgeier <mail@bastianallgeier.com>
2022-03-29 16:30:27 +0200

    Export button

--- site/templates/pixels.php
+++ site/templates/pixels.php
@@ -74,7 +74,7 @@
         <label class="label">Headline</label>
         <input class="input" type="text" v-model="settings.headline">
       </div>
-      <div class="columns" style="--columns: 2; --gap: var(--spacing-1)">
+      <div class="columns mb-3" style="--columns: 2; --gap: var(--spacing-1)">
         <div class="field">
           <label class="label">Logo</label>
           <div class="checkbox">
@@ -88,19 +88,24 @@
           </div>
         </div>
       </div>
+      <button class="btn" @click="exportImage">
+        <?= icon('download') ?> Export
+      </button>
     </div>
   </nav>
 
   <div class="editor-main">
-    <div v-cloak
-      class="editor-canvas"
-      :style="{
+
+    <div v-cloak v-if="isExporting" class="editor-exporter">
+      <p>Exporting …</p>
+    </div>
+
+    <div v-cloak class="editor-canvas" :style="{
         background: settings.background,
         width: settings.width + 'px',
         height: settings.height + 'px',
         color: settings.color
-      }"
-    >
+      }">
 
       <header class="editor-header">
         <div class="editor-headline" :style="{ fontWeight: settings.fontWeight }">
@@ -109,39 +114,29 @@
         <div v-if="settings.logo" class="editor-logo"><?= icon('icon') ?></div>
       </header>
 
-      <div
-        class="editor-image"
-        :data-rounded="settings.rounded"
-        :data-shadow="settings.shadow"
-        :style="{
+      <div class="editor-image" :data-rounded="settings.rounded" :data-shadow="settings.shadow" :style="{
           top: settings.mt + 'rem',
           right: settings.mr + 'rem',
           bottom: settings.mb + 'rem',
           left: settings.ml + 'rem',
           ...settings.corners,
-        }"
-      >
+        }">
         <template v-if="settings.browser">
           <div class="editor-browser bg-black flex items-center" style="--gap: .375rem; padding: .625rem">
             <svg width="10" height="10">
-              <circle fill="var(--color-gray-700)" cx="5" cy="5" r="5"/>
+              <circle fill="var(--color-gray-700)" cx="5" cy="5" r="5" />
             </svg>
             <svg width="10" height="10">
-              <circle fill="var(--color-gray-700)" cx="5" cy="5" r="5"/>
+              <circle fill="var(--color-gray-700)" cx="5" cy="5" r="5" />
             </svg>
             <svg width="10" height="10">
-              <circle fill="var(--color-gray-700)" cx="5" cy="5" r="5"/>
+              <circle fill="var(--color-gray-700)" cx="5" cy="5" r="5" />
             </svg>
           </div>
         </template>
         <template v-if="settings.image">
           <div class="editor-image-wrapper">
-            <img
-              :data-y="settings.position.y"
-              :data-x="settings.position.x"
-              :src="settings.image"
-              :style="{ width: settings.scale + '%' }"
-            >
+            <img :data-y="settings.position.y" :data-x="settings.position.x" :src="settings.image" :style="{ width: settings.scale + '%' }">
           </div>
         </template>
       </div>
@@ -149,7 +144,10 @@
   </div>
 </div>
 
+<?= js('assets/js/libraries/download.js') ?>
 <script type="module">
+  import * as htmlToImage from '<?= url('assets/js/libraries/html-to-image.js') ?>';
+
   import {
     createApp,
     reactive
@@ -177,15 +175,51 @@
   };
 
   const positions = {
-    topLeft: { y: "top", x: "left", arrow: "↘" },
-    topCenter: { y: "top", x: "center", arrow: "↓" },
-    topRight: { y: "top", x: "right", arrow: "↙" },
-    centerLeft: { y: "center", x: "left", arrow: "→" },
-    centerCenter: { y: "center", x: "center", arrow: "↔" },
-    centerRight: { y: "center", x: "right", arrow: "←" },
-    bottomLeft: { y: "bottom", x: "left", arrow: "↗" },
-    bottomCenter: { y: "bottom", x: "center", arrow: "↑" },
-    bottomRight: { y: "bottom", x: "right", arrow: "↖" },
+    topLeft: {
+      y: "top",
+      x: "left",
+      arrow: "↘"
+    },
+    topCenter: {
+      y: "top",
+      x: "center",
+      arrow: "↓"
+    },
+    topRight: {
+      y: "top",
+      x: "right",
+      arrow: "↙"
+    },
+    centerLeft: {
+      y: "center",
+      x: "left",
+      arrow: "→"
+    },
+    centerCenter: {
+      y: "center",
+      x: "center",
+      arrow: "↔"
+    },
+    centerRight: {
+      y: "center",
+      x: "right",
+      arrow: "←"
+    },
+    bottomLeft: {
+      y: "bottom",
+      x: "left",
+      arrow: "↗"
+    },
+    bottomCenter: {
+      y: "bottom",
+      x: "center",
+      arrow: "↑"
+    },
+    bottomRight: {
+      y: "bottom",
+      x: "right",
+      arrow: "↖"
+    },
   };
 
   const presets = {
@@ -313,6 +347,20 @@
     positions,
     presets,
     settings,
+    isExporting: false,
+    async exportImage() {
+
+      this.isExporting = true;
+
+      const canvas = document.querySelector(".editor-canvas");
+
+      htmlToImage.toPng(canvas)
+        .then(dataUrl => {
+          download(dataUrl, "pixels.png");
+          this.isExporting = false;
+        });
+
+    },
     onDrop(event) {
       if (!event.dataTransfer.files || event.dataTransfer.files.length === 0) {
         return;
@@ -352,248 +400,273 @@
       });
     }
   }).mount();
-
 </script>
 
 <style>
-[v-cloak] {
-  display: none;
-}
+  [v-cloak] {
+    display: none;
+  }
 
-html, body {
-  height: 100%;
-  overflow: hidden;
-}
+  html,
+  body {
+    height: 100%;
+    overflow: hidden;
+  }
 
-.editor {
-  display: grid;
-  height: 100%;
-  grid-template-columns: 20rem 2fr;
-}
+  .editor {
+    display: grid;
+    height: 100%;
+    grid-template-columns: 20rem 2fr;
+  }
 
-.editor-toolbar {
-  background: var(--color-light);
-  accent-color: var(--color-blue-700);
-  padding: 1.5rem;
-  font-size: var(--text-sm);
-}
+  .editor-toolbar {
+    background: var(--color-light);
+    accent-color: var(--color-blue-700);
+    padding: 1.5rem;
+    font-size: var(--text-sm);
+  }
 
-.editor-toolbar .field {
-  margin-bottom: var(--spacing-6);
-}
+  .editor-toolbar .field {
+    margin-bottom: var(--spacing-6);
+  }
 
-.editor-toolbar .label {
-  font-weight: var(--font-bold);
-  margin-bottom: var(--spacing-1);
-  display: block;
-}
+  .editor-toolbar .label {
+    font-weight: var(--font-bold);
+    margin-bottom: var(--spacing-1);
+    display: block;
+  }
 
-.editor-toolbar .input {
-  font-size: var(--text-sm);
-  padding: .25rem .5rem;
-}
+  .editor-toolbar .input {
+    font-size: var(--text-sm);
+    padding: .25rem .5rem;
+  }
 
-.editor-toolbar .upload {
-  position: relative;
-  background: var(--color-white);
-  padding: .25rem .5rem;
-}
-.editor-toolbar .upload input {
-  position: absolute;
-  inset: 0;
-  opacity: 0;
-  cursor: pointer;
-}
+  .editor-toolbar .upload {
+    position: relative;
+    background: var(--color-white);
+    padding: .25rem .5rem;
+  }
 
-.editor-toolbar .inputs {
-  display: grid;
-  grid-template-columns: 1;
-  grid-gap: 2px;
-}
+  .editor-toolbar .upload input {
+    position: absolute;
+    inset: 0;
+    opacity: 0;
+    cursor: pointer;
+  }
 
-.editor-toolbar .inputs li {
-  padding: .25rem .5rem;
-  background: var(--color-white);
-}
+  .editor-toolbar .inputs {
+    display: grid;
+    grid-template-columns: 1;
+    grid-gap: 2px;
+  }
 
-.editor-toolbar .inputs label {
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-}
+  .editor-toolbar .inputs li {
+    padding: .25rem .5rem;
+    background: var(--color-white);
+  }
 
-.editor-toolbar .inputs input {
-  margin-right: .5rem;
-}
+  .editor-toolbar .inputs label {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+  }
 
-.editor-toolbar .checkbox {
-  background: var(--color-white);
-  padding: .25rem .5rem;
-}
+  .editor-toolbar .inputs input {
+    margin-right: .5rem;
+  }
 
-.editor-toolbar .range {
-  line-height: 0;
-  background: var(--color-white);
-  padding: .25rem .5rem;
-}
+  .editor-toolbar .checkbox {
+    background: var(--color-white);
+    padding: .25rem .5rem;
+  }
 
-.editor-toolbar .select {
-  position: relative;
-  background: var(--color-white);
-  padding: .25rem .5rem;
-}
+  .editor-toolbar .range {
+    line-height: 0;
+    background: var(--color-white);
+    padding: .25rem .5rem;
+  }
 
-.editor-toolbar .select::after {
-  content: " ";
-}
+  .editor-toolbar .select {
+    position: relative;
+    background: var(--color-white);
+    padding: .25rem .5rem;
+  }
 
-.editor-toolbar .select select {
-  position: absolute;
-  inset: 0;
-  width: 100%;
-  background: none;
-  padding: 0 .25rem;
-  appearance: none;
-  cursor: pointer;
-}
+  .editor-toolbar .select::after {
+    content: " ";
+  }
 
-.editor-toolbar input[type="range"] {
-  width: 100%;
-}
+  .editor-toolbar .select select {
+    position: absolute;
+    inset: 0;
+    width: 100%;
+    background: none;
+    padding: 0 .25rem;
+    appearance: none;
+    cursor: pointer;
+  }
 
-.editor-toolbar .colors {
-  display: flex;
-  gap: 2px;
-}
+  .editor-toolbar input[type="range"] {
+    width: 100%;
+  }
 
-.editor-toolbar .colors button {
-  padding: .25rem;
-  background: var(--color-white);
-}
+  .editor-toolbar .colors {
+    display: flex;
+    gap: 2px;
+  }
 
-.editor-toolbar .colors button[aria-selected="true"] {
-  outline: 2px solid var(--color-blue-700);
-}
+  .editor-toolbar .colors button {
+    padding: .25rem;
+    background: var(--color-white);
+  }
 
-.editor-toolbar .colors button span {
-  display: block;
-  background: var(--color);
-  width: 1.25rem;
-  height: 1.25rem;
-}
+  .editor-toolbar .colors button[aria-selected="true"] {
+    outline: 2px solid var(--color-blue-700);
+  }
 
-.editor-main {
-  position: relative;
-  display: grid;
-  place-items: center;
-}
+  .editor-toolbar .colors button span {
+    display: block;
+    background: var(--color);
+    width: 1.25rem;
+    height: 1.25rem;
+  }
 
-.editor-canvas {
-  position: relative;
-  background: var(--color-light);
-  overflow: hidden;
-  color: var(--color-white);
-}
+  .editor-toolbar .btn {
+    border: 2px solid currentColor;
+    width: 100%;
+    padding: .25rem;
+    border-radius: var(--rounded);
+  }
 
-.editor-header {
-  position: absolute;
-  top: 4rem;
-  left: 6rem;
-  right: 6rem;
-  z-index: 1;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
+  .editor-main {
+    position: relative;
+    display: grid;
+    place-items: center;
+  }
 
-.editor-headline {
-  font-weight: 300;
-  font-size: var(--text-4xl);
-  flex-grow: 1;
-  margin-right: 1.5rem;
-}
-.editor-headline input {
-  font: inherit;
-  background: none;
-  color: currentColor;
-  width: 100%;
-}
-.editor-headline input:focus {
-  outline: none;
-}
+  .editor-canvas {
+    position: relative;
+    background: var(--color-light);
+    overflow: hidden;
+    color: var(--color-white);
+  }
 
-.editor-logo svg {
-  --size: 3.5rem;
-  width: var(--size);
-  height: var(--size);
-}
+  .editor-header {
+    position: absolute;
+    top: 4rem;
+    left: 6rem;
+    right: 6rem;
+    z-index: 1;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
-.editor-logo svg * {
-  fill: currentColor;
-}
+  .editor-headline {
+    font-weight: 300;
+    font-size: var(--text-4xl);
+    flex-grow: 1;
+    margin-right: 1.5rem;
+  }
 
-.editor-image {
-  --rounded: 7px;
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-}
+  .editor-headline input {
+    font: inherit;
+    background: none;
+    color: currentColor;
+    width: 100%;
+  }
 
-.editor-image[data-shadow="true"] {
-  filter: drop-shadow(0 25px 25px rgb(0 0 0 / 0.2));
-}
+  .editor-headline input:focus {
+    outline: none;
+  }
 
-.editor-browser {
-  flex-shrink: 0;
-  z-index: 1;
-}
+  .editor-logo svg {
+    --size: 3.5rem;
+    width: var(--size);
+    height: var(--size);
+  }
 
-.editor-image-wrapper {
-  position: relative;
-  flex-grow: 1;
-}
+  .editor-logo svg * {
+    fill: currentColor;
+  }
 
-.editor-image img {
-  position: absolute;
-  max-width: none;
-  object-fit: cover;
-  object-position: top center;
-}
+  .editor-image {
+    --rounded: 7px;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+  }
 
-.editor-image img[data-x="left"] {
-  left: 0;
-}
+  .editor-image[data-shadow="true"] {
+    filter: drop-shadow(0 25px 25px rgb(0 0 0 / 0.2));
+  }
 
-.editor-image img[data-x="center"] {
-  left: 50%;
-  transform: translateX(-50%);
-}
+  .editor-browser {
+    flex-shrink: 0;
+    z-index: 1;
+  }
 
-.editor-image img[data-x="right"] {
-  right: 0;
-}
+  .editor-image-wrapper {
+    position: relative;
+    flex-grow: 1;
+  }
 
-.editor-image img[data-y="top"] {
-  top: 0;
-}
+  .editor-image img {
+    position: absolute;
+    max-width: none;
+    object-fit: cover;
+    object-position: top center;
+  }
 
-.editor-image img[data-y="center"] {
-  top: 50%;
-  transform: translateY(-50%);
-}
+  .editor-image img[data-x="left"] {
+    left: 0;
+  }
 
-.editor-image img[data-y="bottom"] {
-  bottom: 0;
-}
+  .editor-image img[data-x="center"] {
+    left: 50%;
+    transform: translateX(-50%);
+  }
 
-.editor-image img[data-x="center"][data-y="center"] {
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
+  .editor-image img[data-x="right"] {
+    right: 0;
+  }
 
+  .editor-image img[data-y="top"] {
+    top: 0;
+  }
+
+  .editor-image img[data-y="center"] {
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  .editor-image img[data-y="bottom"] {
+    bottom: 0;
+  }
+
+  .editor-image img[data-x="center"][data-y="center"] {
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .editor-exporter {
+    position: fixed;
+    inset: 0;
+    background: rgba(255,255,255, .6);
+    z-index: 1;
+    display: grid;
+    place-items: center;
+  }
+  .editor-exporter p {
+    background: white;
+    box-shadow: var(--shadow-xl);
+    padding: 1rem 2rem;
+    font-weight: var(--font-bold);
+    border-radius: var(--rounded);
+  }
 </style>
