672be43f01d0c5108dd1e1e9a54c57620b4297fb
Bastian Allgeier <mail@bastianallgeier.com>
2022-06-02 14:37:35 +0200

    First rough setup for a release page

---
+++ site/snippets/templates/release-37/release-menu.php
@@ -0,0 +1,7 @@
+<nav class="release-menu">
+  <ul class="font-mono text-sm pt-6 sticky" style="--top: var(--spacing-2)">
+    <li><a href="#blocks">Statistics</a></li>
+    <li><a href="#changes">Changelog</a></li>
+    <li><a href="#contributors">Contributors</a></li>
+  </ul>
+</nav>
