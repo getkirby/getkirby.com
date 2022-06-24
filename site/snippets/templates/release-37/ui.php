80b9ae41c8ef8fab2e11691849631fe2c6ffcf35
Bastian Allgeier <mail@bastianallgeier.com>
2022-06-24 11:20:00 +0200

    Responsive release page

--- site/snippets/templates/release-37/ui.php
+++ site/snippets/templates/release-37/ui.php
@@ -1,5 +1,36 @@
-<section id="ui" class="mb-42">
+<style>
+.v37-ui-grid {
+  display: grid;
+  grid-gap: var(--spacing-6);
+  grid-template-columns: 1fr;
+}
 
+@media screen and (min-width: 45rem) {
+  .v37-ui-grid {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+
+@media screen and (min-width: 60rem) {
+  .v37-ui-grid {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+  .v37-ui-grid > :nth-child(2) {
+    grid-column: span 2;
+  }
+}
+
+@media screen and (min-width: 90rem) {
+  .v37-ui-grid {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+  .v37-ui-grid > :nth-child(2) {
+    grid-column: span 3;
+  }
+}
+</style>
+
+<section id="ui" class="mb-42">
   <div class="sr-only">
     <?= F::read($kirby->root('panel') . '/dist/img/icons.svg') ?>
   </div>
@@ -10,29 +41,30 @@
     'mb'       => 12
   ]) ?>
 
-  <div class="columns">
-
-    <div class="bg-light rounded-xl overflow-hidden" style="--span: 4">
+  <div class="columns mb-6" style="--columns: 3">
+    <div class="release-box bg-light">
       <figure style="--aspect-ratio: 1/1">
         <?= $page->image('pages.png') ?>
       </figure>
     </div>
 
-    <div class="bg-light rounded-xl overflow-hidden" style="--span: 4">
+    <div class="release-box bg-light">
       <figure style="--aspect-ratio: 1/1">
         <?= $page->image('fields.png') ?>
       </figure>
     </div>
 
-    <div class="p-12 bg-white rounded-xl" style="--span: 4">
-      <h3 class="text-lg font-bold">Fresh & familiar</h3>
-      <div class="prose text-lg">
+    <div class="release-text-box">
+      <h3>Fresh & familiar</h3>
+      <div class="prose">
         Slightly rounder, a bit more friendly, and somewhat more cosy: you will feel instantly at home.
       </div>
     </div>
+  </div>
 
-    <div class="bg-white highlight rounded-xl" style="--span: 3">
-      <h3 class="text-lg font-bold mb-6">New icons</h3>
+  <div class="v37-ui-grid">
+    <div class="release-text-box">
+      <h3 class="mb-6">New icons</h3>
       <ul class="columns" style="--columns: 1; --gap: var(--spacing-3)">
         <?php foreach ($page->icons()->yaml() as $icon) : ?>
           <li class="flex items-center font-mono text-xs">
@@ -45,12 +77,11 @@
       </ul>
     </div>
 
-    <div class="highlight bg-light rounded-xl overflow-hidden" style="--span: 9">
-      <h3 class="text-lg font-bold mb-6">New file icons</h3>
+    <div class="release-padded-box bg-light">
+      <h3 class="mb-6">New file icons</h3>
       <figure style="--aspect-ratio: 1968/834">
         <?= $page->image('fileicons.png') ?>
       </figure>
     </div>
-
-
+  </div>
 </section>
