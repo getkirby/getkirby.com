a17cc349af73c3412ffbcc0474b43deb7b0a3f36
Nico Hoffmann <me@nhoffmann.com>
2022-06-25 10:54:38 +0200

    More release page work

--- site/snippets/templates/release-37/changes.php
+++ site/snippets/templates/release-37/changes.php
@@ -5,25 +5,39 @@
       'title' => $section->title(),
     ]) ?>
 
-    <div class="columns" style="--columns: 2; --gap: var(--spacing-1)">
-      <div class="highlight bg-light">
+    <div class="masonry" style="--columns: 2; --gap: var(--spacing-6)">
+      <div class="rounded-xl highlight bg-light">
         <h3 class="h3 mb-6">Features</h3>
         <div class="prose text-sm">
           <?= $section->features()->kt() ?>
         </div>
       </div>
 
-      <div class="highlight bg-light">
+      <div class="rounded-xl highlight bg-light">
+        <h3 class="h3 mb-6">Bug fixes</h3>
+        <div class="prose text-sm">
+          <?= $section->fixes()->kt() ?>
+        </div>
+      </div>
+
+      <div class="rounded-xl highlight bg-light">
         <h3 class="h3 mb-6">Enhancements</h3>
         <div class="prose text-sm">
           <?= $section->enhancements()->kt() ?>
         </div>
       </div>
 
-      <div class="highlight bg-light">
-        <h3 class="h3 mb-6">Bug fixes</h3>
+      <div class="rounded-xl highlight bg-light">
+        <h3 class="h3 mb-6">Refactoring</h3>
         <div class="prose text-sm">
-          <?= $section->fixes()->kt() ?>
+          <?= $section->refactoring()->kt() ?>
+        </div>
+      </div>
+
+      <div class="rounded-xl highlight bg-light">
+        <h3 class="h3 mb-6">Deprecated</h3>
+        <div class="prose text-sm">
+          <?= $section->deprecated()->kt() ?>
         </div>
       </div>
 
@@ -34,19 +48,6 @@
         </div>
       </div>
 
-      <div class="highlight bg-light">
-        <h3 class="h3 mb-6">Refactoring</h3>
-        <div class="prose text-sm">
-          <?= $section->refactoring()->kt() ?>
-        </div>
-      </div>
-
-      <div class="highlight bg-light">
-        <h3 class="h3 mb-6">Deprecated</h3>
-        <div class="prose text-sm">
-          <?= $section->deprecated()->kt() ?>
-        </div>
-      </div>
     </div>
 
   </section>
--- site/snippets/templates/release-37/stats.php
+++ site/snippets/templates/release-37/stats.php
@@ -1,10 +1,27 @@
-<?php snippet('templates/features/section', [
-  'id'       => 'stats',
-  'title'    => 'Statistics',
-  'intro'    => 'A new section',
-  'text'     => 'The brand new statistics section, can be used to show beautiful stats for your site or shop. Revenue, transactions, Twitter likes, page impressions … it’s totally up to you. You can add as many reports to a stats section as needed. Reports can be customized with our query syntax and integrated easily into page models or site methods.',
-  'figure'   => 'templates/release-37/stats-figure',
-  'footer'   => 'templates/release-37/stats-footer',
-  'features' => [
-  ]
-]);
+<section id="table-layout" class="mb-42">
+  <?php snippet('hgroup', [
+    'title'    => 'New stats section',
+    'subtitle' => 'Show reports on your dashboard',
+    'mb'       => 12
+  ]) ?>
+
+  <div class="columns" style="--columns: 3; --gap: var(--spacing-6)">
+
+    <figure class="bg-light rounded-xl overflow-hidden" style="--aspect-ratio: 1265/1177; grid-column: span 2; grid-row: span 2">
+      <img src="<?= ($image = $page->image('stats.png'))->url() ?>" loading="lazy">
+    </figure>
+
+    <div class="p-12 bg-white rounded-xl flex flex-column">
+      <h3 class="text-xl font-bold">Easy as 1-2-3</h3>
+      <div class="prose">
+        Show beautiful stats for your site or shop. Revenue, transactions, Twitter likes, page impressions … it’s totally up to you. You can add as many reports to a stats section as needed. Reports can be customized with our query syntax and integrated easily into page models or site methods.
+      </div>
+    </div>
+
+    <div class="bg-black p-1 rounded-xl overflow-hidden">
+      <?= $page->stats()->kt() ?>
+    </div>
+  </div>
+
+
+</section>
