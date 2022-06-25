d4d0ee1509e3aa94fc99588bfb8bd6c10bf56b77
Nico Hoffmann <me@nhoffmann.com>
2022-06-25 10:54:38 +0200

    More release page work

--- site/snippets/templates/release-37/system.php
+++ site/snippets/templates/release-37/system.php
@@ -1,9 +1,36 @@
-<?php snippet('templates/features/section', [
-  'id'       => 'system',
-  'title'    => 'System check',
-  'intro'    => 'Like the TSA just nicer',
-  'text'     => 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur. Sed posuere consectetur est at lobortis. Sed posuere consectetur est at lobortis.',
-  'figure'   => 'templates/release-37/system-figure',
-  'features' => [
-  ]
-]);
+<section id="system-view" class="mb-42">
+  <?php snippet('hgroup', [
+    'title'    => 'New system view',
+    'subtitle' => 'All systems go',
+    'mb'       => 12
+  ]) ?>
+
+  <div class="columns" style="--columns: 3; --gap: var(--spacing-6)">
+    <figure class="bg-light rounded-xl overflow-hidden" style="--aspect-ratio: 1433/651; --span: 3">
+      <img src="<?= ($image = $page->image('system.png'))->url() ?>" loading="lazy">
+    </figure>
+
+    <div class="p-12 bg-white rounded-xl">
+      <h3 class="text-xl font-bold">Environment</h3>
+      <div class="prose">
+        Get vital information about your installation at a glance: License key, version number, PHP version, server software.
+      </div>
+    </div>
+
+    <div class="p-12 bg-white rounded-xl">
+      <h3 class="text-xl font-bold">Security</h3>
+      <div class="prose">
+        Find potential issues and unintentionally exposed parts of your installation (Git repo, content folder, site folder, kirby folder)
+      </div>
+    </div>
+
+    <div class="p-12 bg-white rounded-xl">
+      <h3 class="text-xl font-bold">Plugins</h3>
+      <div class="prose">
+        See all installed plugins, their authors, license and current version number in our redesigned plugins table.
+      </div>
+    </div>
+
+  </div>
+
+</section>
--- site/snippets/templates/release-37/table.php
+++ site/snippets/templates/release-37/table.php
@@ -1,10 +1,26 @@
-<?php snippet('templates/features/section', [
-  'reverse'  => true,
-  'id'       => 'table',
-  'title'    => 'Table layout',
-  'intro'    => 'For pages, files, etc.',
-  'text'     => 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur. Sed posuere consectetur est at lobortis. Sed posuere consectetur est at lobortis.',
-  'figure'   => 'templates/release-37/table-figure',
-  'features' => [
-  ]
-]);
+<section id="table-layout" class="mb-42">
+
+  <?php snippet('hgroup', [
+    'title'    => 'New table layout',
+    'subtitle' => 'Who said spreadsheets cannot be cool?',
+    'mb'       => 12
+  ]) ?>
+
+  <div class="columns" style="--columns: 2; --gap: var(--spacing-6)">
+
+    <figure class="bg-light rounded-xl overflow-hidden" style="--aspect-ratio: 1428/413; --span: 2">
+      <img src="<?= ($image = $page->image('table.png'))->url() ?>" loading="lazy">
+    </figure>
+
+    <div class="p-12 bg-white rounded-xl">
+      <h3 class="text-xl font-bold">At a glance</h3>
+      <div class="prose">
+        Keep better overview of your pages and files with the brand new table layout. Define custom columns to customize the way you present your data.
+      </div>
+    </div>
+    <div class="bg-black p-1 rounded-xl overflow-hidden">
+      <?= $page->table()->kt() ?>
+    </div>
+  </div>
+
+</section>
