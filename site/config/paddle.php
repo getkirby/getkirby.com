c4f4e20ce5e03688f2915a760014f1748ab947b5
Bastian Allgeier <mail@bastianallgeier.com>
2022-06-26 17:28:03 +0200

    Disable larger volume discounts during sale

--- site/config/paddle.php
+++ site/config/paddle.php
@@ -5,9 +5,9 @@ return [
         5  => 5,
         10 => 10,
         15 => 15,
-        20 => 20,
-        25 => 25,
-        50 => 30
+        // 20 => 20,
+        // 25 => 25,
+        // 50 => 30
     ],
     'product'        => 499826,
     'vendorAuthCode' => trim(F::read(__DIR__ . '/keys/paddle.txt')),
