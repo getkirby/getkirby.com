83049a92276a92beebd794087b35972868347243
Bastian Allgeier <mail@bastianallgeier.com>
2022-06-26 17:29:26 +0200

    Setup sale banner

--- site/config/banners.php
+++ site/config/banners.php
@@ -2,8 +2,8 @@
 
 return [
     'sale' => [
-        'text'      => '🛍 &nbsp; <strong>Save 20%</strong> until April 11th',
-        'startDate' => '2022-04-04',
-        'endDate'   => '2022-04-11',
+        'text'      => '🛍 &nbsp; <strong>Save 20%</strong> until July 11th',
+        'startDate' => '2022-06-27',
+        'endDate'   => '2022-07-11',
     ]
 ];
