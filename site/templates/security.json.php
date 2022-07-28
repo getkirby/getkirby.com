2b9593653b1d577468699a5b0dc1f7a16f2c2356
Lukas Bestle <lukas@getkirby.com>
2022-07-28 21:52:50 +0200

    Security Versions: Move to key-value structure
    Allows direct access to e.g. the latest version

--- site/templates/security.json.php
+++ site/templates/security.json.php
@@ -10,7 +10,7 @@ $idFilter = function ($entry) {
 $data = [
     'latest'    => $kirby->version(),
     'latestUrl' => 'https://github.com/getkirby/kirby/releases/tag/' . $kirby->version(),
-    'versions'  => array_values($page->versions()->toArray($idFilter)),
+    'versions'  => $page->versions()->toArray($idFilter),
     'incidents' => array_values($page->incidents()->toArray($idFilter))
 ];
 
