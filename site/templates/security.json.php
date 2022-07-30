9ae3f5569c026fa9817d245a20076dd97034b01d
Lukas Bestle <lukas@getkirby.com>
2022-07-30 18:07:18 +0200

    Security: Cache JSON in CDN for 30 minutes
    Ensures quicker responses and takes strain from the getkirby.com server

--- site/templates/security.json.php
+++ site/templates/security.json.php
@@ -15,4 +15,9 @@ $data = [
     'messages'  => array_values($page->messages()->toArray($idFilter))
 ];
 
+// keep the data in the client cache for a day,
+// but refresh the data in the CDN cache every half an hour;
+// if getkirby.com is not reachable, continue to serve the data for two days
+$kirby->response()->header('Cache-Control', 'max-age=86400, s-maxage=1800, stale-if-error=172800');
+
 echo json($data);
