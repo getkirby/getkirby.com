57d45958f27f9c7c64a8a0bf17b956340d91db69
Nico Hoffmann <me@nhoffmann.com>
2022-09-21 19:06:40 +0200

    Release microsite: DRY in snippets

---
+++ site/templates/release-3.8.php
@@ -0,0 +1,58 @@
+<?php layout() ?>
+<?= css('assets/css/layouts/features.css') ?>
+<?= css('assets/css/layouts/releases.css') ?>
+
+<style>
+.release-box,
+.release-code-box,
+.release-padded-box,
+.release-text-box {
+  border-radius: .5rem;
+  overflow: hidden;
+  z-index: 1;
+}
+.release-code-box {
+  display: grid;
+  align-items: stretch;
+  background: var(--color-black);
+}
+.release-code-box .code-toolbar {
+  display: grid;
+}
+.release-padded-box,
+.release-text-box {
+  padding: var(--spacing-6);
+}
+.release-text-box {
+  background: var(--color-white);
+}
+.release-padded-box h3,
+.release-text-box h3 {
+  font-weight: var(--font-bold);
+  font-size: var(--text-lg);
+}
+.release-text-box .prose {
+  font-size: var(--text-lg);
+}
+
+@media screen and (min-width: 60rem) {
+  .release-text-box,
+  .release-padded-box {
+    padding: var(--spacing-12);
+  }
+}
+</style>
+
+<header class="mb-36 flex items-end justify-between release-header">
+  <div>
+    <h1 class="h1"><?= $page->title() ?></h1>
+    <p class="h1 color-gray-600"><?= $page->subtitle() ?></p>
+  </div>
+
+  <?php snippet('templates/releases/cta', [
+    'options' => ['center' => false, 'mb' => 0]
+  ]) ?>
+</header>
+
+
+<?php snippet('templates/releases/get-started') ?>
