2c72ca6fcd941b4a108bc85c6c6e0104743ebd09
Bastian Allgeier <mail@bastianallgeier.com>
2022-09-28 18:02:48 +0200

    More scaffolding for the release page

--- site/templates/release-3.8.php
+++ site/templates/release-3.8.php
@@ -3,44 +3,51 @@
 <?= css('assets/css/layouts/releases.css') ?>
 
 <style>
-.release-box,
-.release-code-box,
-.release-padded-box,
-.release-text-box {
-  border-radius: .5rem;
-  overflow: hidden;
-  z-index: 1;
-}
-.release-code-box {
-  display: grid;
-  align-items: stretch;
-  background: var(--color-black);
-}
-.release-code-box .code-toolbar {
-  display: grid;
-}
-.release-padded-box,
-.release-text-box {
-  padding: var(--spacing-6);
-}
-.release-text-box {
-  background: var(--color-white);
-}
-.release-padded-box h3,
-.release-text-box h3 {
-  font-weight: var(--font-bold);
-  font-size: var(--text-lg);
-}
-.release-text-box .prose {
-  font-size: var(--text-lg);
-}
-
-@media screen and (min-width: 60rem) {
-  .release-text-box,
-  .release-padded-box {
-    padding: var(--spacing-12);
+  .release-box,
+  .release-code-box,
+  .release-padded-box,
+  .release-text-box {
+    border-radius: .5rem;
+    overflow: hidden;
+    z-index: 1;
   }
-}
+
+  .release-code-box {
+    display: grid;
+    align-items: stretch;
+    background: var(--color-black);
+  }
+
+  .release-code-box .code-toolbar {
+    display: grid;
+  }
+
+  .release-padded-box,
+  .release-text-box {
+    padding: var(--spacing-6);
+  }
+
+  .release-text-box {
+    background: var(--color-white);
+  }
+
+  .release-padded-box h3,
+  .release-text-box h3 {
+    font-weight: var(--font-bold);
+    font-size: var(--text-lg);
+  }
+
+  .release-text-box .prose {
+    font-size: var(--text-lg);
+  }
+
+  @media screen and (min-width: 60rem) {
+
+    .release-text-box,
+    .release-padded-box {
+      padding: var(--spacing-12);
+    }
+  }
 </style>
 
 <header class="mb-36 flex items-end justify-between release-header">
@@ -54,5 +61,9 @@
   ]) ?>
 </header>
 
+<article class="release-wrapper">
+  <?php snippet('templates/release-38/changes') ?>
+  <?php snippet('templates/release-38/contributors') ?>
+</article>
 
 <?php snippet('templates/releases/get-started') ?>
