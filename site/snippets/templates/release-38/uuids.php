d70ce9443a766417c322854fafe3fdc21dede860
Bastian Allgeier <mail@bastianallgeier.com>
2022-09-29 17:55:42 +0200

    Add UUID graphic

--- site/snippets/templates/release-38/uuids.php
+++ site/snippets/templates/release-38/uuids.php
@@ -6,5 +6,29 @@
     'mb'       => 12
   ]) ?>
 
+  <figure class="release-box mb-6" style="--aspect-ratio: 1558/688">
+    <img src="<?= $page->image('uuids.png')?->url() ?>" loading="lazy" alt="UUIDs">
+  </figure>
+
+  <div class="columns" style="--columns: 3">
+    <div class="release-text-box" style="background: var(--color-light)">
+      <h3>Lorem ipsum</h3>
+      <div class="prose">
+        Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+      </div>
+    </div>
+    <div class="release-text-box" style="background: var(--color-light)">
+      <h3>Lorem ipsum</h3>
+      <div class="prose">
+        Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+      </div>
+    </div>
+    <div class="release-text-box" style="background: var(--color-light)">
+      <h3>Lorem ipsum</h3>
+      <div class="prose">
+        Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+      </div>
+    </div>
+  </div>
 
 </section>
