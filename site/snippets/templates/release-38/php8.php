745ae094eed2ed5abd708e22f9fb987524c624f9
Bastian Allgeier <mail@bastianallgeier.com>
2022-09-30 17:22:03 +0200

    Added PHP 8+ section

---
+++ site/snippets/templates/release-38/php8.php
@@ -0,0 +1,26 @@
+<section id="php8" class="mb-42">
+
+  <?php snippet('hgroup', [
+    'title'    => 'PHP 8+',
+    'subtitle' => 'Use the potential of a modern PHP environment',
+    'mb'       => 12
+  ]) ?>
+
+  <div class="columns" style="--columns: 3">
+
+    <figure class="release-box bg-dark p-24 color-white grid place-items-center" style="--span: 2">
+      <div style="width: 15rem; --aspect-ratio: 2/1;">
+        <?= image('home/php.svg')->read() ?>
+      </div>
+    </figure>
+
+    <div class="release-text-box" style="background: var(--color-light)">
+      <h3>There’s no time to dwell in the past</h3>
+      <div class="prose">
+        <p>PHP has made amazing progress in the last years. With PHP 8 we don't just get an incredible performance boost but also language features that really move Kirby forward.</p>
+        <p>Kirby 3.8 builts exclusively on PHP 8+. With the end-of-life of PHP 7.4 in November and wide-spread hosting support for PHP 8, we don't see any reason to stay with an outdated version if the future is so bright.</p>
+      </div>
+    </div>
+  </div>
+
+</section>
--- site/snippets/templates/release-38/release-menu.php
+++ site/snippets/templates/release-38/release-menu.php
@@ -2,6 +2,7 @@
   <ul class="font-mono text-sm pt-6 sticky" style="--top: var(--spacing-2)">
     <li><a href="#uuids">UUIDs</a></li>
     <li><a href="#updates">Update checks</a></li>
+    <li><a href="#php8">PHP 8+</a></li>
     <li><a href="#object-field">Object field</a></li>
     <li><a href="#gallery-block">Gallery block</a></li>
     <li><a href="#changes">Changelog</a></li>
