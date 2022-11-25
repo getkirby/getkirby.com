7e846a793f06c7c0baab3f48de4a3c5346d1ac02
Lukas Bestle <lukas@getkirby.com>
2022-11-25 22:11:07 +0100

    Document pages API `status` query param
    https://github.com/getkirby/kirby/issues/2846#issuecomment-1257448522

--- site/snippets/docs/api/pages-parameters.php
+++ site/snippets/docs/api/pages-parameters.php
@@ -5,3 +5,4 @@
 | `page`   | `?page=2` | Starts the collection at the given page. Only works when the limit is set. |
 | `pretty` | `?pretty=true` | Pretty prints the result. Only useful for debugging. |
 | `select` | `?select=id,title` | Defines the fields that will be returned for each page. |
+| `status` | `?status=all` | Filters the pages by status (`all`, `listed`, `unlisted`, `published`, `draft`). |
