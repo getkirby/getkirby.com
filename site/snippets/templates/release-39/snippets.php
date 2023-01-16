40025deba042098abef985bc4c249482279fa0eb
Bastian Allgeier <mail@bastianallgeier.com>
2023-01-16 16:49:09 +0100

    Embed new snippet video

--- site/snippets/templates/release-39/snippets.php
+++ site/snippets/templates/release-39/snippets.php
@@ -6,6 +6,19 @@
     'mb'       => 12
   ]) ?>
 
+  <figure class="video mb-6 rounded-xl overflow-hidden" style="--aspect-ratio: 16/9">
+    <?= video('https://www.youtube-nocookie.com/watch?v=ASmy8mcBWqg', [
+      'youtube' => [
+        'controls'       => 0,
+        'modestbranding' => 1,
+        'showinfo'       => 0,
+        'rel'            => 0,
+      ]
+    ], [
+      'loading' => 'lazy'
+    ]) ?>
+  </figure>
+
   <div class="columns mb-6" style="--columns: 2">
     <div class="release-code-box">
       <?= $page->snippetsA()->kt() ?>
