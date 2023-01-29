712b994103f723d96f04ca6f50b9775da4d906a1
Johann Schopplich <mail@johannschopplich.com>
2023-01-29 22:42:49 +0100

    Add misc. plugins from `johannschopplich`

---
+++ site/snippets/templates/plugins/categories/search.php
@@ -0,0 +1,8 @@
+<?php snippet('templates/plugins/section', [
+  'id'      => 'search',
+  'icon'    => 'search',
+  'title'   => 'Search',
+  'layout'  => 'cards',
+  'columns' => 2,
+  'plugins' => $plugins->filter('subcategory', '')->pluck('id')
+]) ?>
