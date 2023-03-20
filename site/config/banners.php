90eabeaab7888c9082cb0a3bdf79f0202e037e4b
Bastian Allgeier <mail@bastianallgeier.com>
2023-03-20 09:23:18 +0100

    Sale banner

--- site/config/banners.php
+++ site/config/banners.php
@@ -2,8 +2,8 @@
 
 return [
     'sale' => [
-        'text'      => '🛍 &nbsp; <strong>Save 20%</strong> until December 12th',
-        'startDate' => '2022-11-28',
-        'endDate'   => '2022-12-12',
+        'text'      => '🛍 &nbsp; <strong>Save 20%</strong> until April 3rd',
+        'startDate' => '2023-03-20',
+        'endDate'   => '2023-04-03',
     ]
 ];
