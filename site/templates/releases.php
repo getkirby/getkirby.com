9783f85f32ed1e611386ad6c43592aa5247124fa
Nico Hoffmann <me@nhoffmann.com>
2023-03-26 22:23:31 +0200

    Tweaking releases template

--- site/templates/releases.php
+++ site/templates/releases.php
@@ -1,14 +1,5 @@
 <?php layout() ?>
-<style>
 
-  .versionlist {
-    display: inline;
-  }
-
-  .versionlist:not(:last-child):after {
-    content: ',';
-  }
-</style>
 <article>
   <header class="h1 mb-24">
     <h1>Kirby keeps getting<br>better and better<h1>
@@ -22,36 +13,33 @@
             <?= $release->version() ?>
           </a>
         </header>
-        <div class=" color-gray-700 mb-12">
 
-        <?php if ($cover = $release->cover()->toFile()): ?>
-          <figure class="border-top pt-6 mb-3">
-            <?= $release->cover()->toFile()->crop(400) ?>
-          </figure>
-        <?php endif ?>
+        <div class=" color-gray-700 mb-12">
+          <?php if ($cover = $release->cover()->toFile()): ?>
+            <figure class="border-top pt-6 mb-3">
+              <?= $release->cover()->toFile()->crop(400) ?>
+            </figure>
+          <?php endif ?>
         </div>
 
-          <div class=" color-gray-700 mb-6">
-            <p><?= $release->description() ?></p>
-          </div>
-          <div class="columns mb-6" style="--columns: 2">
-            <a href="<?= $release->releasePage()->or($release->url()) ?>" class="btn btn--outlined">
-              <?= icon('star') ?>
-              New in <?= $release->version() ?>
-            </a>
-          </div>
-          <div class="">
-            <?php $minorReleases = $kirby->option('versions')[$release->version()->value()]['subreleases'];?>
-            <div class="h3 mb-6">Further releases</div>
-            <ul class="prose">
-              <?php foreach ($minorReleases as $subRelease): ?>
-                <li class="versionlist">
-                  <a href="https://github.com/getkirby/kirby/releases/tag/<?= $subRelease ?>"><?= $subRelease ?></a>
-                </li>
-              <?php endforeach ?>
-            </ul>
-
-          </div>
+        <div class=" color-gray-700 mb-6">
+          <p><?= $release->description() ?></p>
+        </div>
+        <div class="columns mb-6" style="--columns: 2">
+          <a href="<?= $release->releasePage()->or($release->url()) ?>" class="btn btn--outlined">
+            <?= icon('star') ?>
+            New in <?= $release->version() ?>
+          </a>
+        </div>
+        <div class="prose">
+          <div class="h5 mb-4">Further releases</div>
+          <span class="text-base">
+            <?= implode(', ', A::map(
+              $kirby->option('versions')[$release->version()->value()]['subreleases'],
+              fn ($subRelease) => '<a href="https://github.com/getkirby/kirby/releases/tag/' . $subRelease . '">' . $subRelease . '</a>'
+            )) ?>
+          </span>
+        </div>
       </li>
     <?php endforeach ?>
   </ul>
