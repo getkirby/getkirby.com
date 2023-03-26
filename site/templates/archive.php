7d4c883e8b5637e167d5f0223cd80783072a46c8
Sonja Broda <github@texniq.de>
2023-03-26 18:01:46 +0200

    Enhance archive with links to different Kirby doc versions

--- site/templates/archive.php
+++ site/templates/archive.php
@@ -9,69 +9,29 @@
   </header>
 
   <ul class="columns items-center" style="--columns-sm: 1; --columns-md: 1; --columns-lg: 3; --gap: var(--spacing-12)">
+     <?php foreach ($versions as $version): ?>
     <li>
-      <article>
+      <article class="<?= $version === end($versions) ? 'p-6 bg-white shadow-2xl' : '' ?>">
         <header class="mb-3">
-          <p class="h6 mb-1">2012-2014</p>
-          <h2 class="h2 border-top pt-3">Kirby 1</h2>
+          <p class="h6 mb-1"><?= $version['since'] ?></p>
+          <h2 class="h2 border-top pt-3"><?= $version['title'] ?></h2>
         </header>
         <div class="color-gray-700 mb-12">
-          <p>Kirby 1 has reached its end of life and is no longer supported by us.</p>
+          <p><?= $version['title'] . ' ' . $version['description']?> </p>
         </div>
         <div class="columns" style="--columns: 2">
-          <a aria-label="Visit the Kirby 1 docs" href="https://k1.getkirby.com/docs" class="btn btn--outlined">
+          <a aria-label="Visit the <?= $version['title'] ?> docs" href="<?= $version['link'] ?>" class="btn <?= $version === end($versions) ? 'btn--filled' : 'btn--outlined' ?>">
             <?= icon('book') ?>
             Docs
           </a>
-          <a aria-label="The Kirby 1 repository on Github" href="https://github.com/getkirby-v1" class="btn btn--outlined">
+          <a aria-label="The Kirby <?= $version['mainVersion'] ?> repository on Github" href="<?= $version['repo'] ?>" class="btn <?= $version === end($versions) ? 'btn--filled' : 'btn--outlined' ?>">
             <?= icon('github') ?>
             Source
           </a>
         </div>
       </article>
     </li>
-    <li>
-      <article>
-        <header class="mb-3">
-          <p class="h6 mb-1">2014-2020</p>
-          <h2 class="h2 border-top pt-3">Kirby 2</h2>
-        </header>
-        <div class="color-gray-700 mb-12">
-          <p>Kirby 2 has reached its end of life and is no longer supported by us.</p>
-        </div>
-        <div class="columns" style="--columns: 2">
-          <a aria-label="Visit the Kirby 2 docs" href="https://k2.getkirby.com/docs" class="btn btn--outlined">
-            <?= icon('book') ?>
-            Docs
-          </a>
-          <a aria-label="The Kirby 2 repository on Github" href="https://github.com/getkirby-v2" class="btn btn--outlined">
-            <?= icon('github') ?>
-            Source
-          </a>
-        </div>
-      </article>
-    </li>
-    <li>
-      <article class="p-6 bg-white shadow-2xl">
-        <header class="mb-3">
-          <p class="h6 mb-1">Since 2019</p>
-          <h2 class="h2 border-top pt-3">Kirby 3</h2>
-        </header>
-        <div class="color-gray-700 mb-12">
-          <p>Kirby 3 is the latest version of Kirby. <br><strong class="color-black">Start new projects with Kirby 3!</strong></p>
-        </div>
-        <div class="columns" style="--columns: 2">
-          <a aria-label="Visit the Kirby 3 docs" href="/docs" class="btn btn--filled">
-            <?= icon('book') ?>
-            Docs
-          </a>
-          <a aria-label="The Kirby 3 repository on Github" href="https://github.com/getkirby" class="btn btn--filled">
-            <?= icon('github') ?>
-            Source
-          </a>
-        </div>
-      </article>
-    </li>
+    <?php endforeach ?>
   </ul>
 
 </article>
