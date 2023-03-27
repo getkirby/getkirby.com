20d34e8a52069d8ab2d6287920eb643751a93719
Sonja Broda <github@texniq.de>
2023-03-27 09:25:48 +0200

    Releases page (#1983)
    * Releases page
    
    * Fix icon in button
    
    * Releases: tweak gap to button
    
    * Update release content
    
    * Update releases page
    
    * Update archive controller, filter versions
    
    * Add fix from main
    
    * Inline minor releases list
    
    * Tweaking releases template
    
    ---------
    
    Co-authored-by: Nico Hoffmann <me@nhoffmann.com>

--- site/config/versions.php
+++ site/config/versions.php
@@ -3,52 +3,150 @@
 use Kirby\Data\Data;
 
 return [
-    [
+    '1' => [
         'title'       => 'Kirby 1',
+        'hasDocs'     => true,
         'mainVersion' => '1',
         'since'       => '2012-2014',
         'description' => 'has reached its end of life and is no longer supported by us.',
         'link'        => 'https://k1.getkirby.com',
         'repo'        => 'https://github.com/getkirby-v1',
     ],
-    [
+    '2' =>[
         'title'       => 'Kirby 2',
+        'hasDocs'     => true,
         'mainVersion' => '2',
         'since'       => '2014-2020',
         'description' => 'has reached its end of life and is no longer supported by us.',
         'link'        => 'https://k2.getkirby.com',
         'repo'        => 'https://github.com/getkirby-v2',
     ],
-    [
+    '3.0' => [
+        'subreleases' => [
+            '3.0.3',
+            '3.0.2',
+            '3.0.1',
+        ],
+    ],
+    '3.1' => [
+        'subreleases' => [
+            '3.1.4',
+            '3.1.3',
+            '3.1.2',
+            '3.1.1',
+        ],
+    ],
+    '3.2' => [
+        'subreleases' => [
+            '3.2.5',
+            '3.2.4',
+            '3.2.3',
+            '3.2.2',
+            '3.2.1',
+        ],
+    ],
+    '3.3' => [
+        'subreleases' => [
+            '3.3.6',
+            '3.3.5',
+            '3.3.4',
+            '3.3.3',
+            '3.3.2',
+            '3.3.1',
+        ],
+    ],
+    '3.4' => [
+        'subreleases' => [
+            '3.4.5',
+            '3.4.4',
+            '3.4.3',
+            '3.4.2',
+            '3.4.1',
+        ],
+    ],
+    '3.5' => [
         'title'       => 'Kirby 3.5',
+        'hasDocs'     => true,
         'mainVersion' => '3',
         'since'       => 'Dec 2020 - Nov 2021',
         'description' => 'is not the most current version of Kirby and should not be used for new projects.',
         'link'        => 'https://v35.getkirby.com',
         'repo'        => 'https://github.com/getkirby',
+        'subreleases' => [
+            '3.5.8',
+            '3.5.7.1',
+            '3.5.7',
+            '3.5.6',
+            '3.5.5',
+            '3.5.4',
+            '3.5.3.1',
+            '3.5.3',
+            '3.5.2',
+            '3.5.1',
+        ],
     ],
-    [
+    '3.6' => [
         'title'       => 'Kirby 3.6',
+        'hasDocs'     => true,
         'mainVersion' => '3',
         'since'       => 'Nov 2021 - June 2022',
         'description' => 'is not the most current version of Kirby and should not be used for new projects.',
         'link'        => 'https://v36.getkirby.com',
         'repo'        => 'https://github.com/getkirby',
+        'subreleases' => [
+            '3.6.6.2',
+            '3.6.6.1',
+            '3.6.6',
+            '3.6.5',
+            '3.6.4',
+            '3.6.3.1',
+            '3.6.3',
+            '3.6.2',
+            '3.6.1.1',
+            '3.6.1',
+        ],
     ],
-    [
+    '3.7' => [
         'title'       => 'Kirby 3.7',
+        'hasDocs'     => true,
         'mainVersion' => '3',
         'since'       => 'June 2022 - Oct 2022',
         'description' => 'is not the most current version of Kirby and should not be used for new projects.',
         'link'        => 'https://v37.getkirby.com',
         'repo'        => 'https://github.com/getkirby',
+        'subreleases' => [
+            '3.7.5.1',
+            '3.7.5',
+            '3.7.4',
+            '3.7.3',
+            '3.7.2.1',
+            '3.7.2',
+            '3.7.1',
+            '3.7.0.2',
+            '3.7.0.1',
+        ],
     ],
-    [
+    '3.8' => [
+        'subreleases' => [
+            '3.8.1',
+            '3.8.1.1',
+            '3.8.2',
+            '3.8.3',
+            '3.8.4',
+        ],
+    ],
+    '3.9' => [
+        'title'       => 'Kirby 3.9',
+        'hasDocs'     => true,
         'title'       => 'Kirby 3.9',
         'mainVersion' => '3',
         'since'       => 'Jan 2023',
         'description' => 'is the latest version of Kirby. <br><strong class="color-black">Start new projects with this version!',
         'link'        => 'https://getkirby.com/docs',
         'repo'        => 'https://github.com/getkirby',
+        'subreleases' => [
+            '3.9.1',
+            '3.9.2',
+        ]
     ],
 ];
