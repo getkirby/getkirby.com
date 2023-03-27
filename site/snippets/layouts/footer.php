c0bf5cbe2bf70dab5b0dfd10b02d78340fbface7
Bastian Allgeier <mail@bastianallgeier.com>
2023-03-27 10:20:01 +0200

    Link to the release page in the menu and footer

--- site/snippets/layouts/footer.php
+++ site/snippets/layouts/footer.php
@@ -36,7 +36,7 @@
               <li><a href="/features/creators">For content creators</a></li>
               <li><a href="/features/clients">For clients & agencies</a></li>
               <li><a href="/love">Showcase</a></li>
-              <li><a href="https://github.com/getkirby/kirby/releases">Releases</a></li>
+              <li><a href="/releases">Releases</a></li>
 
             </ul>
           </li>
--- site/snippets/layouts/menu.php
+++ site/snippets/layouts/menu.php
@@ -14,7 +14,7 @@
           <li><a href="/features/clients">For clients & agencies</a></li>
           <li><hr /></li>
           <li><a href="/love">Showcase</a></li>
-          <li><a class="is-external" href="https://github.com/getkirby/kirby/releases">Releases</a></li>
+          <li><a class="is-external" href="/releases">Releases</a></li>
         </ul>
       </li>
       <li class="has-submenu">
