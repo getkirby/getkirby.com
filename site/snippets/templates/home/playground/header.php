ca2a2e29d9b4ff0b35330c0c202218ca7600e357
Sonja Broda <github@texniq.de>
2023-03-28 14:14:02 +0200

    Add variable in home controller

--- site/snippets/templates/home/playground/header.php
+++ site/snippets/templates/home/playground/header.php
@@ -2,9 +2,9 @@
   <div class="playground-header-title mb-24">
     <h1 class="h1">Kirby is the CMS<br>that adapts to you</h1>
     <nav class="auto-fit" style="--min: 9rem; --gap: var(--spacing-3); max-width: 24rem;">
-      <a class="btn btn--outlined" href="<?= '/releases/' . implode('.', array_slice(Str::split($kirby->version(), '.'), 0, 2) ) ?>">
+      <a class="btn btn--outlined" href="<?= '/releases/' . $kirbyVersion ?>">
         <?= icon('spaceship') ?>
-        New in <?= implode('.', array_slice(Str::split($kirby->version(), '.'), 0, 2)) ?>
+        New in <?= $kirbyVersion ?>
       </a>
       <a class="btn btn--filled" href="/try">
         <?= icon('download') ?>
