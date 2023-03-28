9604d83a8f374423aabf070101dd76bbf2b68568
Nico Hoffmann <me@nhoffmann.com>
2023-03-28 21:13:59 +0200

    Changelog: use footer slot

--- site/templates/changelog.php
+++ site/templates/changelog.php
@@ -19,45 +19,45 @@
   </div>
 </nav>
 <?php endslot() ?>
-<?php slot('header') ?>
 
-<header>
-  <h1 class="h1 mb-12"><?= $page->title() ?></h1>
-</header>
-  <section>
-    <?php foreach ($releases as $release): ?>
-      <article class="mb-12" style="--span: 12">
-        <header class="mb-6">
-          <h2 class="h2" id="<?= 'version-' . $release->version()->slug() ?>"
-              class="h2">Kirby <?= $release->version() ?></h2>
-        </header>
+<?php slot() ?>
+<section>
+  <?php foreach ($releases as $release): ?>
+    <article class="mb-12" style="--span: 12">
+      <header class="mb-6">
+        <h2 class="h2" id="<?= 'version-' . $release->version()->slug() ?>"
+            class="h2">Kirby <?= $release->version() ?></h2>
+      </header>
+      <div class="mb-12">
+        <h3 class="h3 mb-6">Breaking</h3>
+        <div class="prose text-sm">
+          <?= (new Kirby\Cms\Field(
+            $release,
+            'breaking',
+            str_replace(
+              '(docs: releases/breaking-changes vars: version=' . substr($release->version()->value(), 2, 1) . ')',
+              '',
+              $release->breaking()
+            )))->kt() ?>
+        </div>
+      </div>
+      <?php if ($release->deprecated()->isNotEmpty()): ?>
         <div class="mb-12">
-          <h3 class="h3 mb-6">Breaking</h3>
+          <h3 class="h3 mb-6">Deprecated</h3>
           <div class="prose text-sm">
-            <?= (new Kirby\Cms\Field(
-              $release,
-              'breaking',
-              str_replace(
-                '(docs: releases/breaking-changes vars: version=' . substr($release->version()->value(), 2, 1) . ')',
-                '',
-                $release->breaking()
-              )))->kt() ?>
+            <?= $release->deprecated()->kt() ?>
           </div>
         </div>
-        <?php if ($release->deprecated()->isNotEmpty()): ?>
-          <div class="mb-12">
-            <h3 class="h3 mb-6">Deprecated</h3>
-            <div class="prose text-sm">
-              <?= $release->deprecated()->kt() ?>
-            </div>
-          </div>
-        <?php endif ?>
-      </article>
-    <?php endforeach ?>
-  </section>
-  <footer class="h2 max-w-xl">
-    Full list of features of <a href="/releases"><span class="link">all releases</span> &rarr;</a>
-  </footer>
+      <?php endif ?>
+    </article>
+  <?php endforeach ?>
+</section>
+<?php endslot() ?>
+
+<?php slot('footer') ?>
+<footer class="h2 max-w-xl">
+  Full list of features of <a href="/releases"><span class="link">all releases</span> &rarr;</a>
+</footer>
 <?php endslot() ?>
 
 
