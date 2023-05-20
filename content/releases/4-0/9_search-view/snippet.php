8d4a30d32f37bf60ab849e152375461b02cfd339
Sonja Broda <github@texniq.de>
2023-05-20 14:34:32 +0200

    Use teaser in snippet

--- content/releases/4-0/9_search-view/snippet.php
+++ content/releases/4-0/9_search-view/snippet.php
@@ -1,3 +1,10 @@
-<figure class="release-box bg-light shadow-xl">
-	<?= $section->image('search-view.png') ?>
-</figure>
+<div class="columns" style="--columns: 4">
+	<div class="flex flex-column">
+		<div class="release-padded-box flex-grow bg-light">
+			<?= $section->teaser()->kt() ?>
+		</div>
+	</div>
+	<figure class="release-box bg-light shadow-xl" style="--span: 3">
+		<?= $section->image('search-view.png') ?>
+	</figure>
+</div>
