687d27e5a9003248c52f35c5f0a5ea38787f47c5
Nico Hoffmann <me@nhoffmann.com>
2023-05-23 21:23:25 -0700

    Fix `mark` bg color

--- site/templates/release-4.0.php
+++ site/templates/release-4.0.php
@@ -3,64 +3,68 @@
 <?= css('assets/css/layouts/releases.css') ?>
 
 <style>
-.release-box,
-.release-code-box,
-.release-padded-box,
-.release-text-box {
-	border-radius: .5rem;
-	overflow: hidden;
-	z-index: 1;
-}
+	.release-box,
+	.release-code-box,
+	.release-padded-box,
+	.release-text-box {
+		border-radius: .5rem;
+		overflow: hidden;
+		z-index: 1;
+	}
 
-.release-code-box {
-	display: grid;
-	align-items: stretch;
-	background: var(--color-black);
-}
+	.release-code-box {
+		display: grid;
+		align-items: stretch;
+		background: var(--color-black);
+	}
 
-.release-code-box .code-toolbar {
-	display: grid;
-}
+	.release-code-box .code-toolbar {
+		display: grid;
+	}
 
-.release-padded-box,
-.release-text-box {
-	padding: var(--spacing-8);
-}
+	.release-padded-box,
+	.release-text-box {
+		padding: var(--spacing-8);
+	}
 
-.release-text-box {
-	background: var(--color-light);
-}
+	.release-text-box {
+		background: var(--color-light);
+	}
 
-.release-padded-box h3,
-.release-text-box h3 {
-	font-weight: var(--font-bold);
-	font-size: var(--text-lg);
-}
+	.release-padded-box h3,
+	.release-text-box h3 {
+		font-weight: var(--font-bold);
+		font-size: var(--text-lg);
+	}
 
-.release-text-box .prose {
-	font-size: var(--text-lg);
-}
+	.release-text-box .prose {
+		font-size: var(--text-lg);
+	}
 
-.btn {
-	border-radius: var(--rounded);
-}
+	.btn {
+		border-radius: var(--rounded);
+	}
+
+	mark {
+		background: var(--color-yellow-300);
+	}
 </style>
 
 <header class="mb-12 flex items-end justify-between release-header">
-  <div>
-    <h1 class="h1"><?= $page->title() ?></h1>
-    <p class="h1 color-gray-600"><?= $page->subtitle() ?></p>
-  </div>
-  <?php snippet('templates/releases/cta', [
-    'options' => ['center' => false, 'mb' => 0]
-  ]) ?>
+	<div>
+		<h1 class="h1"><?= $page->title() ?></h1>
+		<p class="h1 color-gray-600"><?= $page->subtitle() ?></p>
+	</div>
+	<?php snippet('templates/releases/cta', [
+		'options' => ['center' => false, 'mb' => 0]
+	]) ?>
 </header>
 
 <article class="release-wrapper">
 	<?php snippet('templates/release-40/intro') ?>
 	<?php snippet('templates/release-40/roadmap') ?>
 
-	<?php foreach ($sections as $section): ?>
+	<?php foreach ($sections as $section) : ?>
 		<?php snippet([
 			'templates/release-40/' . $section->slug(),
 			'templates/release-40/section'
@@ -69,18 +73,18 @@
 
 	<?php snippet('templates/release-40/versioning') ?>
 
-  <section id="faq" class="mb-42">
-    <?php snippet('templates/features/intro', [
-      'title' => 'Frequently asked questions',
-      'text'  => 'We are getting a lot of questions after our announcement and already tried to answer most of them here. We will keep collecting questions from the community here and try to answer them as transparently as possible. Let us know on Discord or in the Forum if you have additional questions.'
-    ]) ?>
-    <?php snippet('faq') ?>
-  </section>
+	<section id="faq" class="mb-42">
+		<?php snippet('templates/features/intro', [
+			'title' => 'Frequently asked questions',
+			'text'  => 'We are getting a lot of questions after our announcement and already tried to answer most of them here. We will keep collecting questions from the community here and try to answer them as transparently as possible. Let us know on Discord or in the Forum if you have additional questions.'
+		]) ?>
+		<?php snippet('faq') ?>
+	</section>
 
 	<?php snippet('templates/release-40/changes') ?>
 
 	<?php snippet('templates/releases/get-started') ?>
-  <?php snippet('templates/release-40/release-menu') ?>
+	<?php snippet('templates/release-40/release-menu') ?>
 
 </article>
 
