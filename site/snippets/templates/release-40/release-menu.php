ddc7fe6f9ae79b69ceeefae1da75dc7bd6fdfd3b
Bastian Allgeier <mail@bastianallgeier.com>
2023-05-24 12:48:04 +0200

    Better menu

--- site/snippets/templates/release-40/release-menu.php
+++ site/snippets/templates/release-40/release-menu.php
@@ -2,10 +2,10 @@
   <ul class="font-mono text-sm pt-6 sticky" style="--top: var(--spacing-2)">
     <li><a href="#roadmap">Roadmap</a></li>
 		<?php foreach ($sections as $section): ?>
-    <li><a href="#<?= $section->slug() ?>"><?= $section->title() ?></a></li>
+    <li><a href="#<?= $section->slug() ?>"><?= $section->menu()->or($section->title()) ?></a></li>
 		<?php endforeach ?>
-    <li><a href="#versioning">New Versioning Scheme</a></li>
-    <li><a href="#faq">Frequently Asked Questions</a></li>
+    <li><a href="#versioning">Versioning Scheme</a></li>
+    <li><a href="#faq">FAQs</a></li>
     <li><a href="#changes">Changelog</a></li>
   </ul>
 </nav>
