7398fa277971f6d9dea5d9473f632825bab0ae0c
Bastian Allgeier <mail@bastianallgeier.com>
2023-05-25 12:29:38 +0200

    Fix block videos

--- site/plugins/site/extensions/tags.php
+++ site/plugins/site/extensions/tags.php
@@ -324,4 +324,90 @@ $tags['section-options'] = [
 	}
 ];
 
+$tags['video'] = [
+	'attr' => [
+		'autoplay',
+		'caption',
+		'controls',
+		'class',
+		'height',
+		'loop',
+		'muted',
+		'playsinline',
+		'poster',
+		'preload',
+		'style',
+		'width',
+	],
+	'html' => function ($tag) {
+		// checks and gets if poster is local file
+		if (
+			empty($tag->poster) === false &&
+			Str::startsWith($tag->poster, 'http://') !== true &&
+			Str::startsWith($tag->poster, 'https://') !== true
+		) {
+			if ($poster = $tag->file($tag->poster)) {
+				$tag->poster = $poster->url();
+			}
+		}
+
+		// checks video is local or provider(remote)
+		$isLocalVideo = (
+			Str::startsWith($tag->value, 'http://') !== true &&
+			Str::startsWith($tag->value, 'https://') !== true
+		);
+		$isProviderVideo = (
+			$isLocalVideo === false &&
+			(
+				Str::contains($tag->value, 'youtu', true) === true ||
+				Str::contains($tag->value, 'vimeo', true) === true
+			)
+		);
+
+		// default attributes for local and remote videos
+		$attrs = [
+			'height' => $tag->height,
+			'width'  => $tag->width
+		];
+
+		// don't use attributes that iframe doesn't support
+		if ($isProviderVideo === false) {
+			// converts tag attributes to supported formats (listed below) to output correct html
+			// booleans: autoplay, controls, loop, muted
+			// strings : poster, preload
+			// for ex  : `autoplay` will not work if `false` is a `string` instead of a `boolean`
+			$attrs['autoplay']    = $autoplay = Str::toType($tag->autoplay, 'bool');
+			$attrs['controls']    = Str::toType($tag->controls ?? true, 'bool');
+			$attrs['loop']        = Str::toType($tag->loop, 'bool');
+			$attrs['muted']       = Str::toType($tag->muted ?? $autoplay, 'bool');
+			$attrs['playsinline'] = Str::toType($tag->playsinline ?? $autoplay, 'bool');
+			$attrs['poster']      = $tag->poster;
+			$attrs['preload']     = $tag->preload;
+		}
+
+		// handles local and remote video file
+		if ($isLocalVideo === true) {
+			// handles local video file
+			if ($tag->file = $tag->file($tag->value)) {
+				$video = Html::video(
+					$tag->file->url(),
+					$tag->kirby()->option('kirbytext.video.options', []),
+					$attrs
+				);
+			}
+		} else {
+			$video = Html::video(
+				$tag->value,
+				$tag->kirby()->option('kirbytext.video.options', []),
+				$attrs
+			);
+		}
+
+		return Html::figure([$video ?? ''], $tag->caption, [
+			'class' => $tag->class ?? 'video',
+			'style' => $tag->style
+		]);
+	}
+];
+
 return $tags;
