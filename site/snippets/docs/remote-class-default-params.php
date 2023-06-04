b0beb9e15438cf3ae0b52fae8bb38e5ea2a53e37
Lukas Bestle <lukas@getkirby.com>
2023-06-04 18:10:09 +0200

    Fix remote class default params table
    Closes #2041

--- site/snippets/docs/remote-class-default-params.php
+++ site/snippets/docs/remote-class-default-params.php
@@ -1,14 +1,14 @@
 | Key | Type | Default | Description
 | ---- | ---- | ---- | ----
 | agent | `string` | `null` | The user agent string to be sent with the HTTP request
-| basicAuth | `string` | `null` | User name and password to use in authentication in format `USERNAME:PASSWORD`
+| basicAuth | `string` | `null` | User name and password to use for the `Authorization` header (formatted as `USERNAME:PASSWORD`)
 | body | `bool` | `true` | When `true` returns transfer as string instead of outputting it directly
-| ca | `int`|`bool`|`string | Remote::CA_INTERNAL | TLS CA to use, to disable SSL verification, set to `false`
+| ca | `int|bool|string` | `Remote::CA_INTERNAL` | TLS CA to use, (link: docs/reference/system/options/remote#configuring-the-list-of-allowed-certificate-authorities-cas-for-https-requests text: see details)
 | data | `array` | `[]`  | The data to be sent with the request
 | encoding | `string` | `utf-8` | Accepted values: `null`, `''`, `identity`, `gzip`, `br`
-| file | `string` | null | Path to file to be uploaded
-| headers | `array` | `[]` | Array of headers to be sent tiwth the request
-| method | `string` | `GET` | HTTP method (`GET`, `POST`, `PUT`, `PATCH`, `DELETE`, `HEAD`
+| file | `string` | `null` | Path to file to be uploaded
+| headers | `array` | `[]` | Array of headers to be sent with the request
+| method | `string` | `GET` | HTTP method (`GET`, `POST`, `PUT`, `PATCH`, `DELETE`, `HEAD`)
 | progress | `callback` | `null` | A callback which accepts 5 parameters, if you want to handle upload/download progress
-| test | `bool` | `false` | When set to true, the curl request is not initiated, and the Remote object returned
-| timeout | `int` | `100` | Request/connect timeout
+| test | `bool` | `false` | When set to `true`, the cURL request is not initiated, and the `Remote` object returned
+| timeout | `int` | `10` | Request/connect timeout
