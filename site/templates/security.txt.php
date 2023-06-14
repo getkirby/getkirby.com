e7da497587b9013fdfb0c7760a189531014fffc0
Bastian Allgeier <mail@bastianallgeier.com>
2023-06-14 10:18:17 +0200

    Add a security.txt

---
+++ site/templates/security.txt.php
@@ -0,0 +1,7 @@
+Contact: mailto:security@getkirby.com
+Expires: <?= date(DATE_ATOM, strtotime('+1 year')) . "\n" ?>
+Encryption: https://getkirby.com/pgp.asc
+Acknowledgments: https://getkirby.com/security
+Preferred-Languages: en
+Canonical: https://getkirby.com/security.txt
+Policy: https://getkirby.com/security
