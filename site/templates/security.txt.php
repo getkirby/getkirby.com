d6792d51aeab6ed31712f9aabd3d87ed706f51f5
Lukas Bestle <lukas@getkirby.com>
2023-06-14 21:20:30 +0200

    Sign security.txt

--- site/templates/security.txt.php
+++ site/templates/security.txt.php
@@ -1,7 +1,28 @@
-Contact: mailto:security@getkirby.com
-Expires: <?= date(DATE_ATOM, strtotime('+1 year')) . "\n" ?>
-Encryption: https://getkirby.com/pgp.asc
-Acknowledgments: https://getkirby.com/security
-Preferred-Languages: en
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA512
+
 Canonical: https://getkirby.com/security.txt
+Contact: mailto:security@getkirby.com
+Encryption: https://getkirby.com/pgp.asc
+Preferred-Languages: en
+Acknowledgments: https://getkirby.com/security
 Policy: https://getkirby.com/security
+Expires: 2024-06-14T19:02:41Z
+-----BEGIN PGP SIGNATURE-----
+
+iQKTBAEBCgB9FiEEuBUhCqAlKL6Zbc2gwN3Pl5VS7dEFAmSKEsdfFIAAAAAALgAo
+aXNzdWVyLWZwckBub3RhdGlvbnMub3BlbnBncC5maWZ0aGhvcnNlbWFuLm5ldEI4
+MTUyMTBBQTAyNTI4QkU5OTZEQ0RBMEMwRERDRjk3OTU1MkVERDEACgkQwN3Pl5VS
+7dFlAQ//X8frY2t+pF5uwagE1v3arzBqJGfds0qG7daOohAOK+FvqbIdrR99AEfH
+OZ+R+hmsxBbxbZkRIFqzwUMgQrZW6DLAhJs32ASqd3FfQLHZxQUMCgkBFMkkGqnr
+fXY8k8I+cwexrFC3O0oiYFXp1IdO60cFKxhnajExLPa779x1+RK3sY7RNayJe1hA
+O3spYQyJzgeasBAinY5/3Gr8qcRwvB4dw4DkVdcvWS3mq0PHnbRFbfqAGLRgpj9D
+ufOIIhmWE6licCAIpuKhPuAGrsX7iBMLC922RSZDUo1dWRoD+F7q9vjGq/UYKpxc
+VNxdKLSBaSNkY3FiOeleJd5Pb5aFlOybhqX5xh9oILOqduVLevRlbsqwFi/+Lp12
+zhLhRqMHyRrWR9qXl0yicUWQshTzRs1mKNeI/pfegbKAsOzTqqvsFufe2bKeE/JI
+Gz9u9y2yzX1g6siJqTETzXxQKscJS2rg6C0jdV8DB96ig+/m7FaQ1kERYew4vkWL
+3wlJyT5Bv8sp/P/wXgUTVqVszTJLFr8Tw1L4+jJl3EfLULNywhn8yxDCnd5TU1HE
+kmqBogB1K4mADSt+Blvi4UiNRjEpI+vIW40qj+xAOAvTqM3SjDT73pHpn5ZPAWcB
+LqsquOXvmJB/NLf5pQqmevmbyAUSOBQQ8FmmApo7Y2NJXIasR6w=
+=o3Dx
+-----END PGP SIGNATURE-----
