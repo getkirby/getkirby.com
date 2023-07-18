fed45e6a3a4e3f1954b6ebb097290e876e2091b2
Bastian Allgeier <mail@bastianallgeier.com>
2023-07-18 18:04:27 +0200

    New design

---
+++ content/releases/4-0/0_design/snippet.php
@@ -0,0 +1,10 @@
+<div class="columns" style="--columns: 1">
+
+	<figure class="release-box bg-light shadow-xl">
+		<?= $section->image('design.png')->resize(1400) ?>
+	</figure>
+
+	<div class="release-text-box">
+		<?php snippet('templates/release-40/teaser', ['section' => $section]) ?>
+	</div>
+</div>
