242784c783c41139d78065005bbfbac8264b8a1d
Bastian Allgeier <mail@bastianallgeier.com>
2023-07-19 10:56:02 +0200

    New screenshots

--- content/releases/4-0/0_design/snippet.php
+++ content/releases/4-0/0_design/snippet.php
@@ -1,14 +1,16 @@
 <div class="columns" style="--columns: 2">
 	<figure class="release-box bg-light shadow-xl" style="--span: 2">
-		<?= $section->image('design.png')->resize(1400) ?>
+		<?= $section->image('blog.png')->resize(1600) ?>
 	</figure>
 
 	<div class="release-text-box prose text-base">
 		<h3 class="mb-6">Editor features</h3>
 		<?= $section->editors()->kt() ?>
+		<p><a href="<?= $section->url() ?>">Read more …</a></p>
 	</div>
 	<div class="release-text-box prose text-base">
 		<h3 class="mb-6">Developer features</h3>
 		<?= $section->developers()->kt() ?>
+		<p><a href="<?= $section->url() ?>">Read more …</a></p>
 	</div>
 </div>
