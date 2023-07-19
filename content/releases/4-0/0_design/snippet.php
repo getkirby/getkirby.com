050f51eb773a13fe081e7dc58c1f7914b755c205
Bastian Allgeier <mail@bastianallgeier.com>
2023-07-19 10:36:57 +0200

    More screenshots and design text

--- content/releases/4-0/0_design/snippet.php
+++ content/releases/4-0/0_design/snippet.php
@@ -1,10 +1,14 @@
-<div class="columns" style="--columns: 1">
-
-	<figure class="release-box bg-light shadow-xl">
+<div class="columns" style="--columns: 2">
+	<figure class="release-box bg-light shadow-xl" style="--span: 2">
 		<?= $section->image('design.png')->resize(1400) ?>
 	</figure>
 
-	<div class="release-text-box">
-		<?php snippet('templates/release-40/teaser', ['section' => $section]) ?>
+	<div class="release-text-box prose text-base">
+		<h3 class="mb-6">Editor features</h3>
+		<?= $section->editors()->kt() ?>
+	</div>
+	<div class="release-text-box prose text-base">
+		<h3 class="mb-6">Developer features</h3>
+		<?= $section->developers()->kt() ?>
 	</div>
 </div>
