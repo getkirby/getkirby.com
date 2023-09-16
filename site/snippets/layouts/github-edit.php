80b8aecde9f6bee863521c6fa16f35453de055f2
Nico Hoffmann <me@nhoffmann.com>
2023-09-16 20:03:16 +0200

    Fix github edit links

--- site/snippets/layouts/github-edit.php
+++ site/snippets/layouts/github-edit.php
@@ -4,6 +4,13 @@
   Did you find an error? Help us improve our docs and edit this page on Github. Make sure to check out
   <a href="/styleguide">our styleguide &rarr;</a>
 </p>
-<a href="<?= option('github.url') ?>/getkirby.com/<?= F::exists($page->contentFile()) ? 'edit/main/content/' . $page->diruri() . '/' . $page->intendedTemplate() . '.txt' : 'issues/new?template=reference.md' ?>" class="btn btn--outlined mb-3">
-  <?= icon('github') ?> <?= F::exists($page->contentFile()) ? 'Edit this page' : 'Report an issue on this page' ?>
+
+<?php if (F::exists($page->root() . '/' . $page->intendedTemplate() . '.txt')): ?>
+<a href="<?= option('github.url') ?>/getkirby.com/edit/main/content/<?= $page->diruri() ?>/<?= $page->intendedTemplate() ?>.txt" class="btn btn--outlined mb-3">
+  <?= icon('github') ?> Edit this page
 </a>
+<?php else: ?>
+<a href="<?= option('github.url') ?>/getkirby.com/issues/new?template=reference.md" class="btn btn--outlined mb-3">
+  <?= icon('github') ?> Report an issue on this page
+</a>
+<?php endif ?>
