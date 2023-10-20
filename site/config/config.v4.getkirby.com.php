b01c0284c070b1cf06a1e30b6e5c79a11e0697f2
Bastian Allgeier <mail@bastianallgeier.com>
2023-10-20 16:52:43 +0200

    New noindex option

---
+++ site/config/config.v4.getkirby.com.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+	'noindex' => true
+];
--- site/snippets/layouts/head.php
+++ site/snippets/layouts/head.php
@@ -7,7 +7,7 @@ extract([
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<?php if (option('archived') === true): ?>
+<?php if (option('noindex') === true): ?>
 <meta name="robots" content="noindex">
 <?php endif ?>
 
