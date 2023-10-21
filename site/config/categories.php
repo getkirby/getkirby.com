790996a7c08a2ca95621452eee4e9fde417d0e6c
Sonja Broda <github@texniq.de>
2023-10-21 16:21:08 +0200

    Change guide titles

---
+++ site/config/categories.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'basics'        => 'Preparing the stage',
+	'data'          => 'Structure & Semantics',
+	'configuration' => 'Configuration',
+	'privacy'       => 'Privacy & Security'
+];
