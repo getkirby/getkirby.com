d339dc2e90f057f4a5da03a533fd063e67053220
Sonja Broda <github@texniq.de>
2023-10-21 16:24:35 +0200

    Change guide structure

--- site/templates/guides.php
+++ site/templates/guides.php
@@ -1,26 +1,35 @@
 <?php layout() ?>
 
 <div class="mb-36">
-  <h1 class="h1 mb-12">Guide</h1>
-  <ul class="guides auto-fill auto-rows-fr mb-12" style="--min: 16rem; --gap: var(--spacing-12)">
-    <?php foreach ($page->children()->listed() as $guide): ?>
-    <li>
-      <article>
-        <a class="block" href="<?= $guide->url() ?>">
-          <?php if ($svg = $guide->images()->findBy('extension', 'svg')): ?>
-            <figure class="mb-3" style="--size: 4rem">
-              <?= $svg->read() ?>
-            </figure>
-          <?php endif ?>
-          <div class="border-top pt-3">
-            <h2 class="h2 mb-3"><?= $guide->title() ?></h2>
-            <p class="color-gray-700"><?= $guide->description() ?></p>
-          </div>
-        </a>
-      </article>
-    </li>
-    <?php endforeach ?>
-  </ul>
+
+  <h1 class="h1 mb-24">Guide</h1>
+	<?php foreach (collection('guides')->group('category') as $category => $guides): ?>
+	<section>
+		<header>
+			<h2 class="h2 mb-24" style="font-size: var(--text-4xl)"><?= option('categories')[$category] ?? ucfirst($category) ?></h2>
+		</header>
+		<ul class="guides auto-fill auto-rows-fr mb-24" style="--min: 16rem; --gap: var(--spacing-12)">
+			<?php foreach ($guides as $guide): ?>
+				<li>
+					<article>
+						<a class="block" href="<?= $guide->url() ?>">
+							<?php if ($svg = $guide->images()->findBy('extension', 'svg')): ?>
+								<figure class="mb-3" style="--size: 4rem">
+									<?= $svg->read() ?>
+								</figure>
+							<?php endif ?>
+							<div class="border-top pt-3">
+								<h3 class="h2 mb-3"><?= $guide->title() ?></h3>
+								<p class="color-gray-700"><?= $guide->description() ?></p>
+							</div>
+						</a>
+					</article>
+				</li>
+			<?php endforeach ?>
+		</ul>
+	</section>
+
+	<?php endforeach ?>
 </div>
 
 <footer class="h2 max-w-xl">
