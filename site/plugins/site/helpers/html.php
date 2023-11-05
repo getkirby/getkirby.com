baae4336deb09937b8c9f95e0ec63dc999ce7bdb
Nico Hoffmann <me@nhoffmann.com>
2023-11-05 20:01:55 +0100

    Fix Panel icons with `<use>`

--- site/plugins/site/helpers/html.php
+++ site/plugins/site/helpers/html.php
@@ -13,15 +13,25 @@ function ariaCurrent(
 
 function icon(string $name): string|false
 {
+	// prefer custom icon files from assets folder
 	if ($svg = svg('assets/icons/' . $name . '.svg')) {
 		return $svg;
 	}
 
+	// fall back to Panel icons
 	static $panel;
 	$panel ??= svg('kirby/panel/dist/img/icons.svg');
 
 	if ($panel) {
+		// find the icon in the Panel sprite
 		if (preg_match('/<symbol[^>]*id="icon-' . $name . '"[^>]*viewBox="(.*?)"[^>]*>(.*?)<\/symbol>/s', $panel, $matches)) {
+
+			//  resolve <use> tags to full inline SVG
+			if (preg_match('/<use href="#icon-(.*?)"[^>]*?>/s', $matches[2], $use)) {
+				return icon($use[1]);
+			}
+
+			// return the icon with the correct viewBox
 			return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="' . $matches[1] . '">' . $matches[2] . '</svg>';
 		}
 	}
