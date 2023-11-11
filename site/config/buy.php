f250fa73035aed03a27545f3e7d21138426dd506
Nico Hoffmann <me@nhoffmann.com>
2023-11-11 17:16:35 +0100

    Remove hub price points from website config

--- site/config/buy.php
+++ site/config/buy.php
@@ -3,14 +3,10 @@
 return [
 	'basic' => [
 		'id'      => 824338,
-		'min'     => 29,
-		'nudge'   => 59,
 		'regular' => 99,
 	],
   	'enterprise' => [
 		'id'      => 824340,
-		'min'     => 199,
-		'nudge'   => 249,
 		'regular' => 399,
 	],
 	'sale' => [
