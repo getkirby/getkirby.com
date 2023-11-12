0b014564b4fb48de1009becb85e86025ab926783
Nico Hoffmann <me@nhoffmann.com>
2023-11-12 23:44:10 +0100

    Update Kosmos reader no

--- site/templates/kosmos-issue.php
+++ site/templates/kosmos-issue.php
@@ -44,7 +44,7 @@
 
 <?php slot('footer') ?>
 <footer>
-  <h2 class="h2 mb-6">Join 3,000+ readers</h2>
+  <h2 class="h2 mb-6">Join 3,500+ readers</h2>
   <?php snippet('templates/kosmos/form') ?>
 </footer>
 <?php endslot() ?>
