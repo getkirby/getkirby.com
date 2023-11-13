35a3d6f1942609088c9c4d53d198845d402671c3
Nico Hoffmann <me@nhoffmann.com>
2023-11-13 20:59:46 +0100

    Sale banner: add border

--- site/snippets/layouts/sale.php
+++ site/snippets/layouts/sale.php
@@ -3,7 +3,7 @@ $sale = new Buy\Sale();
 ?>
 
 <?php if ($page->id() !== 'buy' && $sale->isActive() === true): ?>
-<aside class="banner text-sm rounded shadow-xl">
+<aside class="banner text-sm rounded shadow-lg">
   <a href="<?= url('buy') ?>"><?= $sale->text() ?></a>
 </aside>
 
