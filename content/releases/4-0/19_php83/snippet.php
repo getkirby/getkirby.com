31c8f48c6999304a6f1d91053fa4c6202d95e17f
Bastian Allgeier <mail@bastianallgeier.com>
2023-11-13 12:40:09 +0100

    Add PHP 8.3 to release page

---
+++ content/releases/4-0/19_php83/snippet.php
@@ -0,0 +1,12 @@
+<style>
+  .v4-php83 > a {
+    display: block;
+  }
+</style>
+
+<figure class="v4-php83 release-box bg-dark p-24 color-white grid place-items-center">
+	<a href="https://www.php.net/releases/8.3/en.php">
+		<?= $section->image('php-8.3.svg')->read() ?>
+	</a>
+</figure>
+
