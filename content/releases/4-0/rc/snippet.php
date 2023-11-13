770e7ef98c7257a9d39bcc76d5612714d52b956d
Bastian Allgeier <mail@bastianallgeier.com>
2023-11-13 12:10:45 +0100

    Fixes for the release page

--- content/releases/4-0/rc/snippet.php
+++
@@ -1,8 +0,0 @@
-<div class="columns" style="--columns: 2">
-	<div class="release-text-box">
-		<?php snippet('templates/release-40/teaser', ['section' => $section]) ?>
-	</div>
-	<div class="release-text-box prose text-lg" style="color: black; background: var(--color-orange-300)">
-		<?= $section->warning()->kt() ?>
-	</div>
-</div>
--- site/snippets/templates/release-40/release-menu.php
+++ site/snippets/templates/release-40/release-menu.php
@@ -1,11 +1,9 @@
 <nav class="release-menu">
   <ul class="font-mono text-sm pt-6 sticky" style="--top: var(--spacing-2)">
-    <li><a href="#roadmap">Roadmap</a></li>
 		<?php foreach ($sections as $section): ?>
     <li><a href="#<?= $section->slug() ?>"><?= $section->menu()->or($section->title()) ?></a></li>
 		<?php endforeach ?>
     <li><a href="#versioning">Versioning Scheme</a></li>
-    <li><a href="#faq">FAQs</a></li>
     <li><a href="#changes">Changelog</a></li>
   </ul>
 </nav>
