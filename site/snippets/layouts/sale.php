159cae3052dda1527afa9edca4ceb6fa1d6a9e95
Bastian Allgeier <mail@bastianallgeier.com>
2023-11-13 13:19:20 +0100

    Slightly clearer shadow for the sales banner

--- site/snippets/layouts/sale.php
+++ site/snippets/layouts/sale.php
@@ -3,7 +3,7 @@ $sale = new Buy\Sale();
 ?>
 
 <?php if ($page->id() !== 'buy' && $sale->isActive() === true): ?>
-<aside class="banner text-sm">
+<aside class="banner text-sm rounded shadow-xl">
   <a href="<?= url('buy') ?>"><?= $sale->text() ?></a>
 </aside>
 
