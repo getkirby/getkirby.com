2353b4c38cc62205d403b2934236678b72424d75
Nico Hoffmann <me@nhoffmann.com>
2023-11-30 09:58:20 +0800

    Revert "Cloudflare IP: test different header spelling"
    This reverts commit 0116620334d062571c6edb7e8659eceb1291be81.

--- site/plugins/buy/Visitor.php
+++ site/plugins/buy/Visitor.php
@@ -147,7 +147,7 @@ class Visitor
 
 		// if the site is served via Cloudflare, use the proxied IP header
 		if (option('cloudflare', false) === true) {
-			return $env->get('CF-CONNECTING-IP', '');
+			return $env->get('CF_CONNECTING_IP', '');
 		}
 
 		// otherwise use the direct IP header
