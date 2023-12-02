27d0b76fcfda8207749a2b95fb46da50c5253164
Lukas Bestle <lukas@getkirby.com>
2023-12-02 20:16:07 +0100

    Update and extend PGP key and security.txt

--- site/templates/security.txt.php
+++ site/templates/security.txt.php
@@ -3,26 +3,27 @@ Hash: SHA512
 
 Canonical: https://getkirby.com/security.txt
 Contact: mailto:security@getkirby.com
+Contact: https://github.com/getkirby/kirby/security/advisories/new
 Encryption: https://getkirby.com/pgp.asc
 Preferred-Languages: en
-Acknowledgments: https://getkirby.com/security
-Policy: https://getkirby.com/security
-Expires: 2024-06-14T19:02:41Z
+Acknowledgments: https://getkirby.com/security#past-security-incidents
+Policy: https://getkirby.com/security#reporting-a-vulnerability
+Expires: 2025-01-01T12:00:00.000Z
 -----BEGIN PGP SIGNATURE-----
 
-iQKTBAEBCgB9FiEEuBUhCqAlKL6Zbc2gwN3Pl5VS7dEFAmSKEsdfFIAAAAAALgAo
+iQKTBAEBCgB9FiEEuBUhCqAlKL6Zbc2gwN3Pl5VS7dEFAmVrgiZfFIAAAAAALgAo
 aXNzdWVyLWZwckBub3RhdGlvbnMub3BlbnBncC5maWZ0aGhvcnNlbWFuLm5ldEI4
 MTUyMTBBQTAyNTI4QkU5OTZEQ0RBMEMwRERDRjk3OTU1MkVERDEACgkQwN3Pl5VS
-7dFlAQ//X8frY2t+pF5uwagE1v3arzBqJGfds0qG7daOohAOK+FvqbIdrR99AEfH
-OZ+R+hmsxBbxbZkRIFqzwUMgQrZW6DLAhJs32ASqd3FfQLHZxQUMCgkBFMkkGqnr
-fXY8k8I+cwexrFC3O0oiYFXp1IdO60cFKxhnajExLPa779x1+RK3sY7RNayJe1hA
-O3spYQyJzgeasBAinY5/3Gr8qcRwvB4dw4DkVdcvWS3mq0PHnbRFbfqAGLRgpj9D
-ufOIIhmWE6licCAIpuKhPuAGrsX7iBMLC922RSZDUo1dWRoD+F7q9vjGq/UYKpxc
-VNxdKLSBaSNkY3FiOeleJd5Pb5aFlOybhqX5xh9oILOqduVLevRlbsqwFi/+Lp12
-zhLhRqMHyRrWR9qXl0yicUWQshTzRs1mKNeI/pfegbKAsOzTqqvsFufe2bKeE/JI
-Gz9u9y2yzX1g6siJqTETzXxQKscJS2rg6C0jdV8DB96ig+/m7FaQ1kERYew4vkWL
-3wlJyT5Bv8sp/P/wXgUTVqVszTJLFr8Tw1L4+jJl3EfLULNywhn8yxDCnd5TU1HE
-kmqBogB1K4mADSt+Blvi4UiNRjEpI+vIW40qj+xAOAvTqM3SjDT73pHpn5ZPAWcB
-LqsquOXvmJB/NLf5pQqmevmbyAUSOBQQ8FmmApo7Y2NJXIasR6w=
-=o3Dx
+7dHcTRAAjmp1/0BFRg0HeJ5xE1zPJbrHxZXdKC9WgC3VWIonP1ZIdNf8a46oLxG9
+8yYrOP+bHaBnZ8xK8RaUR/6VKwvrfWWj8sQ8jvm9XnPyJFdfGh5IiG4g+w7Mx3d2
+cE8rcML7QFVuy9qteoZZIZSwmt+OWfB1XbCczq27dzp6LMs7R5vbIXYnz+VwdKcV
+8a04QRvIHxvpzQ1DWClXxUIZhUV/1YzekRa2LNxFJmZAEgRtzTtb1ORqsvmLiUfi
+LfpU8S8j5h+oLoUKRRK1GvHhY5YU7MjF//zyPDr7Fd1ipnnFTux8jwRfzH5KSboH
+tKOwBBEAfhWgad0lxF79U4naoA+WDGDr4AsGSx8z0RcDp4sZzKR6yzzlLrOBvavr
+ey4F3ToFbDow9wnnx4B9A7gqxEkk0PTuqL/6VgD5fHHK2Hvr3SBV5ktuvvkgkWoS
+xVwBGnPMda3F+79s1bfCJMLtJseBytLXXdCZNtPKk7JyDGGeLUyDJGLcIp6LgQlT
+nMendq8nfuzM6Dvcx1tbRoR5h0NJ+Ti8rprVCTmQXCsfDJndrgUeaUYbluhE+cr4
+UtGOiQeExDBryfzznE+Mp8OOAJ6ZKPMVsXTKzKbGmYdj2xJatM6I9Ot5q6UK19wk
+9kxcIt9RUiOUVES0T4mkxAIBe/p48ap6LHLtxMMFzwIARxrb/cA=
+=rowy
 -----END PGP SIGNATURE-----
