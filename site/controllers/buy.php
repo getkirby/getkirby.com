d944873bd84672108dbab46023dd6a0415f87281
Lukas Bestle <lukas@getkirby.com>
2023-12-18 22:26:05 +0100

    Expire buy page cache on sales

--- site/controllers/buy.php
+++ site/controllers/buy.php
@@ -3,9 +3,14 @@
 use Kirby\Cms\Page;
 
 return function (Page $page) {
+	$sale = new Buy\Sale();
+
+	// expire the cache when the sale banner/prices change
+	$sale->expires();
+
 	return [
 		'discounts' => option('buy.volume'),
-		'sale'      => new Buy\Sale(),
+		'sale'      => $sale,
 		'questions' => $page->find('answers')->children()
 	];
 };
