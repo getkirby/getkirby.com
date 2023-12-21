666abddd358d0af2036b085454e4d41d7fece117
Lukas Bestle <lukas@getkirby.com>
2023-12-21 12:49:13 +0100

    Default template: Intro and GitHub edit support

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,8 +1,20 @@
 <?php layout() ?>
 
-<article class="max-w-xl mx-auto mb-42">
-	<h1 class="h1 mb-24"><?= $page->title() ?></h1>
-	<div class="prose">
+<article class="max-w-xl mx-auto">
+	<header>
+		<h1 class="h1 mb-12"><?= $page->title() ?></h1>
+		<?php if ($page->intro()->isNotEmpty()): ?>
+		<div class="prose mb-12">
+			<p class="intro">
+				<?= $page->intro()->kti() ?>
+			</p>
+		</div>
+		<?php endif ?>
+	</header>
+	<div class="prose mb-24">
 		<?= $page->text()->kt() ?>
 	</div>
+	<footer>
+		<?php snippet('layouts/github-edit') ?>
+	</footer>
 </article>
