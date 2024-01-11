bde219df4aec8b68599482343b9bfc5f106b6ad4
Nico Hoffmann <me@nhoffmann.com>
2024-01-11 14:16:48 +0100

    Use more icons from sprite

--- site/snippets/kirbytext/box.php
+++ site/snippets/kirbytext/box.php
@@ -1,4 +1,11 @@
-<figure class="box-icon iconbox bg-black color-white"><?= icon($type) ?></figure>
+<?php
+$icon = match ($type) {
+	'success' => 'smile',
+	default   => $type
+};
+?>
+
+<figure class="box-icon iconbox bg-black color-white"><?= icon($icon) ?></figure>
 <div class="box-text">
 	<?= kirbytext($text ?? null) ?>
 </div>
