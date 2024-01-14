f2e61d990327d26817600842915e49aad8761865
Nico Hoffmann <me@nhoffmann.com>
2024-01-14 10:44:33 +0100

    Fix #2165

--- site/snippets/docs/api/page-create-data.php
+++ site/snippets/docs/api/page-create-data.php
@@ -1,6 +1,5 @@
 | Parameter | Type | Required | Description |
 | - | - | - | - |
 | `slug` | `string` | `required` | Needs to be unique slug/folder name for the new subpage |
-| `title` | `string` | <code></code> | If not passed, the slug will be used as title |
 | `template` | `string` | <code></code> | The name of the page template. If not passed, the default template will be used |
-| `content` | `array` | <code></code> | Additional content fields. Must be defined as key/value array |
+| `content` | `array` | <code></code> | Additional content fields. Must be defined as key/value array. Pass `title` as key, otherwise the slug will be used as page title. |
