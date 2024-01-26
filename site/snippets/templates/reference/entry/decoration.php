8070358b4d4723e43755014c29490b09ddbe83b9
Bastian Allgeier <mail@bastianallgeier.com>
2024-01-26 12:13:43 +0100

    Fix icons

--- site/snippets/templates/reference/entry/decoration.php
+++ site/snippets/templates/reference/entry/decoration.php
@@ -1,7 +1,5 @@
 <?php if ($entry->intendedTemplate()->name() === 'reference-icon'): ?>
 <figure class="p-3 mr-3 bg-light rounded">
-	<svg>
-		<use xlink:href="#icon-<?=  $entry->slug() ?>" />
-	</svg>
+	<?= icon($entry->slug()) ?>
 </figure>
 <?php endif ?>
