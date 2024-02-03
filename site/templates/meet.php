8672af673238db65d8744920effe878c19af349d
Nico Hoffmann <me@nhoffmann.com>
2024-02-03 13:33:11 +0100

    Meet: add photos

--- site/templates/meet.php
+++ site/templates/meet.php
@@ -21,9 +21,9 @@
 	<?php snippet('templates/meet/map', ['people' => $people]) ?>
 	<?php snippet('templates/meet/form') ?>
 
-	<footer class="max-w-xl">
-		<h4 class="h2 mb-3">Want to host a meetup?</h4>
-		<div class="prose">
+	<footer>
+		<h2 class="h2 mb-3">Want to host a meetup?</h2>
+		<div class="relative prose mb-24">
 			<ol>
 				<li><b>It doesn't need to be big:</b> whether a small get-together at the local pub or hosting in the office of an agency. What counts is to get together with likeminded people. Start small.</li>
 				<li><b>Use the community map to find people nearby.</b> Maybe get in touch with one or two. Hosting a meetup together can take off some stress from yourself.</li>
@@ -31,6 +31,35 @@
 				<li><b>Spread the word.</b> Let the community know about your event. Share it on <a href="/docs/archive"><span class="link">Discord</span></a> and the Kirby team will help promote it.</li>
 				<li><b>Take photos.</b> Not a must and nobody who doesn't like it should be forced to. We really love to see some snaps when people from the community get together. If everyone feels comfortable with it, let us see your happy faces.</li>
 			</ol>
-		</p>
+		</div>
+
+		<h2 class="h2 mb-6">Shake it like a polaroid picture</h2>
+		<ul class="album-gallery">
+			<?php foreach ($page->images()->sortBy('sort') as $image): ?>
+			<li>
+				<figure class="img" style="--w:<?= $image->width() ?>;--h:<?= $image->height() ?>">
+					<img src="<?= $image->resize(800)->url() ?>" alt="<?= $image->alt()->esc() ?>">
+				</figure>
+			</li>
+			<?php endforeach ?>
+		</ul>
 	</footer>
 </article>
+
+<style>
+.album-gallery {
+  line-height: 0;
+  columns: 1;
+  column-gap: 1.5rem;
+}
+.album-gallery li {
+  display: block;
+  margin-bottom: 1.5rem;
+  break-inside: avoid;
+}
+@media screen and (min-width: 60rem) {
+  .album-gallery {
+    columns: 2;
+  }
+}
+</style>
