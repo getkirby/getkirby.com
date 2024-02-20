988a16615e5d219cd9670fedbd10a1cf89c6d974
Nico Hoffmann <me@nhoffmann.com>
2024-02-20 20:47:34 +0100

    Buy page: rephrase revenue limit FAQ

--- site/snippets/faq.php
+++ site/snippets/faq.php
@@ -1,7 +1,7 @@
 <div class="faq">
 	<?php foreach ($questions as $question): ?>
 	<details class="details">
-		<summary class="py-3 border-top" id="<?= $question->slug() ?>"><?= $question->title()->widont() ?></summary>
+		<summary class="py-3 border-top" id="<?= $question->slug() ?>"><?= $question->title()->kti()->widont() ?></summary>
 		<div class="py-3 prose text-base">
 			<?= $question->text()->kt() ?>
 		</div>
