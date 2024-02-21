04ffefe13a92ee4802f8eace8ba1815bad8d339a
Bastian Allgeier <mail@bastianallgeier.com>
2024-02-21 12:24:27 +0100

    New grouped menu style

--- site/snippets/sidebar/menu-grouped.php
+++ site/snippets/sidebar/menu-grouped.php
@@ -1,9 +1,6 @@
-<?php
-extract([
-	'open' => $open ?? false
-])
-?>
 <?php foreach ($menu->group('category') as $category => $items): ?>
-	<h2 class="h6 mb-3"><?= option('categories')[$category] ?? ucfirst($category) ?></h2>
+<section class="sidebar-group">
+	<h2><?= option('categories')[$category] ?? ucfirst($category) ?></h2>
 	<?php snippet('sidebar/menu', ['menu' => $items, 'marginBottom' => 'mb-6']); ?>
+</section>
 <?php endforeach ?>
