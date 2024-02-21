36682ffd0c378fde158bbc97dea39696c77e2d7f
Ahmet Bora <byybora@gmail.com>
2024-02-21 16:14:19 +0300

    Add new `cause` field for cases

--- site/collections/causes.php
+++ site/collections/causes.php
@@ -1,20 +1,5 @@
 <?php
 
 return function ($kirby) {
-	return $kirby->collection('cases')->find([
-		'berlin-in-bewegung',
-		'fairclimatefund',
-		'fridays-for-future-at',
-		'klimaliste-berlin',
-		'natucate',
-		'strassenfeger',
-		'teteenlair',
-		'klimadashboard',
-		'viseta',
-		'nowar',
-		'throughpositiveeyes',
-		'gobanyo',
-		'ppks',
-		'adunare'
-	]);
+	return $kirby->collection('cases')->filterBy('cause', true);
 };
