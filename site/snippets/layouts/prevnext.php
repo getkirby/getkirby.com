6c4cb723414c31d375b829c3990fe3fc7a5f1b73
Bastian Allgeier <mail@bastianallgeier.com>
2024-02-21 12:23:57 +0100

    Style prevnext

--- site/snippets/layouts/prevnext.php
+++ site/snippets/layouts/prevnext.php
@@ -1,27 +1,20 @@
 <?php
 $siblings = $siblings ?? $page->siblings();
 if ($siblings->isNotEmpty()): ?>
-<nav class="mb-24">
-	<?php $columnCount = $page->prevListed($siblings) === null ? 1 : 2; ?>
-	<div class="columns" style="--columns-md: 1; --columns: <?= $columnCount ?>;">
-		<?php if ($prev = $page->prevListed($siblings)): ?>
-			<div class="prose">
-				<span class="block text-xs">← Previous</span>
-				<a class="" href="<?= $prev->url() ?>">
-					<span class="block"><?= $prev->title() ?></span>
-				</a>
-			</div>
-		<?php endif ?>
+<nav class="prevnext">
+	<?php if ($prev = $page->prevListed($siblings)): ?>
+	<a class="prevnext-prev" href="<?= $prev->url() ?>">
+		<span class="prevnext-label">Previous page</span>
+		<span class="prevnext-title link"><?= $prev->title() ?></span>
+	</a>
+	<?php endif ?>
 
-		<?php if ($next = $page->nextListed($siblings)): ?>
-			<div class="prose text-right">
-				<span class="block text-xs">Next →</span>
-				<a href="<?= $next->url() ?>">
-					<span class="block"><?= $next->title() ?></span>
-				</a>
-			</div>
-		<?php endif ?>
-	</div>
+	<?php if ($next = $page->nextListed($siblings)): ?>
+	<a class="prevnext-next" href="<?= $next->url() ?>">
+		<span class="prevnext-label">Next page</span>
+		<span class="prevnext-title link"><?= $next->title() ?></span>
+	</a>
+	<?php endif ?>
 </nav>
 <?php endif ?>
 
