36abaae9e078593ff302f4913b8f07d3deaf2521
Sonja Broda <github@texniq.de>
2024-02-25 23:24:47 +0100

    Add tmp styles

--- site/snippets/templates/cookbook/recipes.php
+++ site/snippets/templates/cookbook/recipes.php
@@ -1,7 +1,7 @@
 <ul class="auto-fill auto-rows-fr" style="--min: 16rem; --column-gap: var(--spacing-12); --row-gap: var(--spacing-24)">
 	<?php foreach ($recipes as $recipe): ?>
-	<li>
-		<article>
+	<li style="background-image: url(/assets/patterns/<?= $kirby->option('cookbook.categories')[$recipe->parent()->slug()]['pattern'] ?? 'lagoon' ?>.jpg); background-size: cover; background-repeat: no-repeat; padding: 0 0  1.5rem 1.5rem">
+		<article style="background-color: white; padding: 1rem; height: 100%">
 			<a class="block pt-1" href="<?= $recipe->url() ?>">
 				<figure class="mb-3">
 					<div class="mb-3">
