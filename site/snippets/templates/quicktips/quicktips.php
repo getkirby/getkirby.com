1d3de838c3f385b36f27c60b36fb1dbeb1f477e5
Sonja Broda <github@texniq.de>
2024-03-03 21:44:32 +0100

    Render tags

--- site/snippets/templates/quicktips/quicktips.php
+++ site/snippets/templates/quicktips/quicktips.php
@@ -1,17 +1,26 @@
 <ul class="auto-fill" style="--min: 16rem; --column-gap: var(--spacing-12); --row-gap: var(--spacing-12)">
 	<?php foreach ($quicktips as $tip): ?>
 	<li>
-		<p class="font-mono text-xs mb-1"><?= implode(',', array_map(fn($item) => Str::upper($item), $tip->tags()->split(',') ?? [])) ?></p>
+		<p class="text-xs">
+			<?= implode(' | ',
+				array_map(
+					fn ($tag) => Html::a(page('docs/quicktips')->url() . '/tags/' . Str::slug($tag), Str::upper($tag) , ['class' => '']),
+					$tip->tags()->split(',')
+				)
+			);
+			?>
+		</p>
+
 		<article class="mb-6">
 			<a class="block pt-1" href="<?= $tip->url() ?>">
 				<figure class="">
 					<div class="mb-3">
-						<h2 class="h3">
-							<?= $tip->description() ?>
+						<h2 class="h4">
+							<?= $tip->title() ?>
 						</h2>
 					</div>
 				</figure>
-				<!--p class="color-gray-700"><?= $tip->description()->widont() ?></p-->
+				<p class="color-gray-700"><?= $tip->description()->widont() ?></p>
 			</a>
 		</article>
 	</li>
--- site/templates/cookbook-recipe.php
+++ site/templates/cookbook-recipe.php
@@ -20,7 +20,6 @@
 		)
 	);
 	?>
-
 <?php endslot() ?>
 
 <?php slot('authors') ?>
