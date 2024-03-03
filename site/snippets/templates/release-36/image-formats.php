3ff681badf565c13bf23c5ae8b41fc6407421d40
Sonja Broda <github@texniq.de>
2024-03-03 18:02:36 +0100

    Fix links

--- site/snippets/templates/release-36/image-formats.php
+++ site/snippets/templates/release-36/image-formats.php
@@ -4,6 +4,6 @@ snippet('templates/features/section', [
 	'id'    => 'image-formats',
 	'title' => 'WebP & AVIF Support',
 	'intro' => 'Serve smaller and better images',
-	'text'  => 'With increasingly better browser support for WebP and AVIF on the horizon, optimizing images has become a lot easier. <br><br> The performance benefits are obvious. WebP can produce high quality images with roughly 50% smaller files compared to JPEG and AVIF can go even lower than that.<br><br>Our image processing engine now supports both formats, and with the help of the <code>&lt;picture&gt;</code> element you can even make older browsers happy. <a href="/docs/guide/templates/resize-images-on-the-fly#image-formats">Learn more &rsaquo;</a></p>',
+	'text'  => 'With increasingly better browser support for WebP and AVIF on the horizon, optimizing images has become a lot easier. <br><br> The performance benefits are obvious. WebP can produce high quality images with roughly 50% smaller files compared to JPEG and AVIF can go even lower than that.<br><br>Our image processing engine now supports both formats, and with the help of the <code>&lt;picture&gt;</code> element you can even make older browsers happy. <a href="/docs/guide/files/resize-images-on-the-fly#image-formats">Learn more &rsaquo;</a></p>',
 	'figure' => 'templates/release-36/image-formats-figure',
 ]);
