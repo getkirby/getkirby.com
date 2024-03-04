fe418f2c95ad5cc7db950f9f1db0bf2418c71b29
Bastian Allgeier <mail@bastianallgeier.com>
2024-03-04 17:14:18 +0100

    First styling attempts

--- site/snippets/templates/quicktips/quicktips.php
+++ site/snippets/templates/quicktips/quicktips.php
@@ -1,27 +1,53 @@
-<ul class="auto-fill" style="--min: 16rem; --column-gap: var(--spacing-12); --row-gap: var(--spacing-12)">
+<style>
+.quicktips li {
+	background: var(--color-white);
+	border-radius: var(--rounded);
+	box-shadow: var(--shadow);
+}
+.quicktips li + li {
+	margin-top: 2px;
+}
+.quicktips article {
+	display: flex;
+	justify-content: space-between;
+}
+.quicktip-title {
+	padding: var(--spacing-2) var(--spacing-3);
+	flex-grow: 1;
+	font-size: var(--text-base);
+}
+.quicktip-tags {
+	padding: var(--spacing-2);
+	display: flex;
+	gap: var(--spacing-2);
+	font-size: var(--text-xs);
+}
+.quicktip-tags a {
+	display: inline-flex;
+	align-items: center;
+	border-radius: var(--rounded-sm);
+	background: var(--color-gray-200);
+	padding: 0 .5rem;
+}
+</style>
+
+<ul class="quicktips">
 	<?php foreach ($quicktips as $tip): ?>
 	<li>
-		<p class="text-xs">
-			<?= implode(' | ',
-				array_map(
-					fn ($tag) => Html::a(page('docs/quicktips')->url() . '/tags/' . Str::slug($tag), Str::upper($tag) , ['class' => '']),
-					$tip->tags()->split(',')
-				)
-			);
-			?>
-		</p>
-
-		<article class="mb-6">
-			<a class="block pt-1" href="<?= $tip->url() ?>">
-				<figure class="">
-					<div class="mb-3">
-						<h2 class="h4">
-							<?= $tip->title() ?>
-						</h2>
-					</div>
-				</figure>
-				<p class="color-gray-700"><?= $tip->description()->widont() ?></p>
+		<article>
+			<a class="quicktip-title" href="<?= $tip->url() ?>">
+				<h2 class="link">
+					<?= $tip->title() ?>
+				</h2>
 			</a>
+			<p class="quicktip-tags">
+				<?= implode('',
+					array_map(
+						fn ($tag) => Html::a(page('docs/quicktips')->url() . '/tags/' . Str::slug($tag), Str::ucfirst($tag)),
+						$tip->tags()->split(',')
+					)
+				); ?>
+			</p>
 		</article>
 	</li>
 	<?php endforeach ?>
