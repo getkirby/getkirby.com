8412ae1383f38af4c51657c44940b9bd755be935
Nico Hoffmann <me@nhoffmann.com>
2024-03-05 20:24:54 +0100

    Cookbook: Show tag in filtered view

--- site/templates/cookbook.php
+++ site/templates/cookbook.php
@@ -2,11 +2,11 @@
 
 <?php slot('h1') ?>
 	Recipes
+	<?php if ($tag): ?>
+		<span class="color-gray-400">#<?= $tag ?></span>
+	<?php endif ?>
 <?php endslot() ?>
 
 <?php slot() ?>
-	<?php snippet('templates/cookbook/recipes', [
-		'recipes' => $recipes
-			->sortBy('published', 'desc')
-	]) ?>
+<?php snippet('templates/cookbook/recipes', ['recipes' => $recipes]) ?>
 <?php endslot() ?>
