4d9f354c9b52ac78178bdf3b8618373adba8fa7c
Nico Hoffmann <me@nhoffmann.com>
2024-03-06 19:57:12 +0100

    Adapt for Cookbook

--- site/snippets/templates/cookbook/sidebar.php
+++ site/snippets/templates/cookbook/sidebar.php
@@ -1,6 +1,19 @@
 <nav aria-label="Cookbook menu">
-	<div class="cookbook-sidebar sticky" style="--top: var(--spacing-6)">
+	<div class="sidebar cookbook-sidebar sticky" style="--top: var(--spacing-6)">
 		<p class="h1 color-gray-400 mb-12"><a href="/docs/cookbook">Cookbook</a></p>
+
+		<select
+			class="sidebar-mobile-select"
+			onchange="window.location.href = this.value"
+		>
+			<option disabled selected>Select a category</option>
+				<?php foreach (collection('cookbook/categories') as $category): ?>
+				<option value="<?= $category->url() ?>">
+					&nbsp;&nbsp;&nbsp;<?= $category->title() ?>
+				</option>
+				<?php endforeach ?>
+		</select>
+
 		<ul class="filters">
 			<li>
 				<a href="/docs/cookbook"<?= ariaCurrent($page->slug() === 'cookbook') ?>>
