2ff4fed26a0bb1a87b0ef46b32e6a7f9c71b371a
Nico Hoffmann <me@nhoffmann.com>
2024-03-11 14:22:54 +0100

    Topbar: save vertical space

--- site/snippets/layouts/header.php
+++ site/snippets/layouts/header.php
@@ -1,13 +1,11 @@
-<div class="mb-6">
-	<div class="container">
-		<?php snippet('layouts/topbar', [
-			'icon'   => '🚀',
-			'title'  => 'A new era: Kirby 4',
-			'button' => 'Get to know',
-			'link'   => '/releases/4.0',
-			'active' => $page->is('releases/4-0')
-		]) ?>
-	</div>
+<div class="container">
+	<?php snippet('layouts/topbar', [
+		'icon'   => '🚀',
+		'title'  => 'A new era: Kirby 4',
+		'button' => 'Get to know',
+		'link'   => '/releases/4.0',
+		'active' => $page->is('releases/4-0')
+	]) ?>
 </div>
 
 <header class="header mb-24">
