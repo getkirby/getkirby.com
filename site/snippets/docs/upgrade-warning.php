ed11a928709e26d69f6737cb5817f54147f1d983
Sonja Broda <github@texniq.de>
2024-03-23 16:59:07 +0100

    Add installation  &update via CLI and download, rename getting starter, change order

---
+++ site/snippets/docs/upgrade-warning.php
@@ -0,0 +1,12 @@
+<warning>
+Kirby uses the [Semantic Versioning](https://semver.org) scheme:
+{major}.{minor}(.{patch})
+
+**Major releases** (e.g 4.0 to 5.0) may contain breaking changes and deprecations. In such a case, you might have to update your code and plugins used in your installation. You can find breaking changes of a new release in the changelogs. If you are updating from older versions, multiple changelogs may apply. Always read them carefully to identify potential issues.
+
+**Minor and patch releases** (e.g. 4.1 to 4.2 or 4.1 to 4.1.1) can generally be applied without changes to your site code.
+
+Note that the major version of Kirby 3 was the second number (e.g. the major version of Kirby 3.9.0 is 9). So e.g. updates from Kirby 3.8 to Kirby 3.9 also came with breaking changes.
+
+In any case, please never update a live website directly on the server. Test updates locally first to see if something breaks.
+</warning>
