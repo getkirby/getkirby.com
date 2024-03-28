0ce966986c9c7e3789b850d0df3feaff864efdb5
Lukas Bestle <lukas@getkirby.com>
2024-03-28 22:32:59 +0100

    Demo: Status message for referrer check

--- site/controllers/try.php
+++ site/controllers/try.php
@@ -9,14 +9,16 @@ return function (Page $page) {
 		'rate-limit' => 'Your IP address has reached the maximum number of concurrent demo instances. Please try again later or continue with one of your existing instances. Maybe your colleague has one you can play around with together? :)',
 		'overload'   => 'Our demo server is currently being used by a lot of users, please try again later. Sorry for the inconvenience!',
 		'deleted'    => 'Your demo instance was deleted successfully. Thanks for trying out Kirby! You can create a new demo at any time.',
+		'referrer'   => 'Thanks for your interest in the Kirby demo. Your personal demo can only be created from this official Kirby website. Please use the button below to get started.',
 		'default'    => 'An unexpected error occured in the demo manager. Please let us know if this keeps happening. Thanks!',
 	];
 
 	if ($status = (param('error') ?? param('status'))) {
 		$message = $messages[$status] ?? $messages['default'];
 		$type    = match($status) {
-			'deleted' => 'info',
-			default   => 'warning'
+			'deleted',
+			'referrer' => 'info',
+			default    => 'warning'
 		};
 	}
 
