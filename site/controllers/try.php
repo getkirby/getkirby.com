c6253773bba98430d17f0aaf101f04687beee14c
Lukas Bestle <lukas@getkirby.com>
2024-03-28 22:34:14 +0100

    Sort messages

--- site/controllers/try.php
+++ site/controllers/try.php
@@ -5,10 +5,10 @@ use Kirby\Cms\Page;
 return function (Page $page) {
 	$message  = $type = null;
 	$messages = [
-		'not-found'  => 'The requested demo instance does not exist or has expired. Feel free to create a new instance.',
-		'rate-limit' => 'Your IP address has reached the maximum number of concurrent demo instances. Please try again later or continue with one of your existing instances. Maybe your colleague has one you can play around with together? :)',
-		'overload'   => 'Our demo server is currently being used by a lot of users, please try again later. Sorry for the inconvenience!',
 		'deleted'    => 'Your demo instance was deleted successfully. Thanks for trying out Kirby! You can create a new demo at any time.',
+		'not-found'  => 'The requested demo instance does not exist or has expired. Feel free to create a new instance.',
+		'overload'   => 'Our demo server is currently being used by a lot of users, please try again later. Sorry for the inconvenience!',
+		'rate-limit' => 'Your IP address has reached the maximum number of concurrent demo instances. Please try again later or continue with one of your existing instances. Maybe your colleague has one you can play around with together? :)',
 		'referrer'   => 'Thanks for your interest in the Kirby demo. Your personal demo can only be created from this official Kirby website. Please use the button below to get started.',
 		'default'    => 'An unexpected error occured in the demo manager. Please let us know if this keeps happening. Thanks!',
 	];
