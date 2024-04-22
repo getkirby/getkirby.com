676197c5c0ae57a842ccb3b7df5780dd74e462bd
Lukas Bestle <lukas@getkirby.com>
2024-04-22 12:11:48 +0200

    Update additional plugin URLs

--- site/snippets/templates/features-for-developers/plugins-figure.php
+++ site/snippets/templates/features-for-developers/plugins-figure.php
@@ -1,6 +1,6 @@
 <?php if ($image = image('matomo.jpg')): ?>
 <figure>
-	<a class="block bg-light rounded mb-3" href="/plugins" style="--aspect-ratio: <?= $image->width() . '/' . $image->height() ?>">
+	<a class="block bg-light rounded mb-3" href="https://plugins.getkirby.com" style="--aspect-ratio: <?= $image->width() . '/' . $image->height() ?>">
 		<?= img($image, [
 			'alt' => 'A screenshot of the Matomo plugin by Sylvain Julé',
 			'src' => [
@@ -17,7 +17,7 @@
 		]) ?>
 	</a>
 	<figcaption class="font-mono text-xs color-gray-700">
-		<a href="/plugins/sylvainjule/matomo"><strong class="font-normal color-black link">Matomo Plugin</strong> by Sylvain Julé</a>
+		<a href="https://plugins.getkirby.com/sylvainjule/matomo"><strong class="font-normal color-black link">Matomo Plugin</strong> by Sylvain Julé</a>
 	</figcaption>
 </figure>
 <?php endif ?>
