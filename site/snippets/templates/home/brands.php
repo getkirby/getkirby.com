fa55ff73b0a9bfac5b910fdca3f84d4825608c78
Nico Hoffmann <me@nhoffmann.com>
2024-05-15 22:39:33 +0200

    Change phrasing to clients

--- site/snippets/templates/home/brands.php
+++ site/snippets/templates/home/brands.php
@@ -1,5 +1,5 @@
 <section id="brands" class="mb-42">
-	<h2 class="h2 mb-12">Trusted by brands world&#8209;wide</h2>
+	<h2 class="h2 mb-12">Trusted by clients world&#8209;wide</h2>
 	<ul class="brands items-center">
 		<?php foreach(collection('brands')->limit(18) as $client): ?>
 		<li class="<?= $client->slug() ?>" title="<?= $client->title() ?>">
