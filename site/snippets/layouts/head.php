15ab6c82dcc998d0fc47e0ce24e394fa34f75d6a
Lukas Bestle <lukas@getkirby.com>
2024-05-30 13:09:44 +0200

    Remove dead code for the community map
    Map is now embedded as iframe

--- site/snippets/layouts/head.php
+++ site/snippets/layouts/head.php
@@ -45,13 +45,6 @@ extract([
 <link rel="preload" href="<?= url('buy/prices') ?>" as="fetch" />
 <?php endif ?>
 
-<?php if($page->id() === 'meet'): ?>
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="      crossorigin=""/>
-<link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css"/>
-<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
-<script src="https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js"></script>
-<?php endif ?>
-
 <?= css('assets/css/index.css') ?>
 
 <link rel="icon" type="image/png" href="<?= url('/assets/images/favicon.png') ?>">
