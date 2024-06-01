1c8ccd19bc1814f9a246abcabaf6d1327bf2b9e8
Lukas Bestle <lukas@getkirby.com>
2024-06-01 12:40:30 +0200

    Only link glossary entries if they have a link

--- site/templates/glossary.php
+++ site/templates/glossary.php
@@ -6,12 +6,16 @@
 		<?php foreach ($page->children()->sortBy('title', 'asc') as $term): ?>
 		<li>
 			<article id="<?= $term->slug() ?>">
+				<?php if ($term->link()->isNotEmpty()): ?>
 				<a href="<?= $term->link()->toUrl() ?>">
+				<?php endif ?>
 					<h2 class="h6 mb-1"><?= $term->title() ?></h2>
 					<div class="prose text-base border-top pt-3">
 						<?= $term->entry()->stripGlossary()->kt() ?>
 					</div>
+				<?php if ($term->link()->isNotEmpty()): ?>
 				</a>
+				<?php endif ?>
 			</article>
 		</li>
 		<?php endforeach ?>
