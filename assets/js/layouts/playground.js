344d2511f345985a0d17bbaca98190930840a249
Nico Hoffmann <me@nhoffmann.com>
2024-06-01 15:02:50 +0200

    Playground: fix transition delays

--- assets/js/layouts/playground.js
+++ assets/js/layouts/playground.js
@@ -12,30 +12,40 @@ export class Playground {
 		});
 	}
 
+	async loadHtml(link) {
+		const response = await fetch(link);
+		const body = await response.text();
+		const parser = new DOMParser();
+		const doc = parser.parseFromString(body, "text/html");
+
+		// load the new image as hidden before it gets injected
+		doc
+			.querySelector(".playground-header-figure-wrapper")
+			.classList.add("loading");
+
+		return doc;
+	}
+
 	async switchTo(link) {
 		// fade out the old image
 		this.$el
 			.querySelector(".playground-header-figure-wrapper")
 			.classList.add("loading");
 
-		const response = await fetch(link);
-		const body = await response.text();
-		const parser = new DOMParser();
-		const doc = parser.parseFromString(body, "text/html");
-
-		// hide the new image before it gets injected
-		doc
-			.querySelector(".playground-header-figure-wrapper")
-			.classList.add("loading");
+		// since our CSS transition to fade out the image takes 200ms,
+		// ensure that we wait that long, even if the fetch request is faster
+		const [doc] = await Promise.all([
+			this.loadHtml(link),
+			new Promise((resolve) => setTimeout(resolve, 200)),
+		]);
 
 		// replace the playground
 		this.$el.innerHTML = doc.querySelector(".playground").innerHTML;
 
-		// fade in the image on load
+		// fade in the image once loaded
 		this.$el
 			.querySelector(".playground-header-figure img")
-			.addEventListener("load", async function () {
-				await new Promise((resolve) => setTimeout(resolve, 100));
+			.addEventListener("load", function () {
 				this.parentNode.classList.remove("loading");
 			});
 
