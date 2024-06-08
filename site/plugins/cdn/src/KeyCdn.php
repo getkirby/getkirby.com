0d8861cbacd52ccb2795f58b58fa423f8e966720
Lukas Bestle <lukas@getkirby.com>
2024-06-08 16:13:18 +0200

    Implement remaining Kirby thumb options

--- site/plugins/cdn/src/KeyCdn.php
+++ site/plugins/cdn/src/KeyCdn.php
@@ -6,6 +6,7 @@ use Kirby\Cms\App;
 use Kirby\Cms\File;
 use Kirby\Http\Url;
 use Kirby\Image\Darkroom;
+use Kirby\Image\Focus;
 
 class KeyCdn
 {
@@ -36,10 +37,42 @@ class KeyCdn
 		if (empty($params) === false) {
 			$query = '?' . http_build_query([
 				...static::resizeOrCrop($params),
+				...static::grayscale($params),
+				...static::progressive($params),
+				...static::blur($params),
+				...static::sharpen($params),
 			]);
 		}
 
 		return $kirby->option('cdn.domain') . '/' . $path . $query;
+	}
+
+	protected static function blur(array $params): array
+	{
+		if ($params['blur'] !== false) {
+			$blur = max(0.3, min(100, $params['blur']));
+			return compact('blur');
+		}
+
+		return [];
+	}
+
+	protected static function grayscale(array $params): array
+	{
+		if ($params['grayscale'] === true) {
+			return ['grayscale' => 1];
+		}
+
+		return [];
+	}
+
+	protected static function progressive(array $params): array
+	{
+		if ($params['interlace'] === true) {
+			return ['progressive' => 1];
+		}
+
+		return [];
 	}
 
 	protected static function resizeOrCrop(array $params): array
@@ -56,11 +89,38 @@ class KeyCdn
 			return $query;
 		}
 
-		$query['crop'] = match ($params['crop']) {
-			'top'   =>  'fp,0,0',
-			default => 'smart'
+		// crop based on focus point
+		if (Focus::isFocalPoint($params['crop']) === true) {
+			$focus = Focus::parse($params['crop']);
+			$query['crop'] = 'fp,' . $focus[0] . ',' . $focus[1];
+
+			return $query;
+		}
+
+		// translate the gravity option into something KeyCDN understands
+		$query['crop'] = match ($params['crop'] ?? null) {
+			'top left'     => 'fp,0,0',
+			'top'          => 'fp,0.5,0',
+			'top right'    => 'fp,1.0,0',
+			'left'         => 'fp,0,0.5',
+			'center'       => 'fp,0.5,0.5',
+			'right'        => 'fp,1,0.5',
+			'bottom left'  => 'fp,0,1.0',
+			'bottom'       => 'fp,0.5,1.0',
+			'bottom right' => 'fp,1.0,1.0',
+			default        => 'smart'
 		};
 
 		return $query;
+	}
+
+	protected static function sharpen(array $params): array
+	{
+		if (is_int($params['sharpen']) === true) {
+			$sharpen = max(0, min(100, $params['sharpen']));
+			return compact('sharpen');
+		}
+
+		return [];
 	}
 }
