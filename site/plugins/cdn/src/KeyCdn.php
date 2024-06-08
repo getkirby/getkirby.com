9623e242ef78965db55d6babb97b351644ab26c8
Lukas Bestle <lukas@getkirby.com>
2024-06-08 16:12:47 +0200

    Move KeyCDN logic to separate class

---
+++ site/plugins/cdn/src/KeyCdn.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Kirby\Cdn;
+
+use Kirby\Cms\App;
+use Kirby\Cms\File;
+use Kirby\Http\Url;
+
+class KeyCdn
+{
+	/**
+	 * Generates a URL with KeyCDN image processing parameters
+	 * (https://www.keycdn.com/support/image-processing)
+	 * based on a file URL or object and Kirby thumb params
+	 */
+	public static function url(string|File $file, array $params = []): string
+	{
+		$kirby = App::instance();
+
+		if (is_object($file) === true) {
+			$file = $file->mediaUrl();
+		}
+
+		$path = Url::path($file);
+
+		$query = '';
+		if (empty($params) === false) {
+			$query = '?' . http_build_query([
+				...static::resizeOrCrop($params),
+			]);
+		}
+
+		return $kirby->option('cdn.domain') . '/' . $path . $query;
+	}
+
+	protected static function resizeOrCrop(array $params): array
+	{
+		// Use the width as height if the height is not set
+		if (empty($params['crop']) === false && $params['crop'] !== false) {
+			$params['fit']      = true;
+			$params['height'] ??= $params['width'];
+			$params['crop']     = match ($params['crop']) {
+				'top'   =>  'fp,0,0',
+				default => 'smart'
+			};
+		} else {
+			$params['enlarge'] = 0;
+			$params['fit']     = match (isset($params['width'], $params['height'])) {
+				true    => 'inside',
+				default => true
+			};
+		}
+
+		return $params;
+	}
+}
