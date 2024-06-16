80b41d3fe71c4883ca1846541d8c7136d78ac7f5
Lukas Bestle <lukas@getkirby.com>
2024-06-16 21:21:36 +0200

    Update caching times for update check endpoints
    Refreshing the `security.json` in the CDN every half an hour is no longer necessary because of individual purging

--- site/routes/plugins.php
+++ site/routes/plugins.php
@@ -29,7 +29,7 @@ return [
 						// keep the data in the client cache for a day,
 						// but refresh the data in the CDN cache every half an hour;
 						// if getkirby.com is not reachable, continue to serve the data for two days
-						'Cache-Control' => 'max-age=86400, s-maxage=1800, stale-if-error=172800'
+						'Cache-Control' => 'public, max-age=86400, s-maxage=1800, stale-if-error=172800'
 					]
 				);
 			}
--- site/templates/security.json.php
+++ site/templates/security.json.php
@@ -9,9 +9,8 @@ $data = [
 	'messages'  => array_values($page->messages())
 ];
 
-// keep the data in the client cache for a day,
-// but refresh the data in the CDN cache every half an hour;
+// keep the data in the CDN & client cache for a day;
 // if getkirby.com is not reachable, continue to serve the data for two days
-$kirby->response()->header('Cache-Control', 'max-age=86400, s-maxage=1800, stale-if-error=172800');
+$kirby->response()->header('Cache-Control', 'public, max-age=86400, stale-if-error=172800');
 
 echo json($data);
