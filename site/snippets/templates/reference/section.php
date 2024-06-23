d16d78b88a6a2bb5e75377e82d2a3af2b3911ce4
Lukas Bestle <lukas@getkirby.com>
2024-06-23 12:07:53 +0200

    Fix invalid markup in reference

--- site/snippets/templates/reference/section.php
+++ site/snippets/templates/reference/section.php
@@ -1,14 +1,12 @@
 <ul class="reference-section mb-12 auto-fill" style="--min: 15rem; --row-gap: 0; --column-gap: var(--spacing-3)">
 	<?php foreach ($item as $entry): ?>
 	<li>
-		<a href="<?= $entry->url() ?>" class="flex items-center">
+		<a href="<?= $entry->url() ?>" class="flex items-center mb-1">
 			<?php snippet(
 				'templates/reference/entry/decoration',
 				['entry' => $entry]
 			) ?>
-			<div class="">
-				<h3 class="mb-1"><?= $entry->title() ?></h3>
-			</div>
+			<?= $entry->title() ?>
 		</a>
 	</li>
 	<?php endforeach ?>
