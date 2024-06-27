ac7ce5143748fc0d2388052ffb998be1af88ccae
Lukas Bestle <lukas@getkirby.com>
2024-06-27 21:55:32 +0200

    Wording improvements

--- site/snippets/templates/partners-signup/price.php
+++ site/snippets/templates/partners-signup/price.php
@@ -2,8 +2,8 @@
 	<h3 class="font-bold">Price per year</h3>
 	<p class="text-xl mb-3">€{{ price }}</p>
 	<ul class="text-xs color-gray-700">
-		<li>VAT is not included in the price.</li>
-		<li>You will be charged, once your application has been accepted.</li>
-		<li>Your partnership won't auto-renew after one year.</li>
+		<li>Price + VAT if applicable.</li>
+		<li>You will be charged once your application has been accepted.</li>
+		<li>Your partnership will <em>not</em> automatically renew.</li>
 	</ul>
 </section>
