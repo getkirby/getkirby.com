769287212ab60f17e9881053de69ac45a18d67d9
Lukas Bestle <lukas@getkirby.com>
2024-06-29 15:14:48 +0200

    Fix flickering on load

--- site/snippets/templates/partners-signup/price.php
+++ site/snippets/templates/partners-signup/price.php
@@ -1,6 +1,8 @@
 <section class="mb-12">
 	<h3 class="font-bold">Price per year</h3>
-	<p class="text-xl mb-3">€{{ price }}</p>
+	<p class="text-xl mb-3">
+		€<span v-text="price">499</span>
+	</p>
 	<ul class="text-xs color-gray-700">
 		<li>Price + VAT if applicable.</li>
 		<li>You will be charged once your application has been accepted.</li>
