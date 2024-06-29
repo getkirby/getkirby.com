70a8b7bccf4cae5068a4cebb7dbdd21d99524e24
Lukas Bestle <lukas@getkirby.com>
2024-06-29 16:01:58 +0200

    Clean up removed fields from controller

--- site/controllers/partners-signup.php
+++ site/controllers/partners-signup.php
@@ -4,9 +4,6 @@ use Kirby\Cms\Page;
 
 return function (Page $page) {
 	return [
-		'requirements' => $page->requirements()->yaml(),
-		'benefits'     => $page->benefits()->yaml(),
-		'packages'     => $page->packages()->yaml(),
-		'questions'    => $page->find('answers')->children(),
+		'questions' => $page->find('answers')->children(),
 	];
 };
