47c0a832bc8bb856d2ee16521c4d92fe6718a190
Lukas Bestle <lukas@getkirby.com>
2024-07-05 21:49:50 +0200

    Wording improvements

--- site/templates/partners-signup-success.php
+++ site/templates/partners-signup-success.php
@@ -3,10 +3,11 @@
 <article>
 	<?php if (param('partnership') === 'renewed'): ?>
 		<header class="mb-6">
-			<h1 class="h1">Thank your for renewing your partnership 💛</h1>
+			<h1 class="h1">Thank you for renewing your partnership 💛</h1>
 		</header>
 		<div class="prose text-xl text-base">
-			<p>It means a lot to us that you stick around for another year. If you have made changes to your plan, we will update your listing as soon as possible. Please, let us know if you want to update your texts, image or reference projects and we will take care of it. Let us know if you have any questions or comments about your renewal in the meantime: <a href="mailto:partners@getkirby.com">partners@getkirby.com</a></p>
+			<p>It means a lot to us that you stick around for another year. If you have made changes to your plan, we will update your listing as soon as possible.</p>
+			<p>Let us know if you have any questions or comments about your renewal in the meantime or if you want to update your texts, image or reference projects: <a href="mailto:partners@getkirby.com">partners@getkirby.com</a></p>
 		</div>
 	<?php else: ?>
 		<header class="mb-6">
@@ -18,4 +19,3 @@
 	<?php endif ?>
 
 </article>
-
