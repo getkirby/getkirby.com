1809d17504ac5f2058867efc99f60b0fd6751c48
Bastian Allgeier <mail@bastianallgeier.com>
2024-07-05 11:33:29 +0200

    Remove unnecessary sentence

--- site/templates/partners-signup-success.php
+++ site/templates/partners-signup-success.php
@@ -6,7 +6,7 @@
 			<h1 class="h1">Thank your for renewing your partnership 💛</h1>
 		</header>
 		<div class="prose text-xl text-base">
-			<p>It means a lot to us that you stick around for another year. If you have made changes to your plan, we will update your listing as soon as possible. Please, let us know if you want to update your texts, image or reference projects and we will take care of it. Otherwise, we will add your changes as soon as possible. Let us know if you have any questions or comments about your renewal in the meantime: <a href="mailto:partners@getkirby.com">partners@getkirby.com</a></p>
+			<p>It means a lot to us that you stick around for another year. If you have made changes to your plan, we will update your listing as soon as possible. Please, let us know if you want to update your texts, image or reference projects and we will take care of it. Let us know if you have any questions or comments about your renewal in the meantime: <a href="mailto:partners@getkirby.com">partners@getkirby.com</a></p>
 		</div>
 	<?php else: ?>
 		<header class="mb-6">
