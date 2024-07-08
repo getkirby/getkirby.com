c908ebaff2254ea643fb93670bb938becad19ac9
Nico Hoffmann <me@nhoffmann.com>
2024-07-08 22:15:45 +0900

    Add SMTP config

--- site/config/config.getkirby.com.php
+++ site/config/config.getkirby.com.php
@@ -1,8 +1,21 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+
 return [
 	'cache' => require __DIR__ . '/cache.php',
 	'cdn' => [
 		'domain' => 'https://assets.getkirby.com',
 	],
+	'email' => [
+		'transport' => [
+			'type'     => 'smtp',
+			'host'     => 'smtps-proxy.fastmail.com',
+			'port'     => 443,
+			'security' => PHPMailer::ENCRYPTION_SMTPS,
+			'auth'     => true,
+			'username' => 'mail@getkirby.com',
+			'password' => (require __DIR__ . '/keys.php')['fastmail'],
+		]
+	]
 ];
