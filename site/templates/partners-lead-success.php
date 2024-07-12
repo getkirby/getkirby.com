ee5c6a8a165d999ddb64e4cd9fffc99dae9c891b
Nico Hoffmann <me@nhoffmann.com>
2024-07-12 12:43:38 +0200

    Project lead form: use success page

---
+++ site/templates/partners-lead-success.php
@@ -0,0 +1,10 @@
+<?php layout() ?>
+
+<article>
+	<header class="mb-6">
+		<h1 class="h1">Thank you for posting your project</h1>
+	</header>
+	<div class="prose text-xl text-base">
+		<?= $page->text()->kt() ?>
+	</div>
+</article>
