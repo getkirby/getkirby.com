35d71a4e59bc3493cc530ed2c655ba0dc5e7c0fa
Nico Hoffmann <me@nhoffmann.com>
2024-07-17 18:26:02 +0200

    Lead form: wrap more in try-catch

--- site/controllers/partners-lead.php
+++ site/controllers/partners-lead.php
@@ -7,46 +7,47 @@ use Kirby\Honey\Time;
 return function (App $kirby, Page $page) {
 	// submitted form
 	if ($kirby->request()->is('POST') === true) {
-		// spam protection
 		try {
+			// spam protection
 			Time::validate(get('timestamp'));
+
+			$name     = get('name');
+			$email    = get('email');
+			$customer = get('customer');
+			$company  = get('company');
+			$contact  = get('contact');
+			$project  = get('project');
+			$partner  = get('partner');
+			$language = get('language');
+			$budget   = match(get('budget')) {
+				'known'   => get('budget_available'),
+				'unknown' => 'Recommendation needed'
+			};
+
+			$kirby->email([
+				'from'     => 'partners@getkirby.com',
+				'fromName' => 'Kirby Team',
+				'to'       => 'partners@getkirby.com',
+				'subject'  => 'New project lead',
+				'template' => 'partner-lead',
+				'data'     => [
+					'name'     => $name,
+					'email'    => $email,
+					'customer' => $customer,
+					'company'  => $company,
+					'contact'  => $contact,
+					'project'  => $project,
+					'partner'  => $partner,
+					'budget'   => $budget,
+					'language' => $language,
+				]
+			]);
+
+			go('partners/lead/success');
+
 		} catch (Exception $e) {
 			$message = $e->getMessage();
 		}
-
-		$name     = get('name');
-		$email    = get('email');
-		$customer = get('customer');
-		$company  = get('company');
-		$contact  = get('contact');
-		$project  = get('project');
-		$partner  = get('partner');
-		$language = get('language');
-		$budget   = match(get('budget')) {
-			'known'   => get('budget_available'),
-			'unknown' => 'Recommendation needed'
-		};
-
-		$kirby->email([
-			'from'     => 'partners@getkirby.com',
-			'fromName' => 'Kirby Team',
-			'to'       => 'partners@getkirby.com',
-			'subject'  => 'New project lead',
-			'template' => 'partner-lead',
-			'data'     => [
-				'name'     => $name,
-				'email'    => $email,
-				'customer' => $customer,
-				'company'  => $company,
-				'contact'  => $contact,
-				'project'  => $project,
-				'partner'  => $partner,
-				'budget'   => $budget,
-				'language' => $language,
-			]
-		]);
-
-		go('partners/lead/success');
 	}
 
 	return [
