e500108cbe36102e0fc8055d026fb792e382837f
Nico Hoffmann <me@nhoffmann.com>
2024-08-23 17:48:13 +0200

    More legacy redirects

--- site/routes/legacy.php
+++ site/routes/legacy.php
@@ -21,5 +21,32 @@ return [
 	[
 		'pattern' => 'docs/cookbook/content-representations/load-more-with-ajax',
 		'action'  => fn () => go('docs/cookbook/content-representations/ajax-load-more')
+	],
+	[
+		'pattern' => 'docs/cookbook/setup/ddev',
+		'action'  => fn () => go('docs/cookbook/development-deployment/ddev')
+	],
+	[
+		'pattern' => [
+			'docs/cheatsheet/(:any)/(:any)',
+			'docs/reference/(:any)/(:any)',
+			'docs/reference/objects/(:any)/(:any)',
+			'docs/reference/tools/(:any)/(:any)'
+		],
+		'action'  => function (string $object, string $method) {
+			if ($page = page('docs/reference/objects/cms/' . $object . '/' . $method)) {
+				go($page);
+			}
+
+			if ($page = page('docs/reference/objects/toolkit/' . $object . '/' . $method)) {
+				go($page);
+			}
+
+			if ($page = page('docs/reference/objects/http/' . $object . '/' . $method)) {
+				go($page);
+			}
+
+			$this->next();
+		}
 	]
 ];
