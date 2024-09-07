f116261b69ba0170523deb651bb15ad207b66a52
Nico Hoffmann <me@nhoffmann.com>
2024-09-07 15:12:34 +0200

    Add markthalleneun brand

--- site/snippets/templates/scenarios/brands.php
+++ site/snippets/templates/scenarios/brands.php
@@ -1,7 +1,7 @@
 <section id="brands" class="mb-42">
 	<h2 class="h2 mb-12"><?= $title ?></h2>
 	<ul class="brands items-center" style="--brands-columns: 5">
-		<?php foreach(collection('brands/' . $tag)->limit(10) as $brand): ?>
+		<?php foreach(collection('brands/' . $tag)->limit($limit ?? 10) as $brand): ?>
 		<?php snippet('brand', ['brand' => $brand]) ?>
 		<?php endforeach ?>
 	</ul>
