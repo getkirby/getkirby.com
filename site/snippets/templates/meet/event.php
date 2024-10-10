a6d75e530790dad5f57777546c2c1c5168b00425
Nico Hoffmann <me@nhoffmann.com>
2024-10-10 21:39:41 +0700

    Events: fix timezone support

--- site/snippets/templates/meet/event.php
+++ site/snippets/templates/meet/event.php
@@ -2,8 +2,17 @@
 	<h3><?= $event->icon() ?> <?= $event->shortTitle() ?></h3>
 
 	<?php if ($event->isUpcoming() === true): ?>
-	<localized-datetime date="<?= $event->date()->toDate('Y-m-d H:i:s') ?>"><?= $event->date()->toDate('D, j M Y, H:i T') ?></localized-datetime>
+		<localized-datetime
+			date="<?= $event->datetime()->format('c') ?>"
+			<?php if ($event->isMeetup()) : ?>
+			timezone="<?= $event->timezone() ?>"
+			<?php endif ?>
+		>
+			<?= $event->datetime()->format('D, j M Y, H:i T') ?>
+		</localized-datetime>
 	<?php else: ?>
-	<time date="<?= $event->date()->toDate('Y-m-d H:i:s') ?>"><?= $event->date()->toDate('D, j M Y') ?></time>
+	<time date="<?= $event->datetime()->format('c') ?>">
+		<?= $event->datetime()->format('D, j M Y') ?>
+	</time>
 	<?php endif ?>
 </a>
--- site/snippets/templates/meet/events.js.php
+++ site/snippets/templates/meet/events.js.php
@@ -2,11 +2,12 @@
 class LocalizedDatetimeElement extends HTMLElement {
 	connectedCallback() {
 		this.utc = new Date(this.getAttribute("date"));
-		this.innerText = this.format(this.utc);
+		this.timezone = this.getAttribute("timezone");
+		this.innerText = this.format(this.utc, this.timezone);
 	}
 
-	format(datetime) {
-		return datetime.toLocaleString("en-US", {
+	format(datetime, timezone) {
+		const options = {
 			weekday: "short",
 			day: "numeric",
 			month: "long",
@@ -15,7 +16,13 @@ class LocalizedDatetimeElement extends HTMLElement {
 			hour: "2-digit",
 			minute: "2-digit",
 			timeZoneName: "short"
-		})
+		};
+
+		if (timezone) {
+			options.timeZone = timezone;
+		}
+
+		return datetime.toLocaleString("en-US", options)
 	}
 }
 
