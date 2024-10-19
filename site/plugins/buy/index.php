ab210d6ad3c66b51df679a8b25f0dfcc299663fd
Lukas Bestle <lukas@getkirby.com>
2024-10-19 21:45:35 +0200

    Buy plugin: Switch to Composer setup

--- site/plugins/buy/index.php
+++ site/plugins/buy/index.php
@@ -3,16 +3,7 @@
 use Buy\RevenueLimit;
 use Kirby\Cms\App;
 
-load([
-	'Buy\Paddle'       => __DIR__ . '/Paddle.php',
-	'Buy\Passthrough'  => __DIR__ . '/Passthrough.php',
-	'Buy\Price'        => __DIR__ . '/Price.php',
-	'Buy\Product'      => __DIR__ . '/Product.php',
-	'Buy\RevenueLimit' => __DIR__ . '/RevenueLimit.php',
-	'Buy\Sale'         => __DIR__ . '/Sale.php',
-	'Buy\Upgrade'      => __DIR__ . '/Upgrade.php',
-	'Buy\Visitor'      => __DIR__ . '/Visitor.php',
-]);
+@include_once __DIR__ . '/vendor/autoload.php';
 
 App::plugin('getkirby/buy', [
 	'tags' => [
---
+++ site/plugins/buy/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'getkirby/buy',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '8850c8ab6e85028565bdea8ba8b5504c80712002',
+        'type' => 'kirby-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'getkirby/buy' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '8850c8ab6e85028565bdea8ba8b5504c80712002',
+            'type' => 'kirby-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'getkirby/composer-installer' => array(
+            'pretty_version' => '1.2.1',
+            'version' => '1.2.1.0',
+            'reference' => 'c98ece30bfba45be7ce457e1102d1b169d922f3d',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../getkirby/composer-installer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
