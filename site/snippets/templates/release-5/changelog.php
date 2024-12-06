012a2bf7bbeab949756be9005e65c8f685d8eadb
Nico Hoffmann <me@nhoffmann.com>
2024-12-06 20:09:44 +0100

    v5 changelog: start as open

--- site/snippets/templates/release-5/changelog.php
+++ site/snippets/templates/release-5/changelog.php
@@ -1,4 +1,4 @@
-<details class="bg-light rounded release-changelog">
+<details open class="bg-light rounded release-changelog">
 	<summary><?= $title ?></summary>
 	<div class="prose"><?= $changes->kt() ?></div>
 </details>
