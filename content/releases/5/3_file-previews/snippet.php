9a2c08d3668f6764674c9a46c333a158999b225a
Nico Hoffmann <me@nhoffmann.com>
2024-12-06 15:33:07 +0100

    v5 release: change order of features

---
+++ content/releases/5/3_file-previews/snippet.php
@@ -0,0 +1,13 @@
+<div class="columns" style="--columns: 5">
+	<figure class="release-box bg-light grid place-items-center" style="--span: 5">
+		<video controls autoplay muted loop class="rounded shadow-xl" style="width: 100%">
+			<source src="<?= $section->file('file-previews.mp4')->url() ?>" type="video/mp4">
+		</video>
+	</figure>
+	<div class="release-text-box" style="--span: 2">
+		<?php snippet('templates/release-40/teaser', ['section' => $section]) ?>
+	</div>
+	<figure class="release-code-box text-lg" style="--span: 3">
+		<?= $section->class()->kt() ?>
+	</figure>
+</div>
