b8f218a146fe4bdd93a5563eac04698e878b7637
Lukas Bestle <lukas@getkirby.com>
2024-12-07 18:41:26 +0100

    Fix contributor avatar sizing
    We added a global `height: auto` reset since then, which makes the browser ignore the size attributes on the img elements

--- site/snippets/templates/release-38/contributors.php
+++ site/snippets/templates/release-38/contributors.php
@@ -14,7 +14,7 @@
 			] as $contributor): ?>
 			<li class="inline-flex p-1">
 				<a href="https://github.com/<?= $contributor ?>" class="p-3 text-sm font-mono shadow bg-white rounded flex items-center">
-					<img alt="" src="https://github.com/<?= $contributor ?>.png?size=64" style="border-radius: 100%; width: auto" class="mr-3" width="32" height="32" loading="lazy"> @<?= $contributor ?>
+					<img alt="" src="https://github.com/<?= $contributor ?>.png?size=64" style="border-radius: 100%; width: 32px" class="mr-3" width="32" height="32" loading="lazy"> @<?= $contributor ?>
 				</a>
 			</li>
 			<?php endforeach ?>
