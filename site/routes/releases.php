f904a259117052eef990ad5e302c574856fbb948
Lukas Bestle <lukas@getkirby.com>
2025-01-03 18:00:19 +0100

    Route for x.y.z release URLs
    Used for changelog URLs in the update check

--- site/routes/releases.php
+++ site/routes/releases.php
@@ -8,6 +8,13 @@ return [
 			(($p = page('releases/' . $major . '-0')) ? go($p) : null)
 	],
 	[
+		'pattern' => 'releases/(:num)\.(:num)\.(:num)',
+		'action'  => fn ($major, $minor, $patch) =>
+			(($minor === '0' && $patch === '0' && $p = page('releases/' . $major)) ? go($p) : null) ??
+			(($patch === '0' && $p = page('releases/' . $major . '-' . $minor)) ? go($p) : null) ??
+			go('https://github.com/getkirby/kirby/releases/tag/' . $major . '.' . $minor . '.' . $patch)
+	],
+	[
 		'pattern' => 'releases/(:num)/(:all?)',
 		'action'  => fn ($major, $path) =>
 			page('releases/' . $major . '/' . $path) ??
