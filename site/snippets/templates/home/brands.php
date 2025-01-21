4105f1ae91927f35e50b9c8846ff14cc82e120f7
Nico Hoffmann <me@nhoffmann.com>
2025-01-21 21:37:04 +0100

    Add new brands

--- site/snippets/templates/home/brands.php
+++ site/snippets/templates/home/brands.php
@@ -1,7 +1,7 @@
 <section id="brands" class="mb-42">
 	<h2 class="h2 mb-12">Trusted by clients world&#8209;wide</h2>
 	<ul class="brands items-center">
-		<?php foreach(collection('brands/featured')->limit(18) as $brand): ?>
+		<?php foreach(collection('brands/featured')->limit(24) as $brand): ?>
 		<?php snippet('brand', ['brand' => $brand]) ?>
 		<?php endforeach ?>
 	</ul>
