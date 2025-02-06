209de254dbaf0eabdd9ac6992b088468e6d00108
Sonja Broda <github@texniq.de>
2025-02-06 22:36:15 +0100

    Implement code suggestion (apart from compact)

--- site/templates/meet.json.php
+++ site/templates/meet.json.php
@@ -1,40 +1,33 @@
 <?php
 /**
- * @var Kirby\Cms\Pages $upcoming
- * @var Kirby\Cms\Pages $past
+ * @var \Kirby\Cms\Pages $upcoming
+ * @var \Kirby\Cms\Pages $past
  */
-$upcomingEvents = [];
-$pastEvents     = [];
 
 $since = param('since');
 
-foreach ($upcoming as $event) {
-	$upcomingEvents[] = [
-		'title'    => $event->shortTitle()->value(),
-		'date'     => $event->datetime()->format('c'),
-		'timezone' => $event->timezone()->value(),
-		'link'     => $event->link()->value(),
-		'isMeetup' => $event->isMeetup(),
-	
-	];
-}
+$upcomingEvents = $upcoming->toArray(fn($event) => [
+	'title'    => $event->shortTitle()->value(),
+	'date'     => $event->datetime()->format('c'),
+	'timezone' => $event->timezone()->value(),
+	'link'     => $event->link()->value(),
+	'isMeetup' => $event->isMeetup(),
+]);
 
 // Only meetups, no other events
 $past = $past->filterBy('city', '!=', '');
 
 // Use param since to filter meetups after given date
-if ($since = param('since')) {
+if (is_string($since)) {
 	$past = $past->filterBy('num', '>', $since);
 }
 
-foreach ($past as $event) {
-	$pastEvents[] = [
-		'title' => $event->shortTitle()->value(),
-		'date'  => $event->datetime()->format('c'),
-		'link'  => $event->link()->value(),
-		'isMeetup' => $event->isMeetup(),
-	];
-}
+$pastEvents = $past->toArray(fn($event) => [
+	'title'    => $event->shortTitle()->value(),
+	'date'     => $event->datetime()->format('c'),
+	'link'     => $event->link()->value(),
+	'isMeetup' => $event->isMeetup(),
+]);
 
 echo json([
 	'upcoming' => $upcomingEvents,
