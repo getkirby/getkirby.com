51747a97262d9434fe326caf8f238335d77141e9
Bastian Allgeier <mail@bastianallgeier.com>
2025-02-11 10:41:32 +0100

    Shrink image and add alt text

--- content/releases/5/4_batch-delete/snippet.php
+++ content/releases/5/4_batch-delete/snippet.php
@@ -20,8 +20,8 @@
 <div class="v5-batch-columns">
 	<figure class="release-box bg-light grid place-items-center" style="grid-area: screenshot">
 		<?php snippet('templates/release-4/image', [
-			'alt' => '',
-			'img' => $section->image('batch-delete.png')->resize(850)
+			'alt' => 'A screenshot of the fields section. Batch selection mode has been activated and multiple files are selected. A delete button at the top can delete all the selected files at once.',
+			'img' => $section->image('batch-delete.jpg')->resize(850)
 		]) ?>
 	</figure>
 	<div class="v5-uploads-teaser release-text-box" style="grid-area: teaser">
