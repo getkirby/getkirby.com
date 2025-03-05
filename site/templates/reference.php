a30f93c2a5f5b716155106d9556002b750c1c20c
Nico Hoffmann <me@nhoffmann.com>
2025-03-05 23:01:18 +0100

    Fix reference overview page

--- site/templates/reference.php
+++ site/templates/reference.php
@@ -4,20 +4,26 @@
 <section class="mb-24">
 	<h2 class="h2 mb-12" id="<?= $group->slug() ?>"><?= $group->title() ?></h2>
 
-	<?php foreach ($group->children()->listed() as $section): ?>
+	<?php
+	foreach ($group->children()->listed() as $section):
+
+	if ($section->intendedTemplate()->name() === 'reference-quicklink') {
+		$section = $section->link()->toPage();
+	}
+
+	$children = $section->children()->listed();
+
+	if ($children->count() > 0):
+	?>
 	<section>
 		<h3 class="text-sm font-bold mb-3" id="<?= $section->slug() ?>">
 			<a href="#<?= $section->slug() ?>">
 				<?= $section->title() ?>
 			</a>
 		</h3>
-		<?php
-	if ($section->intendedTemplate()->name() === 'reference-quicklink') {
-		$section = $section->link()->toPage();
-	}
-	snippet('templates/reference/section', $section->children()->listed());
-		?>
-	</section>
+		<?php snippet('templates/reference/section', $children) ?>
+		</section>
+	<?php endif ?>
 	<?php endforeach ?>
 </section>
 <?php endforeach ?>
