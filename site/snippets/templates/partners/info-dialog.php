79347fb0d46e3ecd07418d174c977a6aa9f2de5b
Lukas Bestle <lukas@getkirby.com>
2025-03-06 19:44:04 +0100

    General code cleanup

--- site/snippets/templates/partners/info-dialog.php
+++ site/snippets/templates/partners/info-dialog.php
@@ -57,10 +57,10 @@
 			<ul class="certified-checklist text-base">
 				<li><?= icon('verified') ?> Code quality</li>
 				<li><?= icon('verified') ?> Performance</li>
-				<li><?= icon('verified') ?> Privacy & Security</li>
-				<li><?= icon('verified') ?> Semantics & Accessibility</li>
-				<li><?= icon('verified') ?> Panel layout & Usability</li>
-				<li><?= icon('verified') ?> Responsiveness & Modularity</li>
+				<li><?= icon('verified') ?> Privacy &amp; Security</li>
+				<li><?= icon('verified') ?> Semantics &amp; Accessibility</li>
+				<li><?= icon('verified') ?> Panel layout &amp; Usability</li>
+				<li><?= icon('verified') ?> Responsiveness &amp; Modularity</li>
 				<li><?= icon('verified') ?> Code documentation</li>
 			</ul>
 		</div>
