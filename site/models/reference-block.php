4b117cdab0e855e571bca340b954d4656e7e9ec6
Nico Hoffmann <me@nhoffmann.com>
2025-03-08 14:09:11 +0100

    Blocks: improve links to source code

--- site/models/reference-block.php
+++ site/models/reference-block.php
@@ -1,5 +1,13 @@
 <?php
 
+use Kirby\Cms\App;
+
 class ReferenceBlockPage extends ReferenceArticlePage
 {
+	public function source(): string
+	{
+		$url  = option('github.url') . '/kirby/tree/' . App::version();
+		$url .= '/config/blocks/' . $this->name() . '/';
+		return $url;
+	}
 }
