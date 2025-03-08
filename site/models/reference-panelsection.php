847f94340e4cb84d7292241c8996a2325a38269d
Nico Hoffmann <me@nhoffmann.com>
2025-03-08 14:09:11 +0100

    Blocks: improve links to source code

--- site/models/reference-panelsection.php
+++ site/models/reference-panelsection.php
@@ -4,7 +4,7 @@ use Kirby\Cms\App;
 
 class ReferencePanelSectionPage extends ReferenceArticlePage
 {
-	public function source(): string|null
+	public function source(): string
 	{
 		$url  = option('github.url') . '/kirby/tree/' . App::version();
 		$url .= '/config/sections/' . $this->name() . '.php';
