f2d02723efa117f313c0fead87be3a8ffd1b6583
Ahmet Bora <byybora@gmail.com>
2025-04-09 00:29:53 +0300

    Add webcal url to open in application

---
+++ site/models/meet.php
@@ -0,0 +1,15 @@
+<?php
+
+use Kirby\Cms\Page;
+
+class MeetPage extends Page
+{
+	/**
+	 * Returns the URL to the iCalendar file for this page
+	 * `webcal` protocol is used to allow users to add the calendar to their own
+	 */
+	public function webcalUrl(): string
+	{
+		return 'webcal://' . $this->kirby()->environment()->host() . '/' . $this->slug() . '.ical';
+	}
+}
