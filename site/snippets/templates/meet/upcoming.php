05931ba299d978040510f4e20257890ce227b2e0
Bastian Allgeier <mail@bastianallgeier.com>
2025-04-09 09:54:47 +0200

    Fix responsiveness

--- site/snippets/templates/meet/upcoming.php
+++ site/snippets/templates/meet/upcoming.php
@@ -3,7 +3,7 @@
 		<header class="flex justify-between items-center mb-6">
 			<h2 class="h2">Upcoming events</h2>
 
-			<nav class="flex" style="gap: var(--spacing-3)">
+			<nav class="flex flex-wrap" style="gap: var(--spacing-3)">
 				<a
 					class="btn btn--outlined"
 					href="<?= $page->webcalUrl() ?>"
@@ -22,4 +22,29 @@
 		</header>
 		<?php snippet('templates/meet/events', ['events' => $events]) ?>
 	</section>
+
+	<style>
+		@media screen and (max-width: 45rem) {
+			.events header {			
+				flex-direction: column;
+				gap: var(--spacing-6);
+				align-items: start;
+			}
+			.events header > * {
+				flex-grow: 1;
+				width: 100%;
+			}
+			.events header .btn {
+				flex-grow: 1;
+				padding-inline: var(--spacing-3);
+			}
+		}
+
+		@media screen and (max-width: 50rem) {
+			.events .event {
+				flex-direction: column;
+				align-items: start;
+			}
+		}
+	</style>
 <?php endif ?>
