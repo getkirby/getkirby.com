8f6a2b183ac73504a850534a06c7f12ce86d08c0
Nico Hoffmann <me@nhoffmann.com>
2025-04-09 21:27:49 +0200

    Meet events: support end time

--- site/snippets/templates/meet/calendar.php
+++ site/snippets/templates/meet/calendar.php
@@ -2,10 +2,11 @@
 BEGIN:VEVENT
 UID:meetup-<?= $event->slug() . "\r\n" ?>
 SUMMARY:<?= $event->foldTitle() . "\r\n" ?>
-DTSTART:<?= $event->datetimeUtc()->format('Ymd\THis\Z') . "\r\n" ?>
+DTSTART:<?= $event->datetime('UTC')->format('Ymd\THis\Z') . "\r\n" ?>
+DTEND:<?= $event->datetime('UTC', 'end')->format('Ymd\THis\Z') . "\r\n" ?>
 DTSTAMP:<?= $event->modified('Ymd\THis\Z') . "\r\n" ?>
 <?php if ($event->city()->isNotEmpty()): ?>
-LOCATION:<?= $event->city() ?>,<?= $event->country() . "\r\n" ?>
+LOCATION:<?= $event->city() ?>, <?= $event->country() . "\r\n" ?>
 <?php endif ?>
 <?php if ($event->link()->isNotEmpty()): ?>
 URL:<?= $event->foldLink() . "\r\n" ?>
