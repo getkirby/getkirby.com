8801b7a20b82695ce692159530f1115eb4a72552
Lukas Bestle <lukas@getkirby.com>
2025-05-03 16:18:59 +0200

    Open FAQ entries automatically by the URL hash

--- site/snippets/faq.php
+++ site/snippets/faq.php
@@ -1,10 +1,28 @@
 <div class="faq">
 	<?php foreach ($questions as $question): ?>
-	<details class="details">
-		<summary class="py-3 border-top" id="<?= $question->slug() ?>"><?= $question->title()->kti()->widont() ?></summary>
+	<details class="details" id="<?= $question->slug() ?>">
+		<summary class="py-3 border-top"><?= $question->title()->kti()->widont() ?></summary>
 		<div class="py-3 prose text-base">
 			<?= $question->text()->kt() ?>
 		</div>
 	</details>
 	<?php endforeach ?>
 </div>
+
+<script>
+function openFaqById(hash) {
+	const element = document.querySelector(hash);
+
+	if (element instanceof HTMLDetailsElement) {
+		element.open = true;
+	}
+}
+
+// open the FAQ entry onload
+if (window.location.hash) {
+	openFaqById(window.location.hash);
+}
+
+// open the FAQ entry when the hash in the URL changes (e.g. by clicking a relevant link)
+window.addEventListener('hashchange', () => openFaqById(window.location.hash));
+</script>
