ede79d8d71c141ae3932a70613805a69ee52b80f
Nico Hoffmann <me@nhoffmann.com>
2025-05-22 18:07:11 +0200

    feat: Allow filtering events by city in calendar

--- site/controllers/meet.php
+++ site/controllers/meet.php
@@ -13,6 +13,12 @@ return function (App $kirby) {
 		$kirby->response()->header('Expires', gmdate('D, d M Y H:i:s T', $time));
 	}
 
+	if ($city = get('city')) {
+		$upcoming = $upcoming->filterBy(
+			fn ($event) => strtolower($event->city()) === strtolower($city)
+		);
+	}
+
 	return [
 		'events'   => $events,
 		'upcoming' => $upcoming->sortBy('num', 'asc'),
