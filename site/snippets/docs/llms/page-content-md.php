ed88de2abf08d7fff48ff2907c2a2e1634f1ae4a
Ahmet Bora <byybora@gmail.com>
2025-06-04 23:37:16 +0300

    chore: Clean up html converter codes

--- site/snippets/docs/llms/page-content-md.php
+++ site/snippets/docs/llms/page-content-md.php
@@ -8,14 +8,17 @@ use League\HTMLToMarkdown\HtmlConverter;
 use League\HTMLToMarkdown\Converter\HeaderConverter;
 use League\HTMLToMarkdown\Converter\TableConverter;
 
-$converter = new HtmlConverter();
-$converter->getConfig()->setOption('strip_tags', true);
-$converter->getConfig()->setOption('hard_break', true);
-$converter->getConfig()->setOption('header_style', HeaderConverter::STYLE_ATX);
-$converter->getEnvironment()->addConverter(new HeaderConverter());
+$converter = new HtmlConverter([
+	'strip_tags'   => true,
+	'hard_break'   => true,
+	'remove_nodes' => 'script style',
+	'header_style' => HeaderConverter::STYLE_ATX
+]);
+
 $converter->getEnvironment()->addConverter(new TableConverter());
 
 echo '# ' . $page->title() . PHP_EOL . PHP_EOL;
+
 if ($page->intro()->isNotEmpty()) {
 	echo $converter->convert($page->intro()->kirbytext()) . PHP_EOL . PHP_EOL;
 } elseif ($page->description()->isNotEmpty()) {
