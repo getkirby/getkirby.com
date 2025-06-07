d4d7b6c36f28f052c58696161fda8f5343a6fce4
Nico Hoffmann <me@nhoffmann.com>
2025-06-07 13:57:09 +0200

    test:  `Refelectable\ReflectableClass`

---
+++ site/plugins/site/tests/Reference/Reflectable/ReflectableClassTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Kirby\Reference\Reflectable;
+
+use Kirby\Cms\App;
+use Kirby\Reference\Reflectable\Tags\Throws;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(ReflectableClass::class)]
+#[CoversClass(Reflectable::class)]
+class ReflectableClassTest extends TestCase
+{
+	public function setUp(): void
+	{
+		require_once __DIR__ . '/fixtures/class.php';
+	}
+
+	public function testExamples(): void
+	{
+		$reflectable = new ReflectableClass('Bar\FooWithDocBlock');
+		$this->assertSame('```php
+new FooWithDocBlock();
+```', $reflectable->examples());
+	}
+
+	public function testIsDeprecated(): void
+	{
+		$reflectable = new ReflectableClass('Bar\Foo');
+		$this->assertFalse($reflectable->isDeprecated());
+
+		$reflectable = new ReflectableClass('Bar\FooWithDocBlock');
+		$this->assertTrue($reflectable->isDeprecated());
+		$this->assertSame('6.0.0', $reflectable->deprecated()->version());
+		$this->assertSame('Use Bar\Foo instead', $reflectable->deprecated()->description());
+	}
+
+	public function testIsInternal(): void
+	{
+		$reflectable = new ReflectableClass('Bar\Foo');
+		$this->assertFalse($reflectable->isInternal());
+
+		$reflectable = new ReflectableClass('Bar\FooWithDocBlock');
+		$this->assertTrue($reflectable->isInternal());
+	}
+
+	public function testIsStatic(): void
+	{
+		$reflectable = new ReflectableClass('Bar\Foo');
+		$this->assertFalse($reflectable->isStatic());
+
+		$reflectable = new ReflectableClass('Bar\FooStatic');
+		$this->assertTrue($reflectable->isStatic());
+	}
+
+	public function testIsTrait(): void
+	{
+		$reflectable = new ReflectableClass('Bar\Foo');
+		$this->assertFalse($reflectable->isTrait());
+
+		$reflectable = new ReflectableClass('Bar\FooTrait');
+		$this->assertTrue($reflectable->isTrait());
+	}
+
+	public function testMethods(): void
+	{
+		$reflectable = new ReflectableClass('Bar\Foo');
+		$this->assertCount(3, $reflectable->methods());
+		$this->assertSame('__construct', $reflectable->methods()[0]->getName());
+		$this->assertSame('bar', $reflectable->methods()[1]->getName());
+		$this->assertSame('baz', $reflectable->methods()[2]->getName());
+	}
+
+	public function testName(): void
+	{
+		$reflectable = new ReflectableClass('Bar\Foo');
+		$this->assertSame('Bar\Foo', $reflectable->name());
+		$this->assertSame('Foo', $reflectable->name(short: true));
+	}
+
+	public function testSee(): void
+	{
+		$reflectable = new ReflectableClass('Bar\FooWithDocBlock');
+		$this->assertSame('Bar\Foo', $reflectable->see());
+	}
+
+	public function testSince(): void
+	{
+		$reflectable = new ReflectableClass('Bar\FooWithDocBlock');
+		$this->assertSame('5.0.0', $reflectable->since()->version());
+	}
+
+	public function testSource(): void
+	{
+		$reflectable = new ReflectableClass('Bar\Foo');
+		$this->assertSame('https://github.com/getkirby/kirby/tree/' . App::instance()->version() . '/src/Bar/Foo.php#L5', $reflectable->source());
+	}
+
+	public function testSummary(): void
+	{
+		$reflectable = new ReflectableClass('Bar\FooWithDocBlock');
+		$this->assertSame('This is a class with a doc block', $reflectable->summary());
+	}
+
+	public function testThrows(): void
+	{
+		$reflectable = new ReflectableClass('Bar\FooWithDocBlock');
+		$this->assertInstanceOf(Throws::class, $reflectable->throws());
+
+		$exception = $reflectable->throws()->toArray()[0];
+		$this->assertSame('Exception', $exception->types()->toString());
+		$this->assertSame('when foo is not found', $exception->description());
+	}
+}
---
+++ site/plugins/site/tests/Reference/Reflectable/fixtures/class.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Bar;
+
+class Foo {
+	public function __construct()
+	{
+	}
+
+	public function bar()
+	{
+	}
+
+	public function baz()
+	{
+	}
+}
+
+class FooStatic {
+}
+
+trait FooTrait {
+}
+
+/**
+ * This is a class with a doc block
+ *
+ * ```php
+ * new FooWithDocBlock();
+ * ```
+ *
+ * @see Bar\Foo
+ * @since 5.0.0
+ * @deprecated 6.0.0 Use Bar\Foo instead
+ * @internal
+ * @throws Exception when foo is not found
+ */
+class FooWithDocBlock {
+}
