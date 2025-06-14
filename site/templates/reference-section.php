36b697afa78023722322ecb0b5257ac9b4cbc027
Nico Hoffmann <me@nhoffmann.com>
2025-06-14 15:43:00 +0200

    fix: Duplicate ToC on Reference section pages

--- site/templates/reference-section.php
+++ site/templates/reference-section.php
@@ -1,13 +1,20 @@
 <?php layout('reference') ?>
 
+<!-- Enforce empty default ToC slot -->
+<?php slot('toc') ?>
+<?php endslot() ?>
+
+<?php slot() ?>
 <?php snippet('templates/reference/section-filter') ?>
 
 <div class="mb-24">
 	<?php snippet('templates/reference/section', $entries) ?>
 </div>
 
+<!-- Custom ToC position -->
 <?php snippet('toc') ?>
 
 <div class="prose">
 	<?= $page->text()->kt() ?>
 </div>
+<?php endslot() ?>
