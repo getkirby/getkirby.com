e314faeea70a21310d1e8996f6de43115c275f0b
Lukas Bestle <lukas@getkirby.com>
2025-06-20 20:23:17 +0200

    feat: Flip docs archive order
    Newest release on top

--- site/templates/archive.php
+++ site/templates/archive.php
@@ -11,7 +11,7 @@
 	<ul class="columns items-center" style="--columns-sm: 1; --columns-md: 1; --columns-lg: 3; --gap: var(--spacing-12)">
 		 <?php foreach ($versions as $version): ?>
 		<li>
-			<article class="<?= $version === end($versions) ? 'p-6 bg-white shadow-2xl' : '' ?>">
+			<article class="<?= $version === $versions[0] ? 'p-6 bg-white shadow-2xl' : '' ?>">
 				<header class="mb-3">
 					<p class="h6 mb-1"><?= $version['since'] ?></p>
 					<h2 class="h2 border-top pt-3"><?= $version['title'] ?></h2>
