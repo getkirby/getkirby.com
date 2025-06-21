0a6d711db6f67a627d3cbb704f624841f1f1021a
Lukas Bestle <lukas@getkirby.com>
2025-06-21 21:19:14 +0200

    fix: v5 changelog duplicated headline IDs

--- site/snippets/templates/release-5/changelog.php
+++ site/snippets/templates/release-5/changelog.php
@@ -1,4 +1,4 @@
 <details open id="<?= Str::slug($title) ?>" class="bg-light rounded release-changelog">
 	<summary><?= $title ?></summary>
-	<div class="prose"><?= $changes->kt() ?></div>
+	<div class="prose"><?= $changes->kt(['markdown' => ['idPrefix' => Str::slug($title)]]) ?></div>
 </details>
