8b173f25ee19baa80a5dceaf2cf779f26df250e9
Nico Hoffmann <me@nhoffmann.com>
2025-06-22 16:15:22 +0200

    fix: Try now only on current release page
    Docs link to archived docs for older release pages.

--- site/snippets/templates/releases/cta.php
+++ site/snippets/templates/releases/cta.php
@@ -1,18 +1,29 @@
 <?php
 
-snippet('cta', [
-	'buttons' => [
-		[
-			'text' => 'Docs',
-			'link' => $page->docs()->or('/docs'),
-			'icon' => 'book',
-			'style' => 'outlined'
-		],
-		[
-			'text' => 'Try now',
-			'link' => $page->tryLink()->or('/try'),
-			'icon' => 'download'
-		],
+$version = $page->versionField();
+$docs    = $kirby->option('versions')[$version->value()]['link'] ?? '/docs';
+
+$buttons = [
+	[
+		'text' => 'Docs',
+		'link' => $page->docs()->or($docs),
+		'icon' => 'book'
 	],
+];
+
+if ((int)$kirby->version() === $version->toInt()) {
+	$buttons[] = [
+		'text' => 'Try now',
+		'link' => $page->tryLink()->or('/try'),
+		'icon' => 'download'
+	];
+}
+
+if (count($buttons) > 1) {
+	$buttons[0]['style'] = 'outlined';
+}
+
+snippet('cta', [
+	'buttons' => $buttons,
 	...$options ?? []
 ]);
