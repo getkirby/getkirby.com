5220bd5f86801e3edb0890e5f39bc48b978323b6
Nico Hoffmann <me@nhoffmann.com>
2025-06-23 23:38:57 +0200

    perf: Load reduced template when replacing content

---
+++ site/templates/home.plygrnd.php
@@ -0,0 +1,4 @@
+<?php snippet('templates/home/playground/filesystem') ?>
+<?php snippet('templates/home/playground/backend') ?>
+<?php snippet('templates/home/playground/medium') ?>
+<?php snippet('templates/home/playground/audience') ?>
