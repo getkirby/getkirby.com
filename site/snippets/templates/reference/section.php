3ba06fad50583abc469de68f44c8ca0a6b0669f0
Nico Hoffmann <me@nhoffmann.com>
2025-06-23 11:16:33 +0200

    fix: API endpoints section overview widths

--- site/snippets/templates/reference/section.php
+++ site/snippets/templates/reference/section.php
@@ -1,6 +1,13 @@
+<?php
+$min = match ($page->intendedTemplate()->name()) {
+	'reference-endpoints' => '30rem',
+	default               => '15rem',
+};
+?>
+
 <ul
 	class="reference-section mb-12 auto-fill"
-	style="--min: 15rem; --row-gap: 0; --column-gap: var(--spacing-3)"
+	style="--min: <?= $min ?>; --row-gap: 0; --column-gap: var(--spacing-3)"
 >
 	<?php foreach ($item as $entry): ?>
 	<li
