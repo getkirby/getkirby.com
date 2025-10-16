9d291ff0716be643bc804beb54bc96529bdd0374
Sonja Broda <github@texniq.de>
2025-10-16 23:14:20 +0200

    Redirect unlisted pages to parent if token not present

--- site/templates/partner.php
+++ site/templates/partner.php
@@ -1,3 +1,11 @@
+<?php
+
+if ($page->status() !== 'listed' &&
+    get('token') !== $page->token()->value()
+) {
+    go($page->parent());
+}
+?>
 <?php layout() ?>
 
 <style>
@@ -48,7 +56,6 @@
 	color: var(--color-gray-700);
 }
 </style>
-
 <header class="mb-12">
 	<h1 class="h1 mb-1">
 		<?= $page->title() ?>
@@ -57,7 +64,6 @@
 		<?= $page->subtitle() ?>
 	</p>
 </header>
-
 <div class="partner-grid columns mb-24">
 	<figure style="--aspect-ratio: 3/2;" class="partner-hero rounded overflow-hidden mb-3">
 		<?php if ($image = $page->card()): ?>
