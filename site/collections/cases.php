d331587209a9454856c27064261c01364e493789
Sonja Broda <github@texniq.de>
2026-02-14 19:17:44 +0100

    fix: Improve filtering by links, removing trailing slash

--- site/collections/cases.php
+++ site/collections/cases.php
@@ -1,19 +1,22 @@
 <?php
 
 return function ($site) {
-
-    $cases = $site
-        ->find('love')
-        ->children()
-        ->listed();
-
-    $partnerGallery = $site
-        ->find('partners')
-        ->grandChildren()
-        ->filterBy('showcase', true)
-        ->filterBy('link', 'not in', $cases->pluck('link', ','));
-
-
-    return $cases
-        ->add($partnerGallery);
+	
+	$cases = $site
+		->find('love')
+		->children()
+		->listed();
+	
+	$partnerGallery = $site
+		->find('partners')
+		->grandChildren()
+		->filterBy('showcase', true)
+		->filter(fn($item) => in_array(
+				rtrim($item->link()->value()),
+				array_map(fn($link) => rtrim($link, '/'),
+					$cases->pluck('link', ',')
+				), true) === false);
+	
+	return $cases
+		->add($partnerGallery);
 };
