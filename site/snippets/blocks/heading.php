06f15654d9e38a67cbc2abbc0983f7bd09d3f3ca
Sonja Broda <github@texniq.de>
2026-03-07 20:32:36 +0100

    fix: Remove clean up code

--- site/snippets/blocks/heading.php
+++ site/snippets/blocks/heading.php
@@ -5,14 +5,10 @@
 use Kirby\Cms\Block;
 
 // Remove emojis and whitespace from start of string
-$clean = preg_replace(
-	'/^(?:[\p{Extended_Pictographic}\x{2600}-\x{27BF}](?:\x{FE0F}|\x{200D}[\p{Extended_Pictographic}\x{2600}-\x{27BF}])*)+\s*/u',
-	'',
-	$block->text()->value()
-);
+
 ?>
 <<?= $level = $block->level()->or('h2') ?> id="<?= $block->text()->slug() ?>">
 <a href="#<?= $block->text()->slug() ?>">
-	<?= ltrim($clean, ' ') ?>
+	<?= $block->text()->value() ?>
 </a>
 </<?= $level ?>>
