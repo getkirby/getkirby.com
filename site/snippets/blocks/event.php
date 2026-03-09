20d3812e8f31a9f6e9addb8ff281d7539aabd322
Sonja Broda <github@texniq.de>
2026-03-09 21:53:04 +0100

    refact: Fix indentation in snippets

--- site/snippets/blocks/event.php
+++ site/snippets/blocks/event.php
@@ -7,8 +7,8 @@ use Kirby\Cms\Block;
 ?>
 
 <p>
-    <strong>
-        <a href="<?= $block->link()->value() ?>"><?= $block->title()->value() ?></a>
-    </strong><br>
+	<strong>
+		<a href="<?= $block->link()->value() ?>"><?= $block->title()->value() ?></a>
+	</strong><br>
 	<?= $block->date()->toDate('D F d, Y \a\t H:i T') ?>
 </p>
--- site/snippets/blocks/image.php
+++ site/snippets/blocks/image.php
@@ -9,17 +9,13 @@ $url  = $block->url()->esc();
 $link = $block->link();
 
 if ($url): ?>
-    <figure class="image">
-		<?php
-		if ($link->isNotEmpty()): ?>
-            <a href="<?= Str::esc($link->toUrl()) ?>">
-                <img src="<?= $url ?>" alt="<?= $alt->esc() ?>">
-            </a>
-		<?php
-		else: ?>
-            <img src="<?= $url ?>" alt="<?= $alt->esc() ?>">
-		<?php
-		endif ?>
-    </figure>
-<?php
-endif ?>
+	<figure class="image">
+		<?php if ($link->isNotEmpty()): ?>
+			<a href="<?= Str::esc($link->toUrl()) ?>">
+				<img src="<?= $url ?>" alt="<?= $alt->esc() ?>">
+			</a>
+		<?php else: ?>
+			<img src="<?= $url ?>" alt="<?= $alt->esc() ?>">
+		<?php endif ?>
+	</figure>
+<?php endif ?>
