c2e9a54a91342f5788274c2f1e76d1b70aa8bf5e
Bastian Allgeier <mail@bastianallgeier.com>
2026-03-17 13:14:08 +0100

    a11y: Add label to sidebar navigation

--- site/snippets/sidebar.php
+++ site/snippets/sidebar.php
@@ -1,8 +1,8 @@
-<nav class="sidebar">
+<nav class="sidebar" aria-labelledby="sidebar-title">
 	<header class="sidebar-header mb-12">
 		<?php if ($title ?? null): ?>
 		<p class="h1 color-gray-400">
-			<a href="<?= $link ?? '#' ?>"><?= $title ?></a>
+			<a id="sidebar-title" href="<?= $link ?? '#' ?>"><?= $title ?></a>
 		</p>
 		<?php endif ?>
 
