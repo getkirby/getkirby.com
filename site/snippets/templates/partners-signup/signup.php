36370a87974e48f2e1cc9c10668fea3c676deb30
Sonja Broda <github@texniq.de>
2026-03-25 21:37:35 +0100

    refact: Hide honeypot block via inline styles

--- site/snippets/templates/partners-signup/signup.php
+++ site/snippets/templates/partners-signup/signup.php
@@ -89,11 +89,6 @@
 [v-cloak] {
 	display: none !important;
 }
-
-.offset {
-	position:absolute;
-	left:-9999px;
-}
 </style>
 
 <div v-scope id="signup" class="signup bg-white rounded" @mounted="mounted">
@@ -105,11 +100,16 @@
 		@submit="submit"
 	>
 		<input type="hidden" name="timestamp" :value="locale.timestamp">
-		<div class="offset">
-			<label for="date_of_birth">Date of birth</label>
-			<input type="text" name="date_of_birth" autocomplete="off" class="offset" tabindex="-1">
-		</div>
 		<input type="hidden" name="csrf" value="<?= csrf() ?>">
+		<div style="position:absolute;width:1px;height:1px;padding:0;margin:-1px;overflow:hidden;clip:rect(0,0,0,0);white-space:nowrap;border-width:0;">
+			<label for="date_of_birth">Date of birth</label>
+			<input
+				type="text"
+				aria-hidden="true"
+				tabindex="-1"
+				name="date_of_birth"
+			>
+		</div>
 		<div>
 			<fieldset class="mb-6">
 				<legend class="label">Partnership</legend>
