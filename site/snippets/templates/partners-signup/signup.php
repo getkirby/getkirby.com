f4ad5fd380a09f27ae8a139cfed05f3fe0972d0d
Sonja Broda <github@texniq.de>
2026-06-22 19:01:27 +0200

    fix: Restore honeypot field

--- site/snippets/templates/partners-signup/signup.php
+++ site/snippets/templates/partners-signup/signup.php
@@ -100,6 +100,15 @@
 		@submit="submit"
 	>
 		<input type="hidden" name="timestamp" :value="locale.timestamp">
+		<div style="position:absolute;width:1px;height:1px;padding:0;margin:-1px;overflow:hidden;clip:rect(0,0,0,0);white-space:nowrap;border-width:0;">
+			<label for="date_of_birth">Date of birth</label>
+			<input
+				type="text"
+				aria-hidden="true"
+				tabindex="-1"
+				name="date_of_birth"
+			>
+		</div>
 
 		<div>
 			<fieldset class="mb-6">
